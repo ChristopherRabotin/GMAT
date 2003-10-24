4a876127a8da5b24ba2aacd7bf7da68d4f9f62fa
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2003-10-24 16:56:47 +0000

    Initial Version.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@45 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/factory/SubscriberFactory.hpp
@@ -0,0 +1,57 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                         SubscriberFactory
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G
+//
+// Author: Wendy Shoan
+// Created: 2003/10/22
+//
+/**
+*  This class is the factory class for Subscribers.
+ */
+//------------------------------------------------------------------------------
+#ifndef SubscriberFactory_hpp
+#define SubscriberFactory_hpp
+
+#include "gmatdefs.hpp"
+#include "Factory.hpp"
+#include "Subscriber.hpp"
+
+class GMAT_API SubscriberFactory : public Factory
+{
+public:
+   Subscriber*  CreateSubscriber(std::string ofType,
+                                 std::string withName = "");
+
+   // default constructor
+   SubscriberFactory();
+   // constructor
+   SubscriberFactory(StringArray createList);
+   // copy constructor
+   SubscriberFactory(const SubscriberFactory& fact);
+   // assignment operator
+   SubscriberFactory& operator= (const SubscriberFactory& fact);
+
+   // destructor
+   ~SubscriberFactory();
+
+protected:
+      // protected data
+
+private:
+      // private data
+
+
+};
+
+#endif // SubscriberFactory_hpp
+
+
+
+
