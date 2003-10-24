e685b9ec0027c4f6ddc5385318f60a665e2fcbd4
dconway <dconway@222415a0-0dc2-4184-858d-a27cde375526>
2003-10-24 14:23:54 +0000

    Added gmatdefs to the repository
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@38 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/include/gmatdefs.hpp
@@ -0,0 +1,104 @@
+// *** File Name : gmatdefs.h
+// *** Created   : May 20, 2003
+// **************************************************************************
+// ***  Developed By  :  Thinking Systems, Inc. (www.thinksysinc.com)     ***
+// ***  For:  Flight Dynamics Analysis Branch (Code 595)                  ***
+// ***  Under Contract:  P.O.  GSFC S-67573-G                             ***
+// ***  Copyright Thinking Systems 2003                                   ***
+// ***                                                                    ***
+// ***  Header Version: July 12, 2002                                     ***
+// **************************************************************************
+// Module Type               : ANSI C++ Source
+// Development Environment   : Visual C++ 7.0
+// Modification History      : 5/20/2003 - D. Conway, Thinking Systems, Inc.
+//                             Original delivery
+//                             Source code not yet transferred to GSFC
+//                           : 2003/09/16 - W. Shoan/GSFC/583
+//                             added ObjectTypes enum type in namespace Gmat
+// **************************************************************************
+
+#ifndef GMATDEFS_HPP
+#define GMATDEFS_HPP
+
+
+#include <string>               // For std::string
+#include <vector>               // For std::vector
+
+
+#ifdef _WIN32  // Windows
+
+   #ifdef _MSC_VER  // Microsoft Visual C++
+
+      #define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
+      // Windows Header File entry point:
+      #include <windows.h>
+
+      #define  _USE_MATH_DEFINES  // makes Msoft define pi
+    
+   #endif  // End Microsoft C++ specific block
+
+   #ifdef _DYNAMICLINK  // Only used for Windows DLLs
+      #ifdef GMAT_EXPORTS
+         #define GMAT_API __declspec(dllexport)
+      #else
+         #define GMAT_API __declspec(dllimport)
+      #endif
+   #endif
+
+#endif //  End of OS nits
+
+#ifndef GMAT_API
+   #define GMAT_API
+#endif
+
+
+typedef double Real;              // 8 byte float
+typedef int    Integer;           // 4 byte signed integer
+
+typedef std::vector<Real> RealArray;
+typedef std::vector<Integer> IntegerArray;
+typedef std::vector<std::string> StringArray;
+
+namespace Gmat
+{
+   enum ObjectType
+   {
+      SPACECRAFT= 1001,
+      GROUND_STATION,
+      COMMAND,
+      PROPAGATOR,
+      FORCE_MODEL,
+      PHYSICAL_MODEL,
+      INTERPOLATOR,
+      SOLAR_SYSTEM,
+      CELESTIAL_BODY,
+      PARAMETER,
+      STOP_CONDITION,
+      SOLVER,
+      SUBSCRIBER,
+      PROP_SETUP,
+      UNKNOWN_OBJECT
+   };
+
+
+   enum ParameterType
+   {
+      INTEGER_TYPE,
+      REAL_TYPE,
+      STRING_TYPE,
+      BOOLEAN_TYPE,
+      RVECTOR3_TYPE,
+      RVECTOR6_TYPE,
+      RMATRIX33_TYPE,
+      CARTESIAN_TYPE,
+      KEPLERIAN_TYPE,
+      A1MJD_TYPE,
+      UTCDATE_TYPE,
+      TypeCount,
+      UNKNOWN_PARAMETER_TYPE = -1
+   };
+
+}
+
+#endif //GMATDEFS_HPP
+
