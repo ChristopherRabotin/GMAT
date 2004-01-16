abf0e730f15e1ed08bb6351abcc8fbcf7b819e4e
dconway <dconway@222415a0-0dc2-4184-858d-a27cde375526>
2004-01-16 18:33:36 +0000

    Added wrappers around RealTypes macros so it will build with gcc 3.3 on Mac.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@265 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/util/RealTypes.hpp
+++ src/base/util/RealTypes.hpp
@@ -13,7 +13,7 @@
 // Created: 2003/09/15
 //
 /**
- * Ddeclarations for real constants and types.
+ * Declarations for real constants and types.
  */
 //------------------------------------------------------------------------------
 #ifndef RealTypes_hpp
@@ -27,17 +27,61 @@ typedef Real Radians;
 namespace GmatRealConst
 {
 
-   const Integer SHORT_REAL_RADIX = __glibcpp_f64_radix;
+   #ifdef __glibcpp_f64_radix
+      const Integer SHORT_REAL_RADIX = __glibcpp_f64_radix;
+   #else
+      const Integer SHORT_REAL_RADIX = 2;
+   #endif
+      
    //loj: Couldn't find coressponding MANT_DIG, do this later
    //const Integer REAL_MANT_DIG;
-   const Real    REAL_EPSILON     = __glibcpp_f64_epsilon;
-   const Integer REAL_DIG         = __glibcpp_f64_digits;
-   const Integer REAL_MIN_EXP     = __glibcpp_f64_min_exponent;
-   const Real    REAL_MIN         = __glibcpp_f64_min;
-   const Integer REAL_MIN_10_EXP  = __glibcpp_f64_min_exponent10;
-   const Integer REAL_MAX_EXP     = __glibcpp_f64_max_exponent;
-   const Real    REAL_MAX         = __glibcpp_f64_max;
-   const Integer REAL_MAX_10_EXP  = __glibcpp_f64_max_exponent10;
+   #ifdef __glibcpp_f64_epsilon
+      const Real    REAL_EPSILON     = __glibcpp_f64_epsilon;
+   #else
+      const Real    REAL_EPSILON     = 2.2204460492503131e-16;
+   #endif
+
+   #ifdef __glibcpp_f64_digits
+      const Integer REAL_DIG         = __glibcpp_f64_digits;
+   #else
+      const Integer REAL_DIG         = 53;
+   #endif
+
+   #ifdef __glibcpp_f64_min_exponent
+      const Integer REAL_MIN_EXP     = __glibcpp_f64_min_exponent;
+   #else
+      const Integer REAL_MIN_EXP     = -1021;
+   #endif
+
+   #ifdef __glibcpp_f64_min
+      const Real    REAL_MIN         = __glibcpp_f64_min;
+   #else
+      const Real    REAL_MIN         = 2.2250738585072014e-308;
+   #endif
+
+   #ifdef __glibcpp_f64_min_exponent10
+      const Integer REAL_MIN_10_EXP  = __glibcpp_f64_min_exponent10;
+   #else
+      const Integer REAL_MIN_10_EXP  = -307;
+   #endif
+
+   #ifdef __glibcpp_f64_max_exponent
+      const Integer REAL_MAX_EXP     = __glibcpp_f64_max_exponent;
+   #else
+      const Integer REAL_MAX_EXP     = 1024;
+   #endif
+
+   #ifdef __glibcpp_f64_max
+      const Real    REAL_MAX         = __glibcpp_f64_max;
+   #else
+      const Real    REAL_MAX         = 1.7976931348623157e+308;
+   #endif
+
+   #ifdef __glibcpp_f64_max_exponent10
+      const Integer REAL_MAX_10_EXP  = __glibcpp_f64_max_exponent10;
+   #else
+      const Integer REAL_MAX_10_EXP  = 308;
+   #endif
    
    const Real    REAL_TOL         = REAL_EPSILON;
 };
