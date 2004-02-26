dc5322d74a7e7cbdb09274d2b1d3aaa9968ed649
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2004-02-26 19:12:59 +0000

    Added default values for sij, cij.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@488 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/solarsys/Planet.cpp
+++ src/base/solarsys/Planet.cpp
@@ -22,21 +22,29 @@
 // initialize static default values
 // default values for CelesitalBody data
 const Gmat::BodyType        Planet::BODY_TYPE           = Gmat::PLANET;
-const Real                  Planet::MASS                = 5.976e24;     // kg
-const Real                  Planet::EQUATORIAL_RADIUS   = 6378.140;     // km
-const Real                  Planet::POLAR_RADIUS        = 6356.755;       // km - need more precision?
-const Real                  Planet::MU                  = 3.986005e14;      // m^3/s^2
-const Gmat::PosVelSource    Planet::POS_VEL_SOURCE      = Gmat::SLP;   // for Build 2, at least
+const Real                  Planet::MASS                = 5.976e24;      // kg
+const Real                  Planet::EQUATORIAL_RADIUS   = 6378.1363;     // km
+const Real                  Planet::POLAR_RADIUS        = 6356.755;      // km - need more precision?
+const Real                  Planet::MU                  = 398600.4415;   // km^3/s^2
+const Gmat::PosVelSource    Planet::POS_VEL_SOURCE      = Gmat::SLP;     // for Build 2, at least
 const Gmat::AnalyticMethod  Planet::ANALYTIC_METHOD     = Gmat::TWO_BODY; // ??
 const Integer               Planet::BODY_NUMBER         = 1;  
 const Integer               Planet::REF_BODY_NUMBER     = 3; 
 const Integer               Planet::ORDER               = 4;  
 const Integer               Planet::DEGREE              = 4;
-const Integer               Planet::COEFFICIENT_SIZE    = 4;
-const Rmatrix               Planet::SIJ                 = Rmatrix(4,4,
-        0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
-const Rmatrix               Planet::CIJ                 = Rmatrix(4,4,
-       0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
+const Integer               Planet::COEFFICIENT_SIZE    = 4;  // do I need this?  Or just degree and order?
+const Rmatrix               Planet::SIJ                 = Rmatrix(5,5,
+   0.0, 0.0,             0.0,             0.0,             0.0,
+   0.0, 0.0,             0.0,             0.0,             0.0,
+   0.0, 0.119528010e-08,-0.140010930e-05, 0.0,             0.0,
+   0.0, 0.248806640e-06,-0.619230590e-06, 0.141403690e-05, 0.0,
+   0.0,-0.473422660e-06, 0.662868900e-06,-0.201009910e-06, 0.308845320e-06);
+const Rmatrix               Planet::CIJ                 = Rmatrix(5,5,
+   1.0,                0.0,                0.0,                0.0,                 0.0,
+   0.0,                0.0,                0.0,                0.0,                 0.0,
+   -0.484165390000e-03,-0.186987640000e-09, 0.243908370000e-05, 0.0,                 0.0,
+   0.957122390000e-06, 0.202839970000e-05, 0.904408620000e-06, 0.721153880000e-06,  0.0,
+   0.540143330000e-06,-0.536367950000e-06, 0.350349320000e-06, 0.990258120000e-06, -0.188488500000e-06);
 
 //------------------------------------------------------------------------------
 // public methods
