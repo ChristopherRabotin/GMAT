65c8daaa2c2abccec87635de79176220acbc9c63
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2004-03-10 15:40:04 +0000

    * No message *
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@571 222415a0-0dc2-4184-858d-a27cde375526

--- src/gui/app/GmatMainNotebook.cpp
+++ src/gui/app/GmatMainNotebook.cpp
@@ -194,6 +194,12 @@ void GmatMainNotebook::CreatePage(GmatTreeItemData *item)
             sizer->Add (new ParameterSetupPanel (panel, item->GetDesc()),
                         0, wxGROW|wxALL, 0 );
         }
+        else
+        {
+           // if no panel set up then just exit function
+           // instead of opening blank panel
+           return;
+        }
 
         panel->SetScrollRate( 5, 5 );
         panel->SetAutoLayout( TRUE );
@@ -244,34 +250,38 @@ void GmatMainNotebook::ClosePage()
 bool GmatMainNotebook::OpenPage(GmatTreeItemData *item)
 {
   bool result = TRUE;
-  int dataType = item->GetDataType();
-
-  if ((dataType == GmatTree::RESOURCES_FOLDER)            ||
-      (dataType == GmatTree::SPACECRAFT_FOLDER)           ||
-      (dataType == GmatTree::FORMATIONS_FOLDER)           ||
-      (dataType == GmatTree::CONSTELLATIONS_FOLDER)       ||
-      (dataType == GmatTree::BURNS_FOLDER)                ||
-      (dataType == GmatTree::PROPAGATORS_FOLDER)          ||
-      (dataType == GmatTree::SOLVERS_FOLDER)              ||
-      (dataType == GmatTree::SUBSCRIBERS_FOLDER)          ||
-      (dataType == GmatTree::SUBSCRIPTS_FOLDER)           ||
-      (dataType == GmatTree::INTERFACES_FOLDER)           ||
-      (dataType == GmatTree::VARIABLES_FOLDER)            ||
-      (dataType == GmatTree::DEFAULT_BODY)                ||
-      (dataType == GmatTree::CREATED_BODY)                ||
-      (dataType == GmatTree::DEFAULT_FORMATION_FOLDER)    ||
-      (dataType == GmatTree::CREATED_FORMATION_FOLDER)    ||
-      (dataType == GmatTree::DEFAULT_CONSTELLATION_FOLDER)||
-      (dataType == GmatTree::CREATED_CONSTELLATION_FOLDER)||
-      (dataType == GmatTree::MISSIONS_FOLDER)             ||
-      (dataType == GmatTree::MISSION_SEQ_TOP_FOLDER)      ||
-      (dataType == GmatTree::MISSION_SEQ_SUB_FOLDER))
-      
-  {
-    result = FALSE;
-  }
-  else
-  {
+  
+// ag: Kind of redundent, CreatePage was changed, so that
+//     if there is no data a blank page no longer opens
+  
+//  int dataType = item->GetDataType();
+//
+//  if ((dataType == GmatTree::RESOURCES_FOLDER)            ||
+//      (dataType == GmatTree::SPACECRAFT_FOLDER)           ||
+//      (dataType == GmatTree::FORMATIONS_FOLDER)           ||
+//      (dataType == GmatTree::CONSTELLATIONS_FOLDER)       ||
+//      (dataType == GmatTree::BURNS_FOLDER)                ||
+//      (dataType == GmatTree::PROPAGATORS_FOLDER)          ||
+//      (dataType == GmatTree::SOLVERS_FOLDER)              ||
+//      (dataType == GmatTree::SUBSCRIBERS_FOLDER)          ||
+//      (dataType == GmatTree::SUBSCRIPTS_FOLDER)           ||
+//      (dataType == GmatTree::INTERFACES_FOLDER)           ||
+//      (dataType == GmatTree::VARIABLES_FOLDER)            ||
+//      (dataType == GmatTree::DEFAULT_BODY)                ||
+//      (dataType == GmatTree::CREATED_BODY)                ||
+//      (dataType == GmatTree::DEFAULT_FORMATION_FOLDER)    ||
+//      (dataType == GmatTree::CREATED_FORMATION_FOLDER)    ||
+//      (dataType == GmatTree::DEFAULT_CONSTELLATION_FOLDER)||
+//      (dataType == GmatTree::CREATED_CONSTELLATION_FOLDER)||
+//      (dataType == GmatTree::MISSIONS_FOLDER)             ||
+//      (dataType == GmatTree::MISSION_SEQ_TOP_FOLDER)      ||
+//      (dataType == GmatTree::MISSION_SEQ_SUB_FOLDER))
+//      
+//  {
+//    result = FALSE;
+//  }
+//  else
+//  {
     int sel = curPages->IndexOf((wxObject*) item);
 
     if (sel == -1)   // NOT_FOUND
@@ -283,7 +293,7 @@ bool GmatMainNotebook::OpenPage(GmatTreeItemData *item)
       this->SetSelection(sel);
       result = FALSE;
     }
-  }
+ // }
   
   return result;
 }
