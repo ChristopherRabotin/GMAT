edc40e017de9bc42e92dddabaa5d3ca88c9c295b
ljun <ljun@222415a0-0dc2-4184-858d-a27cde375526>
2004-05-03 19:14:18 +0000

    added Rvector(Real vec[6])
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@765 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/util/Rvector6.hpp
+++ src/base/util/Rvector6.hpp
@@ -29,42 +29,46 @@ class Rvector6 : public Rvector
 {
 public:
 
-    Rvector6();
-    Rvector6(const Real e1, const Real e2, const Real e3, const Real e4,
-             const Real e5, const Real e6);
-    Rvector6(const Rvector3 &r, const Rvector3 &v);
-    Rvector6(const Rvector6 &v);
-    Rvector6& operator=(const Rvector6 &v);
-    virtual ~Rvector6();
+   Rvector6();
+   Rvector6(const Real e1, const Real e2, const Real e3, const Real e4,
+            const Real e5, const Real e6);
+   Rvector6(const Rvector3 &r, const Rvector3 &v);
+   Rvector6(Real vec[6]); //loj: 4/20/04 added
+   Rvector6(const Rvector6 &v);
+   Rvector6& operator=(const Rvector6 &v);
+   virtual ~Rvector6();
    
-    Real Get(Integer index) const;
-    Rvector3 GetR() const;
-    Rvector3 GetV() const;
-
-    void Set(const Real e1, const Real e2, const Real e3,
-             const Real e4, const Real e5, const Real e6);
-    void SetR(const Rvector3 &v);
-    void SetV(const Rvector3 &v);
-
-    bool operator==(const Rvector6 &v)const;
-    bool operator!=(const Rvector6 &v)const;
-    Rvector6 operator-() const;                    // negation 
-    Rvector6 operator+(const Rvector6 &v) const;
-    const Rvector6& operator+=(const Rvector6 &v);
-    Rvector6 operator-(const Rvector6 &v) const; 
-    const Rvector6& operator-=(const Rvector6 &v);
-    Rvector6 operator*(Real s) const;
-    const Rvector6& operator*=(Real s);
-    Rvector6 operator/(Real s) const;
-    const Rvector6& operator/=(Real s);
+   Real Get(Integer index) const;
+   Rvector3 GetR() const;
+   Rvector3 GetV() const;
+   void GetR(Real *r) const; //loj: 4/19/04 added
+   void GetV(Real *v) const; //loj: 4/19/04 added
    
-    Integer GetNumData() const;
-    const std::string* GetDataDescriptions() const;
-    std::string* ToValueStrings();
+   void Set(const Real e1, const Real e2, const Real e3,
+            const Real e4, const Real e5, const Real e6);
+   void SetR(const Rvector3 &v);
+   void SetV(const Rvector3 &v);
 
-    bool IsValid(const Real val);
-    static const Real UTIL_REAL_UNDEFINED = -987654321.0123e-45;
-    static const Rvector6 RVECTOR6_UNDEFINED;
+   bool operator==(const Rvector6 &v)const;
+   bool operator!=(const Rvector6 &v)const;
+   Rvector6 operator-() const;                    // negation 
+   Rvector6 operator+(const Rvector6 &v) const;
+   const Rvector6& operator+=(const Rvector6 &v);
+   Rvector6 operator-(const Rvector6 &v) const; 
+   const Rvector6& operator-=(const Rvector6 &v);
+   Rvector6 operator*(Real s) const;
+   const Rvector6& operator*=(Real s);
+   Rvector6 operator/(Real s) const;
+   const Rvector6& operator/=(Real s);
+   
+   Integer GetNumData() const;
+   const std::string* GetDataDescriptions() const;
+   std::string* ToValueStrings();
+   std::string ToString();
+   
+   bool IsValid(const Real val);
+   static const Real UTIL_REAL_UNDEFINED = -987654321.0123e-45;
+   static const Rvector6 RVECTOR6_UNDEFINED;
     
 protected:
 private:
