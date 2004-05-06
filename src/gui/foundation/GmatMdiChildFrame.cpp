0a57c21f7c250f5b2c9345e7178817d456e0c5e0
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2004-05-06 20:50:55 +0000

    Initial version
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@798 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/foundation/GmatMdiChildFrame.cpp
@@ -0,0 +1,65 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                             GmatNotebook
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// ** Legal **
+//
+// Author: Allison Greene
+// Created: 2003/08/28
+/**
+ * This class provides the layout of a mdi child frame
+ */
+//------------------------------------------------------------------------------
+#include "GmatMdiChildFrame.hpp"
+#include "GmatAppData.hpp"
+
+//------------------------------
+// event tables for wxWindows
+//------------------------------
+
+//------------------------------------------------------------------------------
+// EVENT_TABLE(GmatMdiChildFrame, wxMDIChildFrame)
+//------------------------------------------------------------------------------
+/**
+ * Events Table for the menu and tool bar
+ *
+ * @note Indexes event handler functions.
+ */
+//------------------------------------------------------------------------------
+BEGIN_EVENT_TABLE(GmatMdiChildFrame, wxMDIChildFrame)
+   EVT_CLOSE(GmatMdiChildFrame::OnClose) 
+END_EVENT_TABLE()
+
+GmatMdiChildFrame::GmatMdiChildFrame(wxMDIParentFrame* parent, 
+                       wxWindowID id, 
+                       const wxString& title, 
+                       const wxPoint& pos, 
+                       const wxSize& size, 
+                       long style, 
+                       const wxString& name)
+                 :wxMDIChildFrame(parent, id, title, pos, size, style, name)
+{
+   this->title.Printf("%s", title.c_str());
+}
+
+GmatMdiChildFrame::~GmatMdiChildFrame()
+{
+}
+
+
+void GmatMdiChildFrame::OnClose(wxCommandEvent& event)
+{
+   // check if window is dirty?
+   
+   // remove from list of frames
+//   event.Skip();
+//   GmatAppData::GetMainFrame()->RemoveChild(title);
+}
+
+wxString GmatMdiChildFrame::GetTitle()
+{
+   return title;
+}
+
---
+++ src/gui/foundation/GmatMdiChildFrame.hpp
@@ -0,0 +1,52 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              GmatMdiChildFrame
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Linda Jun
+// Created: 2004/02/02
+//
+/**
+ * Declares GmatMdiChildFrame class.
+ */
+//------------------------------------------------------------------------------
+
+#ifndef GmatMdiChildFrame_hpp
+#define GmatMdiChildFrame_hpp
+
+#include "gmatwxdefs.hpp"
+
+#include "GmatAppData.hpp"
+#include "GuiInterpreter.hpp"
+#include "GuiItemManager.hpp"
+
+class GmatMdiChildFrame : public wxMDIChildFrame
+{
+public:
+    // constructors
+    GmatMdiChildFrame( wxMDIParentFrame* parent, 
+                        wxWindowID id = -1, 
+                        const wxString& title = "", 
+                        const wxPoint& pos = wxDefaultPosition, 
+                        const wxSize& size = wxDefaultSize, 
+                        long style = wxDEFAULT_FRAME_STYLE, 
+                        const wxString& name = "");
+    ~GmatMdiChildFrame();
+   
+protected:
+    wxString title;
+    
+    // any class wishing to process wxWindows events must use this macro
+    DECLARE_EVENT_TABLE();
+    void OnClose(wxCommandEvent& event);
+    wxString GetTitle();
+    
+    // IDs for the controls and the menu commands
+    enum
+    {     
+    };
+
+};
+
+#endif // GmatMdiChildFrame_hpp
