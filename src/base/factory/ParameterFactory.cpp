66903c68be238a36875a7817bb500cbc5ee8e614
ljun <ljun@222415a0-0dc2-4184-858d-a27cde375526>
2004-07-13 18:42:48 +0000

    added OrbitPeriod
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1120 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/factory/ParameterFactory.cpp
+++ src/base/factory/ParameterFactory.cpp
@@ -32,9 +32,6 @@
 //#include "Ecc.hpp"
 //#include "Inc.hpp"
 
-
-// add others here for future builds
-
 //---------------------------------
 //  public methods
 //---------------------------------
@@ -52,83 +49,85 @@
 Parameter* ParameterFactory::CreateParameter(std::string ofType,
                                              std::string withName)
 {
-    // Time parameters
-    if (ofType == "ElapsedDays")
-        return new ElapsedDays(withName);
-    if (ofType == "ElapsedSecs")
-        return new ElapsedSecs(withName);
-    if (ofType == "CurrA1MJD")
-        return new CurrA1MJD(withName);
+   // Time parameters
+   if (ofType == "ElapsedDays")
+      return new ElapsedDays(withName);
+   if (ofType == "ElapsedSecs")
+      return new ElapsedSecs(withName);
+   if (ofType == "CurrA1MJD")
+      return new CurrA1MJD(withName);
 
     // Cartesian paramters
-    if (ofType == "X")
-        return new CartX(withName);
-    if (ofType == "Y")
-        return new CartY(withName);
-    if (ofType == "Z")
-        return new CartZ(withName);
-    if (ofType == "VX")
-        return new CartVx(withName);
-    if (ofType == "VY")
-        return new CartVy(withName);
-    if (ofType == "VZ")
-        return new CartVz(withName);
+   if (ofType == "X")
+      return new CartX(withName);
+   if (ofType == "Y")
+      return new CartY(withName);
+   if (ofType == "Z")
+      return new CartZ(withName);
+   if (ofType == "VX")
+      return new CartVx(withName);
+   if (ofType == "VY")
+      return new CartVy(withName);
+   if (ofType == "VZ")
+      return new CartVz(withName);
 
     // Keplerian parameters
-    if (ofType == "SMA")
-        return new KepSMA(withName);
-    if (ofType == "ECC")
-        return new KepEcc(withName);
-    if (ofType == "INC")
-        return new KepInc(withName);
-    if (ofType == "RAAN")
-        return new KepRAAN(withName);
-    if (ofType == "AOP")
-        return new KepAOP(withName);
-    if (ofType == "TA")
-        return new KepTA(withName);
-    if (ofType == "MA")
-        return new KepMA(withName);
-    if (ofType == "MM")
-        return new KepMM(withName);
-    //if (ofType == "SMA")
-    //    return new SMA(withName);
-    //if (ofType == "Ecc")
-    //    return new Ecc(withName);
-    //if (ofType == "Inc")
-    //    return new Inc(withName);
+   if (ofType == "SMA")
+      return new KepSMA(withName);
+   if (ofType == "ECC")
+      return new KepEcc(withName);
+   if (ofType == "INC")
+      return new KepInc(withName);
+   if (ofType == "RAAN")
+      return new KepRAAN(withName);
+   if (ofType == "AOP")
+      return new KepAOP(withName);
+   if (ofType == "TA")
+      return new KepTA(withName);
+   if (ofType == "MA")
+      return new KepMA(withName);
+   if (ofType == "MM")
+      return new KepMM(withName);
+   //if (ofType == "SMA")
+   //    return new SMA(withName);
+   //if (ofType == "Ecc")
+   //    return new Ecc(withName);
+   //if (ofType == "Inc")
+   //    return new Inc(withName);
 
-    // Spherical parameters
-    if (ofType == "RMAG")
-        return new SphRMag(withName);
-    if (ofType == "RA")
-        return new SphRA(withName);
-    if (ofType == "DEC")
-        return new SphDec(withName);
-    if (ofType == "VMAG")
-        return new SphVMag(withName);
-    if (ofType == "RAV")
-        return new SphRAV(withName);
-    if (ofType == "DECV")
-        return new SphDecV(withName);
+   // Spherical parameters
+   if (ofType == "RMAG")
+      return new SphRMag(withName);
+   if (ofType == "RA")
+      return new SphRA(withName);
+   if (ofType == "DEC")
+      return new SphDec(withName);
+   if (ofType == "VMAG")
+      return new SphVMag(withName);
+   if (ofType == "RAV")
+      return new SphRAV(withName);
+   if (ofType == "DECV")
+      return new SphDecV(withName);
 
-    // Orbital parameters
-    if (ofType == "VelApoapsis")
-        return new VelApoapsis(withName);
-    if (ofType == "VelPeriapsis")
-        return new VelPeriapsis(withName);
-    if (ofType == "Apoapsis")
-        return new Apoapsis(withName);
-    if (ofType == "Periapsis")
-        return new Periapsis(withName);
+   // Orbital parameters
+   if (ofType == "VelApoapsis")
+      return new VelApoapsis(withName);
+   if (ofType == "VelPeriapsis")
+      return new VelPeriapsis(withName);
+   if (ofType == "Apoapsis")
+      return new Apoapsis(withName);
+   if (ofType == "Periapsis")
+      return new Periapsis(withName);
+   if (ofType == "OrbitPeriod") //loj: 7/13/04 added
+      return new OrbitPeriod(withName);
 
-    // Angular parameters
-    if (ofType == "SemilatusRectum")
-        return new SemilatusRectum(withName);
+   // Angular parameters
+   if (ofType == "SemilatusRectum")
+      return new SemilatusRectum(withName);
 
-    // add others here
-    else
-        return NULL;
+   // add others here
+   else
+      return NULL;
 }
 
 
@@ -143,53 +142,54 @@ Parameter* ParameterFactory::CreateParameter(std::string ofType,
  */
 //------------------------------------------------------------------------------
 ParameterFactory::ParameterFactory()
-    : Factory(Gmat::PARAMETER)
+   : Factory(Gmat::PARAMETER)
 {
-    if (creatables.empty())
-    {
-        // Time parameters
-        creatables.push_back("ElapsedDays");
-        creatables.push_back("ElapsedSecs");
-        creatables.push_back("CurrA1MJD");
+   if (creatables.empty())
+   {
+      // Time parameters
+      creatables.push_back("ElapsedDays");
+      creatables.push_back("ElapsedSecs");
+      creatables.push_back("CurrA1MJD");
 
-        // Cartesian parameters
-        creatables.push_back("X");
-        creatables.push_back("Y");
-        creatables.push_back("Z");
-        creatables.push_back("VX");
-        creatables.push_back("VY");
-        creatables.push_back("VZ");
+      // Cartesian parameters
+      creatables.push_back("X");
+      creatables.push_back("Y");
+      creatables.push_back("Z");
+      creatables.push_back("VX");
+      creatables.push_back("VY");
+      creatables.push_back("VZ");
 
-        // Keplerian parameters
-        creatables.push_back("SMA");
-        creatables.push_back("ECC");
-        creatables.push_back("INC");
-        creatables.push_back("RAAN");
-        creatables.push_back("AOP");
-        creatables.push_back("TA");
-        creatables.push_back("MA");
-        creatables.push_back("MM");
-        //creatables.push_back("SMA");
-        //creatables.push_back("Ecc");
-        //creatables.push_back("Inc");
+      // Keplerian parameters
+      creatables.push_back("SMA");
+      creatables.push_back("ECC");
+      creatables.push_back("INC");
+      creatables.push_back("RAAN");
+      creatables.push_back("AOP");
+      creatables.push_back("TA");
+      creatables.push_back("MA");
+      creatables.push_back("MM");
+      //creatables.push_back("SMA");
+      //creatables.push_back("Ecc");
+      //creatables.push_back("Inc");
 
-        // Orbital parameters
-        creatables.push_back("VelApoapsis");
-        creatables.push_back("VelPeriapsis");
-        creatables.push_back("Apoapsis");
-        creatables.push_back("Periapsis");
+      // Orbital parameters
+      creatables.push_back("VelApoapsis");
+      creatables.push_back("VelPeriapsis");
+      creatables.push_back("Apoapsis");
+      creatables.push_back("Periapsis");
+      creatables.push_back("OrbitPeriod"); //loj: 7/13/04 added
 
-        // Spherical parameters
-        creatables.push_back("RMAG");
-        creatables.push_back("RA");
-        creatables.push_back("DEC");
-        creatables.push_back("VMAG");
-        creatables.push_back("RAV");
-        creatables.push_back("DECV");
+      // Spherical parameters
+      creatables.push_back("RMAG");
+      creatables.push_back("RA");
+      creatables.push_back("DEC");
+      creatables.push_back("VMAG");
+      creatables.push_back("RAV");
+      creatables.push_back("DECV");
 
-        // Angular parameters
-        creatables.push_back("SemilatusRectum");
-    }
+      // Angular parameters
+      creatables.push_back("SemilatusRectum");
+   }
 }
 
 //------------------------------------------------------------------------------
@@ -204,7 +204,7 @@ ParameterFactory::ParameterFactory()
  */
 //------------------------------------------------------------------------------
 ParameterFactory::ParameterFactory(StringArray createList) :
-Factory(createList, Gmat::PARAMETER)
+   Factory(createList, Gmat::PARAMETER)
 {
 }
 
@@ -212,16 +212,15 @@ Factory(createList, Gmat::PARAMETER)
 //  PropagatorFactory(const PropagatorFactory& fact)
 //------------------------------------------------------------------------------
 /**
-   * This method creates an object of the class ParameterFactory
-   * (copy constructor).
+ * This method creates an object of the class ParameterFactory
+ * (copy constructor).
    *
    * @param <fact> the factory object to copy to "this" factory.
    */
 //------------------------------------------------------------------------------
 ParameterFactory::ParameterFactory(const ParameterFactory& fact) :
-Factory(fact)
+   Factory(fact)
 {
-
 }
 
 //------------------------------------------------------------------------------
@@ -246,7 +245,7 @@ ParameterFactory& ParameterFactory::operator= (const ParameterFactory& fact)
 // ~ParameterFactory()
 //------------------------------------------------------------------------------
 /**
-   * Destructor for the ParameterFactory base class.
+ * Destructor for the ParameterFactory base class.
    *
    */
 //------------------------------------------------------------------------------
@@ -255,10 +254,3 @@ ParameterFactory::~ParameterFactory()
    // deletes handled by Factory destructor
 }
 
-//---------------------------------
-//  protected methods
-//---------------------------------
-
-//---------------------------------
-//  private methods
-//---------------------------------
