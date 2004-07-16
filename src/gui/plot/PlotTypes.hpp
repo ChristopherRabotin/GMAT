dcfe7630e6343c401d3ce0db6638a2825f4ac582
ljun <ljun@222415a0-0dc2-4184-858d-a27cde375526>
2004-07-16 15:11:00 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1134 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/plot/PlotTypes.hpp
@@ -0,0 +1,23 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              PlotTypes
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// ** Legal **
+//
+// Author: Linda Jun
+// Created: 2004/07/14
+/**
+ * Declares constants used by plot.
+ */
+//------------------------------------------------------------------------------
+#ifndef PlotTypes_hpp
+#define PlotTypes_hpp
+
+namespace GmatPlot
+{
+   const int MAX_XY_CURVE = 6;
+}
+
+#endif
