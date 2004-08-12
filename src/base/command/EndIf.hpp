0cdb941778f3f110cb3ba2866e22564fd354add0
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2004-08-12 18:08:16 +0000

    Changed form EndIfCommand
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1245 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/command/EndIf.hpp
@@ -0,0 +1,46 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                             EndIf
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P
+//
+// Author:  Joey Gurganus 
+// Created: 2004/01/30
+//
+/**
+ * Definition for the closing line of the IF Statement
+ */
+//------------------------------------------------------------------------------
+
+
+#ifndef EndIf_hpp
+#define EndIf_hpp
+
+
+#include "Command.hpp"
+
+
+class EndIf : public GmatCommand
+{
+public:
+    EndIf(void);
+    virtual ~EndIf(void);
+    
+    EndIf(const EndIf& ic);
+    EndIf&              operator=(const EndIf& ic);
+    
+    virtual bool            Initialize(void);
+    virtual bool            Execute(void);
+
+    // inherited from GmatBase
+    virtual GmatBase* Clone(void) const;
+
+};
+
+
+#endif // EndIf_hpp
