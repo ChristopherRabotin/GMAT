5ef9f36eb5f9fb5a246058bc3b43d45f7f2b5208
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2004-08-12 18:45:58 +0000

    Adding just to fix tag
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1254 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/command/EndIfCommand.cpp
@@ -0,0 +1,81 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                             EndIfCommand
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P
+//
+// Author:  Joey Gurganus
+// Created: 2004/01/29
+//
+/**
+ * Definition for the closing line of the ENDIF Statement
+ */
+//------------------------------------------------------------------------------
+
+
+#include "EndIfCommand.hpp"
+
+
+EndIfCommand::EndIfCommand(void) :
+    GmatCommand         ("EndIf")
+{}
+
+
+EndIfCommand::~EndIfCommand(void)
+{}
+    
+
+EndIfCommand::EndIfCommand(const EndIfCommand& ic) :
+    GmatCommand         (ic)
+{}
+
+
+EndIfCommand& EndIfCommand::operator=(const EndIfCommand& ic)
+{
+    if (this == &ic)
+        return *this;
+    GmatCommand::operator=(ic);
+    
+    return *this;
+}
+    
+
+bool EndIfCommand::Initialize(void)
+{
+    // Validate that next points to the owning for command
+    if (!next)
+        throw CommandException("EndIf Command not properly reconnected");
+    
+    if (next->GetTypeName() != "If" && next->GetTypeName() != "Else")
+        throw CommandException("EndIf Command not connected to IF Command");
+                             
+    return true;    
+}
+
+
+bool EndIfCommand::Execute(void)
+{
+    return true;
+}
+
+
+//------------------------------------------------------------------------------
+//  GmatBase* Clone(void) const
+//------------------------------------------------------------------------------
+/**
+ * This method returns a clone of the EndIfCommand.
+ *
+ * @return clone of the EndIfCommand.
+ *
+ */
+//------------------------------------------------------------------------------
+GmatBase* EndIfCommand::Clone(void) const
+{
+   return (new EndIfCommand(*this));
+}
+
---
+++ src/base/command/EndIfCommand.hpp
@@ -0,0 +1,46 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                             EndIfCommand
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P
+//
+// Author:  Joey Gurganus 
+// Created: 2004/01/30
+//
+/**
+ * Definition for the closing line of the IF Statement
+ */
+//------------------------------------------------------------------------------
+
+
+#ifndef EndIfCommand_hpp
+#define EndIfCommand_hpp
+
+
+#include "Command.hpp"
+
+
+class EndIfCommand : public GmatCommand
+{
+public:
+    EndIfCommand(void);
+    virtual ~EndIfCommand(void);
+    
+    EndIfCommand(const EndIfCommand& ic);
+    EndIfCommand&              operator=(const EndIfCommand& ic);
+    
+    virtual bool            Initialize(void);
+    virtual bool            Execute(void);
+
+    // inherited from GmatBase
+    virtual GmatBase* Clone(void) const;
+
+};
+
+
+#endif // EndIfCommand_hpp
