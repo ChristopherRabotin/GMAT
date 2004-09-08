5c7ae3ed1acb41751c1b441db91cb05eeaaf0f11
ljun <ljun@222415a0-0dc2-4184-858d-a27cde375526>
2004-09-08 22:02:09 +0000

    initial version
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1302 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/interface/GmatInterface.cpp
@@ -0,0 +1,207 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              GmatInterface
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G
+//
+// Author: Linda Jun
+// Created: 2004/8/30
+//
+/**
+ * Implements class to provide sending scripts from MATLAB to GMAT.
+ */
+//------------------------------------------------------------------------------
+
+#include "GmatInterface.hpp"
+#include "Moderator.hpp"         // for Instance()
+#include "MessageInterface.hpp"
+
+GmatInterface* GmatInterface::instance = NULL;
+
+//#define DEBUG_GMAT_INTERFACE 1
+
+//---------------------------------
+// public methods
+//---------------------------------
+
+//------------------------------------------------------------------------------
+// static Instance()
+//------------------------------------------------------------------------------
+GmatInterface* GmatInterface::Instance()
+{
+   if (instance == NULL)
+      instance = new GmatInterface();
+   return instance;
+}
+
+
+//------------------------------------------------------------------------------
+// void OpenScript()
+//------------------------------------------------------------------------------
+void GmatInterface::OpenScript()
+{
+   if (!mInStringStream)
+      mInStringStream = new std::istringstream;
+
+   //ClearScript();
+}
+
+//------------------------------------------------------------------------------
+// void ClearScrip()
+//------------------------------------------------------------------------------
+void GmatInterface::ClearScript()
+{
+   //Moderator *moderator = Moderator::Instance();
+   mStringStream.str("");
+   //moderator->ClearResource();
+   //moderator->ClearCommandSeq();
+   //Moderator::GetGuiInterpreter()->UpdateResourceTree();
+   //Moderator::GetGuiInterpreter()->UpdateMissionTree();
+   Moderator::GetGuiInterpreter()->CloseCurrentProject();
+}
+
+//------------------------------------------------------------------------------
+// void PutScript(char *str)
+//------------------------------------------------------------------------------
+void GmatInterface::PutScript(char *str)
+{
+   mStringStream << std::string(str) << std::endl;
+#if DEBUG_GMAT_INTERFACE
+   MessageInterface::ShowMessage("GmatInterface::PutScript() str=%s\n", str);
+#endif
+}
+
+//------------------------------------------------------------------------------
+// void BuildObject()
+//------------------------------------------------------------------------------
+void GmatInterface::BuildObject()
+{
+  
+   Moderator *moderator = Moderator::Instance();
+   
+   std::streambuf *streamBuf = mStringStream.rdbuf();
+
+   // redirect mInStringStream into mStringStream
+   RedirectBuffer(mInStringStream, streamBuf);
+   
+#if DEBUG_GMAT_INTERFACE
+   //loj: 8/31/04 Why this causes problem for long scripts? buffer overflow?
+   //MessageInterface::ShowMessage
+   //   ("GmatInterface::BuildObject() mStringStream.str=\n%s", mStringStream.str().c_str());
+   //MessageInterface::ShowMessage
+   //   ("GmatInterface::BuildObject() mInStringStream.str=\n%s\n", mInStringStream->str().c_str());
+#endif
+   
+   // flag to clear objects and mission sequence
+   moderator->InterpretScript(mInStringStream, true);
+  
+   Moderator::GetGuiInterpreter()->UpdateResourceTree();
+   Moderator::GetGuiInterpreter()->UpdateMissionTree();
+
+   // empty the buffer, once objects are created
+   mStringStream.str("");
+}
+
+//------------------------------------------------------------------------------
+// void UpdateObject()
+//------------------------------------------------------------------------------
+void GmatInterface::UpdateObject()
+{
+  
+   Moderator *moderator = Moderator::Instance();
+   
+   std::streambuf *streamBuf = mStringStream.rdbuf();
+
+   // redirect mInStringStream into mStringStream
+   RedirectBuffer(mInStringStream, streamBuf);
+   
+#if DEBUG_GMAT_INTERFACE
+   //loj: 8/31/04 Why this causes problem for long scripts? buffer overflow?
+   //MessageInterface::ShowMessage
+   //   ("GmatInterface::UpdateObject() mStringStream.str=\n%s", mStringStream.str().c_str());
+   //MessageInterface::ShowMessage
+   //   ("GmatInterface::UpdateObject() mInStringStream.str=\n%s\n", mInStringStream->str().c_str());
+#endif
+
+   // flag not to clear objects and mission sequence
+   moderator->InterpretScript(mInStringStream, false);
+   Moderator::GetGuiInterpreter()->UpdateResourceTree();
+   Moderator::GetGuiInterpreter()->UpdateMissionTree();
+   
+   // empty the buffer, once objects are created
+   mStringStream.str("");
+}
+
+//------------------------------------------------------------------------------
+// void RunScript()
+//------------------------------------------------------------------------------
+void GmatInterface::RunScript()
+{
+#if DEBUG_GMAT_INTERFACE
+   MessageInterface::ShowMessage("GmatInterface::RunScript() entered\n");
+#endif
+   
+   Moderator::Instance()->RunScript();
+}
+
+//------------------------------------------------------------------------------
+// char* GetParameterData(const std::string &name)
+//------------------------------------------------------------------------------
+char* GmatInterface::GetParameterData(const std::string &name)
+{
+#if DEBUG_GMAT_INTERFACE
+   MessageInterface::ShowMessage
+      ("GmatInterface::GetParameterData() name=%s\n", name.c_str());
+#endif
+
+   static char dataString[MAX_DATA_STRING];
+   static char *undefindString = "-123456789.123456789\0";
+   //static char *tempString2 = "[123456.12345, 234567.12345, 345678.12345]\0";
+   strcpy(dataString, undefindString);
+   //dataString[0] = '\0';
+   Parameter *param;
+   
+   param = Moderator::Instance()->GetParameter(name);
+   if (param != NULL)
+   {
+      param->Evaluate();
+      std::string str = param->ToString();
+      str = "[" + str + "]";
+      
+#if DEBUG_GMAT_INTERFACE
+      MessageInterface::ShowMessage("str=%s\n", str.c_str());
+#endif
+      
+      sprintf(dataString, "%s", str.c_str());
+      
+   }
+   
+   return dataString;
+}
+
+//---------------------------------
+// private methods
+//---------------------------------
+
+//------------------------------------------------------------------------------
+// GmatInterface()
+//------------------------------------------------------------------------------
+GmatInterface::GmatInterface()
+{
+   mInStringStream = NULL;
+}
+
+//------------------------------------------------------------------------------
+// GmatInterface()
+//------------------------------------------------------------------------------
+GmatInterface::~GmatInterface()
+{
+   if (mInStringStream)
+      delete mInStringStream;
+}
+  
---
+++ src/base/interface/GmatInterface.hpp
@@ -0,0 +1,54 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              GmatInterface
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G
+//
+// Author: Linda Jun
+// Created: 2004/8/30
+//
+/**
+ * Declares class to provide sending scripts from MATLAB to GMAT or
+ * requesting GMAT data from MATLAB.
+ */
+//------------------------------------------------------------------------------
+#ifndef GmatInterface_hpp
+#define GmatInterface_hpp
+
+#include "gmatdefs.hpp"
+#include <sstream>       // for stringstream, istringstream
+
+class GMAT_API GmatInterface
+{
+public:
+   static GmatInterface* Instance();
+
+   void OpenScript();
+   void ClearScript();
+   void PutScript(char *str);
+   void BuildObject();
+   void UpdateObject();
+   void RunScript();
+
+   char* GetParameterData(const std::string &name);
+   
+private:
+   static const int MAX_DATA_STRING = 256;
+   
+   void RedirectBuffer(std::ios *stream, std::streambuf* newBuff) 
+      { stream->rdbuf(newBuff); }
+
+   GmatInterface();
+   ~GmatInterface();
+   
+   std::stringstream mStringStream;
+   std::istringstream *mInStringStream;
+   static GmatInterface *instance;
+
+};
+#endif
