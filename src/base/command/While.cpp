c0215f5cee7f370de4d6a78a44b4d56ba8661a42
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2004-09-29 18:07:48 +0000

    Updated to be a subclass of ConditionalBranch; added support for multiple conditions.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1427 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/command/While.cpp
+++ src/base/command/While.cpp
@@ -22,17 +22,6 @@
 #include "While.hpp"
 #include "Parameter.hpp"
 
-const std::string
-While::OPTYPE_TEXT[NumberOfOperators] =
-{
-   "==",
-   "~=",
-   ">",
-   "<",
-   ">=",
-   "<="
-};
-
 //------------------------------------------------------------------------------
 //  While()
 //------------------------------------------------------------------------------
@@ -41,8 +30,7 @@ While::OPTYPE_TEXT[NumberOfOperators] =
  */
 //------------------------------------------------------------------------------
 While::While() :
-BranchCommand      ("While"),
-numberOfConditions (0),
+ConditionalBranch  ("While"),
 nestLevel          (0)
 {
 }
@@ -56,8 +44,7 @@ nestLevel          (0)
  */
 //------------------------------------------------------------------------------
 While::While(const While &wc) :
-BranchCommand      (wc),
-numberOfConditions (0),  // or do I want to copy them?
+ConditionalBranch  (wc),
 nestLevel          (0)
 {
 }
@@ -76,7 +63,7 @@ While& While::operator=(const While &wc)
 {
    if (this == &wc)
       return *this;
-   BranchCommand::operator=(wc);
+   ConditionalBranch::operator=(wc);
    // do I need to assign numberofConditions, all of the conditions, etc. here?
    return *this;
 }
@@ -110,7 +97,7 @@ While::~While()
 //------------------------------------------------------------------------------
 bool While::Append(GmatCommand *cmd)
 {
-    if (!BranchCommand::Append(cmd))
+    if (!ConditionalBranch::Append(cmd))
         return false;
 
     // Check for the end of "While" branch, point that end back to this command
@@ -137,25 +124,6 @@ bool While::Append(GmatCommand *cmd)
 }
 
 //------------------------------------------------------------------------------
-//  bool Initialize()
-//------------------------------------------------------------------------------
-/**
- * Performs the initialization needed to run the While statement.
- *
- * @return true if the Command is initialized, false if an error occurs.
- */
-//------------------------------------------------------------------------------
-bool While::Initialize()
-{
-    bool retval = BranchCommand::Initialize();
-
-    // if specific initialization goes here:
-    
-    return retval;
-}
-
-
-//------------------------------------------------------------------------------
 //  bool Execute()
 //------------------------------------------------------------------------------
 /**
@@ -170,11 +138,11 @@ bool While::Execute()
 {
    bool retval = true;
    
-   BranchCommand::Execute();
+   ConditionalBranch::Execute();
    //commandComplete  = false;
    //commandExecuting = true;
    
-   if (EvaluateCondition(0)) // must deal with multiple conditions later
+   if (EvaluateAllConditions()) // must deal with multiple conditions later
    {
       retval = ExecuteBranch();
    }
@@ -186,40 +154,6 @@ bool While::Execute()
    
    return retval;
 }
-
-//------------------------------------------------------------------------------
-//  bool SetCondition(std::string lhs, std::string operation,
-//                    std::string rhs)
-//------------------------------------------------------------------------------
-/**
- * This method sets a condition for the While Command.
- *
- * @return true if successful; false otherwise.
- *
- */
-//------------------------------------------------------------------------------
-bool While::SetCondition(std::string lhs, std::string operation,
-                         std::string rhs)
-{
-   bool retVal = false;
-   for (Integer i = 0; i < NumberOfOperators; i++)
-   {
-      if (operation == OPTYPE_TEXT[i])
-      {
-         opList.push_back((OpType)i);
-         retVal = true;
-         break;
-      }
-   }
-   if (retVal)
-   {
-      lhsList.push_back(lhs);
-      rhsList.push_back(rhs);
-      numberOfConditions++;
-   }
-   return retVal;
-}
-
 
 //------------------------------------------------------------------------------
 //  GmatBase* Clone() const
@@ -237,170 +171,7 @@ GmatBase* While::Clone() const
 }
 
 
-//------------------------------------------------------------------------------
-//  GmatBase* GetRefObject(const Gmat::ObjectType type,
-//                         const std::string &name,
-//                         const Integer index)
-//------------------------------------------------------------------------------
-/**
-* This method returns a reference object from the While Command.
- *
- * @param <type>  type of the reference object requested
- * @param <name>  name of the reference object requested
- * @param <index> index into the array of reference objects
- *
- * @return pointer to the reference object requested.
- *
- */
-//------------------------------------------------------------------------------
-GmatBase* While::GetRefObject(const Gmat::ObjectType type,
-                              const std::string &name,
-                              const Integer index)
-{
-   switch (type)
-   {
-      case Gmat::PARAMETER:
-         if (index < (Integer)params.size())
-         {
-            return params[index];
-         }
-         else
-         {
-            throw CommandException("While::GetRefObject() invalid index\n");
-         }
-      default:
-         break;
-   }
-
-   // Not handled here -- invoke the next higher GetRefObject call
-   return BranchCommand::GetRefObject(type, name, index);
-}
-
-//------------------------------------------------------------------------------
-//  bool SetRefObject(GmatBase *obj, const Gmat::ObjectType type,
-//                    const std::string &name, const Integer index)
-//------------------------------------------------------------------------------
-/**
- * This method sets a reference object for the While Command.
- *
- * @param <obj>   pointer to the reference object
- * @param <type>  type of the reference object 
- * @param <name>  name of the reference object
- * @param <index> index into the array of reference objects (where to put this
- *                 one)
- *
- * @return true if successful; otherwise, false.
- *
- */
-//------------------------------------------------------------------------------
-bool While::SetRefObject(GmatBase *obj, const Gmat::ObjectType type,
-                        const std::string &name,
-                        const Integer index)
-{
-   switch (type)
-   {
-      case Gmat::PARAMETER:
-      {
-         Integer size = params.size();
-         if (params.empty() && index == 0)
-         {
-            params.push_back((Parameter *)obj);
-            return true;
-         }
-         else if (index == size)
-         {
-            params.push_back((Parameter *)obj);
-            return true;
-         }
-         else if (index < size)
-         {
-            params[index] = (Parameter *)obj;
-            return true;
-         }
-         else
-         {
-            return false;
-         }
-      }
-      default:
-         break;
-   }
-
-   // Not handled here -- invoke the next higher SetRefObject call
-   return BranchCommand::SetRefObject(obj, type, name, index);
-}
-
 
 //------------------------------------------------------------------------------
 // protected methods
 //------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-//  bool EvaluateCondition(Integer which)
-//------------------------------------------------------------------------------
-/**
- * This method evaluates the spscified condition.
- *
- * @param which index into the array(s) of conditions
- *
- * @return false if which is out of bounds; result of evaluating the
- *         condition, otherwise.
- *
- * @note This method currently assumes that the rhs of the condition is a
- *       Real number.  In the future, we will need to allow for the possibility
- *       of the rhs of the condition being another parameter, which will need
- *       to be evaluated.
- */
-//------------------------------------------------------------------------------
-bool While::EvaluateCondition(Integer which)
-{
-   if ((which < 0) || (which >= (Integer) (lhsList.size())))
-   {
-      return false;
-   }
-   Real        lhsValue;
-   Real        rhsValue;
-   std::string theParmName = lhsList.at(which);
-   std::istringstream rhsStr(rhsList.at(which));
-   rhsStr >> rhsValue;
-
-   // iterate over the list of reference objects to find the parameter
-   std::vector<Parameter*>::iterator p = params.begin();
-   while (p != params.end())
-   {
-      if ((*p)->GetName() == theParmName)
-      {
-         lhsValue = (*p)->EvaluateReal();
-         switch (opList.at(which))
-         {
-            case EQUAL_TO:
-               return (lhsValue == rhsValue);
-               break;
-            case NOT_EQUAL:
-               return (lhsValue != rhsValue);
-               break;
-            case GREATER_THAN:
-               return (lhsValue > rhsValue);
-               break;
-            case LESS_THAN:
-               return (lhsValue < rhsValue);
-               break;
-            case GREATER_OR_EQUAL:
-               return (lhsValue >= rhsValue);
-               break;
-            case LESS_OR_EQUAL:
-               return (lhsValue <= rhsValue);
-               break;
-            default:
-               return false;
-               break;
-         }
-      }
-      ++p;
-   }
-   return false; // what should be done here, really?
-   
-}
-
-
-
-
