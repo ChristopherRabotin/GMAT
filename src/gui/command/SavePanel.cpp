52bcf55d87f3edb3f99f4ad3a7344b0d86ca5659
ljun <ljun@222415a0-0dc2-4184-858d-a27cde375526>
2004-10-20 18:21:55 +0000

    initial
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1477 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/command/SavePanel.cpp
@@ -0,0 +1,131 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              SavePanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Linda Jun
+// Created: 2004/10/14
+//
+/**
+ * This class contains the Save Setup window.
+ */
+//------------------------------------------------------------------------------
+
+#include "SavePanel.hpp"
+//#include "MessageInterface.hpp"
+
+//------------------------------------------------------------------------------
+// event tables and other macros for wxWindows
+//------------------------------------------------------------------------------
+
+BEGIN_EVENT_TABLE(SavePanel, GmatPanel)
+   EVT_BUTTON(ID_BUTTON_OK, GmatPanel::OnOK)
+   EVT_BUTTON(ID_BUTTON_APPLY, GmatPanel::OnApply)
+   EVT_BUTTON(ID_BUTTON_CANCEL, GmatPanel::OnCancel)
+   EVT_BUTTON(ID_BUTTON_SCRIPT, GmatPanel::OnScript)
+   EVT_COMBOBOX(ID_COMBOBOX, SavePanel::OnComboBoxChange)
+END_EVENT_TABLE()
+
+//------------------------------
+// public methods
+//------------------------------
+
+//------------------------------------------------------------------------------
+// SavePanel(wxWindow *parent)
+//------------------------------------------------------------------------------
+/**
+ * Constructs SavePanel object.
+ *
+ * @param <parent> input parent.
+ */
+//------------------------------------------------------------------------------
+SavePanel::SavePanel(wxWindow *parent, GmatCommand *cmd)
+   : GmatPanel(parent)
+
+{
+   theCommand = cmd;
+
+   if (theCommand != NULL)
+   {
+      Create();
+      Show();
+   }
+}
+
+//---------------------------------
+// protected methods
+//---------------------------------
+
+//----------------------------------
+// methods inherited from GmatPanel
+//----------------------------------
+
+//------------------------------------------------------------------------------
+// void Create()
+//------------------------------------------------------------------------------
+/**
+ * Creates the panel for the Maneuver Command
+ */
+//------------------------------------------------------------------------------
+void SavePanel::Create()
+{
+   StringArray items;
+   int bsize = 3;
+    
+   // create sizers
+   wxBoxSizer *pageBoxSizer = new wxBoxSizer(wxHORIZONTAL);
+
+   // create object label
+   wxStaticText *objectLabel =
+      new wxStaticText(this, ID_TEXT, wxT("Object"), wxDefaultPosition,
+                       wxDefaultSize, 0);
+
+   // list of object
+   //loj: 10/14/04 currently only Spacecraft is avaiable
+   mObjectComboBox =
+      theGuiManager->GetSpacecraftComboBox(this, ID_COMBOBOX, wxSize(150,-1));
+   
+   // add object label and combobox to sizer
+   pageBoxSizer->Add(objectLabel, 0, wxALIGN_CENTER | wxALL, bsize);
+   pageBoxSizer->Add(mObjectComboBox, 0, wxALIGN_CENTER | wxALL, bsize);
+   
+   // add to middle sizer
+   theMiddleSizer->Add(pageBoxSizer, 0, wxALIGN_CENTRE|wxALL, bsize);     
+
+}
+
+//------------------------------------------------------------------------------
+// virtual void LoadData()
+//------------------------------------------------------------------------------
+void SavePanel::LoadData()
+{
+   // load data from the core engine
+    
+   // object
+   wxString objName = theCommand->GetRefObjectName(Gmat::SPACECRAFT).c_str();
+   mObjectComboBox->SetStringSelection(objName);
+}
+
+//------------------------------------------------------------------------------
+// virtual void SaveData()
+//------------------------------------------------------------------------------
+void SavePanel::SaveData()
+{
+   std::string newObjName = mObjectComboBox->GetStringSelection().c_str();
+   
+   theCommand->SetRefObjectName(Gmat::SPACECRAFT, newObjName);
+}
+
+//---------------------------------
+// event handling
+//---------------------------------
+
+//------------------------------------------------------------------------------
+// void OnComboBoxChange(wxCommandEvent& event)
+//------------------------------------------------------------------------------
+void SavePanel::OnComboBoxChange(wxCommandEvent& event)
+{
+   theApplyButton->Enable();
+}
+
---
+++ src/gui/command/SavePanel.hpp
@@ -0,0 +1,55 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              SavePanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Linda Jun
+// Created: 2004/10/14
+//
+/**
+ * This class contains the Save Setup window.
+ */
+//------------------------------------------------------------------------------
+
+#ifndef SavePanel_hpp
+#define SavePanel_hpp
+
+#include "gmatwxdefs.hpp"
+#include "GmatAppData.hpp"
+#include "GmatPanel.hpp"
+#include "Command.hpp"
+#include "GuiItemManager.hpp"
+
+class SavePanel : public GmatPanel
+{
+public:
+   // constructors
+   SavePanel(wxWindow *parent, GmatCommand *cmd);
+    
+protected:
+   // member data
+   GmatCommand *theCommand;
+
+   wxComboBox *mObjectComboBox;
+    
+   // methods inherited from GmatPanel
+   virtual void Create();
+   virtual void LoadData();
+   virtual void SaveData();
+
+   // event handling
+   void OnComboBoxChange(wxCommandEvent& event);
+
+   // any class wishing to process wxWindows events must use this macro
+   DECLARE_EVENT_TABLE();
+    
+   // IDs for the controls and the menu commands
+   enum
+   {     
+      ID_TEXT = 80000,        
+      ID_COMBOBOX
+   };
+};
+
+#endif // SavePanel_hpp
