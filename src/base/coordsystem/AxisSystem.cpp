58dc711486cac3f557d155745f86917e75bfa893
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2004-12-29 14:04:06 +0000

    Base class for the axis systems.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1763 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/coordsystem/AxisSystem.cpp
@@ -0,0 +1,217 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                                  AxisSystem
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G and MOMS Task order 124
+//
+// Author: Wendy C. Shoan
+// Created: 2004/12/20
+//
+/**
+ * Implementation of the AxisSystem class.  This is the base class for the
+ * InertialAxes and DynamicAxes classes.
+ *
+ */
+//------------------------------------------------------------------------------
+
+#include "gmatdefs.hpp"
+#include "GmatBase.hpp"
+#include "AxisSystem.hpp"
+#include "CoordinateBase.hpp"
+#include "Rmatrix33.hpp"
+
+//---------------------------------
+// static data
+//---------------------------------
+
+const std::string
+AxisSystem::PARAMETER_TEXT[AxisSystemParamCount - CoordinateBaseParamCount] =
+{
+   "RotationMatrix",
+   "RotationDotMatrix",
+};
+
+const Gmat::ParameterType
+AxisSystem::PARAMETER_TYPE[AxisSystemParamCount - CoordinateBaseParamCount] =
+{
+   Gmat::RMATRIX33_TYPE,
+   Gmat::RMATRIX33_TYPE,
+};
+
+
+//------------------------------------------------------------------------------
+// public methods
+//------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+//  AxisSystem(const std::string &itsType,
+//             const std::string &itsName);
+//---------------------------------------------------------------------------
+/**
+ * Constructs base AxisSystem structures used in derived classes
+ * (default constructor).
+ *
+ * @param <itsType> GMAT script string associated with this type of object.
+ * @param <itsName> Optional name for the object.  Defaults to "".
+ *
+ * @note There is no parameter free constructor for AxisSystem.  Derived 
+ *       classes must pass in the typeId and typeStr parameters.
+ */
+//---------------------------------------------------------------------------
+AxisSystem::AxisSystem(const std::string &itsType,
+                       const std::string &itsName) :
+CoordinateBase(Gmat::AXIS_SYSTEM,itsType,itsName),
+rotMatrix         (NULL),
+rotDotMatrix      (NULL)
+{
+}
+
+//---------------------------------------------------------------------------
+//  AxisSystem(const AxisSystem &axisSys);
+//---------------------------------------------------------------------------
+/**
+ * Constructs base AxisSystem structures used in derived classes, by copying 
+ * the input instance (copy constructor).
+ *
+ * @param <sp>  AxisSystem instance to copy to create "this" instance.
+ */
+//---------------------------------------------------------------------------
+AxisSystem::AxisSystem(const AxisSystem &axisSys) :
+CoordinateBase(axisSys),
+rotMatrix         (axisSys.rotMatrix),
+rotDotMatrix      (axisSys.rotDotMatrix)
+{
+}
+
+//---------------------------------------------------------------------------
+//  AxisSystem& operator=(const AxisSystem &axisSys)
+//---------------------------------------------------------------------------
+/**
+ * Assignment operator for AxisSystem structures.
+ *
+ * @param <sp> The original that is being copied.
+ *
+ * @return Reference to this object
+ */
+//---------------------------------------------------------------------------
+const AxisSystem& AxisSystem::operator=(const AxisSystem &axisSys)
+{
+   if (&axisSys == this)
+      return *this;
+   CoordinateBase::operator=(axisSys);
+   rotMatrix     = axisSys.rotMatrix;
+   rotDotMatrix  = axisSys.rotDotMatrix;
+   
+   return *this;
+}
+//---------------------------------------------------------------------------
+//  ~AxisSystem(void)
+//---------------------------------------------------------------------------
+/**
+ * Destructor.
+ */
+//---------------------------------------------------------------------------
+AxisSystem::~AxisSystem()
+{
+}
+
+//---------------------------------------------------------------------------
+//  void AxisSystem::Initialize()
+//---------------------------------------------------------------------------
+/**
+ * Initialization method for this AxisSystem.
+ *
+ */
+//---------------------------------------------------------------------------
+void AxisSystem::Initialize()
+{
+   CoordinateBase::Initialize();
+}
+
+
+//------------------------------------------------------------------------------
+// public methods inherited from GmatBase
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//  std::string  GetParameterText(const Integer id) const
+//------------------------------------------------------------------------------
+/**
+ * This method returns the parameter text, given the input parameter ID.
+ *
+ * @param <id> Id for the requested parameter text.
+ *
+ * @return parameter text for the requested parameter.
+ *
+ */
+//------------------------------------------------------------------------------
+std::string AxisSystem::GetParameterText(const Integer id) const
+{
+   if (id >= CoordinateBaseParamCount && id < AxisSystemParamCount)
+      return PARAMETER_TEXT[id - CoordinateBaseParamCount];
+   return CoordinateBase::GetParameterText(id);
+}
+
+//------------------------------------------------------------------------------
+//  Integer  GetParameterID(const std::string &str) const
+//------------------------------------------------------------------------------
+/**
+ * This method returns the parameter ID, given the input parameter string.
+ *
+ * @param <str> string for the requested parameter.
+ *
+ * @return ID for the requested parameter.
+ *
+ */
+//------------------------------------------------------------------------------
+Integer AxisSystem::GetParameterID(const std::string &str) const
+{
+   for (Integer i = CoordinateBaseParamCount; i < AxisSystemParamCount; i++)
+   {
+      if (str == PARAMETER_TEXT[i - CoordinateBaseParamCount])
+         return i;
+   }
+   
+   return CoordinateBase::GetParameterID(str);
+}
+
+//------------------------------------------------------------------------------
+//  Gmat::ParameterType  GetParameterType(const Integer id) const
+//------------------------------------------------------------------------------
+/**
+ * This method returns the parameter type, given the input parameter ID.
+ *
+ * @param <id> ID for the requested parameter.
+ *
+ * @return parameter type of the requested parameter.
+ *
+ */
+//------------------------------------------------------------------------------
+Gmat::ParameterType AxisSystem::GetParameterType(const Integer id) const
+{
+   if (id >= CoordinateBaseParamCount && id < AxisSystemParamCount)
+      return PARAMETER_TYPE[id - CoordinateBaseParamCount];
+   
+   return CoordinateBase::GetParameterType(id);
+}
+
+//------------------------------------------------------------------------------
+//  std::string  GetParameterTypeString(const Integer id) const
+//------------------------------------------------------------------------------
+/**
+ * This method returns the parameter type string, given the input parameter ID.
+ *
+ * @param <id> ID for the requested parameter.
+ *
+ * @return parameter type string of the requested parameter.
+ *
+ */
+//------------------------------------------------------------------------------
+std::string AxisSystem::GetParameterTypeString(const Integer id) const
+{
+   return CoordinateBase::PARAM_TYPE_STRING[GetParameterType(id)];
+}
---
+++ src/base/coordsystem/AxisSystem.hpp
@@ -0,0 +1,133 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                                  AxisSystem
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G and MOMS Task order 124
+//
+// Author: Wendy C. Shoan/GSFC/MAB
+// Created: 2004/12/20
+//
+/**
+ * Definition of the AxisSystem class.  This is the base class for the 
+ * InertialAxes and DynamicAxes classes.
+ *
+ */
+//------------------------------------------------------------------------------
+
+#ifndef AxisSystem_hpp
+#define AxisSystem_hpp
+
+#include "gmatdefs.hpp"
+#include "GmatBase.hpp"
+#include "CoordinateBase.hpp"
+#include "Rmatrix33.hpp"
+#include "A1Mjd.hpp"
+
+class GMAT_API AxisSystem : public CoordinateBase
+{
+public:
+
+   // default constructor
+   AxisSystem(const std::string &itsType,
+              const std::string &itsName = "");
+   // copy constructor
+   AxisSystem(const AxisSystem &axisSys);
+   // operator = for assignment
+   const AxisSystem& operator=(const AxisSystem &axisSys);
+   // destructor
+   virtual ~AxisSystem();
+   
+   
+   // initilaizes the AxisSystem
+   virtual void Initialize();
+   
+   //---------------------------------------------------------------------------
+   //  bool RotateToMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+   //                        Rvector &outState, SpacePoint *j2000Body)
+   //---------------------------------------------------------------------------
+   /**
+    * This method will rotate the input inState into the MJ2000Eq frame.
+    *
+    * @param <epoch>     the epoch at which to perform the rotation.
+    * @param <inState>   the input state (in this AxisSystem) to be rotated.
+    * @param <iutState>  the output state, in the MJ2000Eq AxisSystem, the result 
+    *                    of rotating the input inState.
+    * @param <j2000Body> the origin of the output MJ2000EqAxes frame.
+    *
+    * @return success or failure of the operation.
+    */
+   //---------------------------------------------------------------------------
+   virtual bool RotateToMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+                                 Rvector &outState, SpacePoint *j2000Body) = 0;
+
+   //---------------------------------------------------------------------------
+   //  bool RotateFromMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+   //                          Rvector &outState, SpacePoint *j2000Body)
+   //---------------------------------------------------------------------------
+   /**
+    * This method will rotate the input inState from the MJ2000Eq frame into
+    * this AxisSystem.
+    *
+    * @param <epoch>     the epoch at which to perform the rotation.
+    * @param <inState>   the input state (in MJ2000Eq AxisSystem) to be rotated.
+    * @param <iutState>  the output state, in this AxisSystem, the result 
+    *                    of rotating the input inState.
+    * @param <j2000Body> the origin of the input MJ2000EqAxes frame.
+    *
+    * @return success or failure of the operation.
+    */
+   //---------------------------------------------------------------------------
+   virtual bool RotateFromMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+                                   Rvector &outState, SpacePoint *j2000Body) = 0;
+   
+   
+   // all classes derived from GmatBase must supply this Clone method;
+   // this must be implemented in the 'leaf' classes
+   //virtual GmatBase*       Clone(void) const;
+
+   // Parameter access methods - overridden from GmatBase 
+   virtual std::string     GetParameterText(const Integer id) const;     
+   virtual Integer         GetParameterID(const std::string &str) const; 
+   virtual Gmat::ParameterType
+                           GetParameterType(const Integer id) const;
+   virtual std::string     GetParameterTypeString(const Integer id) const;
+   
+   // currently, no access to RotMatrix and RotDotMatrix allowed
+   
+protected:
+
+   enum
+   {
+      ROTATION_MATRIX = CoordinateBaseParamCount,
+      ROTATION_DOT_MATRIX,
+      AxisSystemParamCount
+   };
+   
+   static const std::string PARAMETER_TEXT[AxisSystemParamCount - CoordinateBaseParamCount];
+   
+   static const Gmat::ParameterType PARAMETER_TYPE[AxisSystemParamCount - CoordinateBaseParamCount];
+   
+   //---------------------------------------------------------------------------
+   //  void CalculateRotationMatrix(SpacePoint *j2000Body)
+   //---------------------------------------------------------------------------
+   /**
+    * This method will compute the rotMatrix and rotDotMatrix used for rotations
+    * from/to this AxisSystem to/from the MJ2000EqAxes system.
+    *
+    * @param <j2000Body> the origin of the MJ2000EqAxes frame.
+    */
+   //---------------------------------------------------------------------------
+   virtual void CalculateRotationMatrix() = 0;
+   
+   /// rotation matrix
+   Rmatrix33   rotMatrix;
+   /// derivative of rotation matrix
+   Rmatrix33   rotDotMatrix;
+   
+};
+#endif // AxisSystem_hpp
