91e049eb2fd4ff17749ba69a26b5e391cc815266
wwaktola <wwaktola@222415a0-0dc2-4184-858d-a27cde375526>
2005-01-26 20:31:50 +0000

    New.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1886 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/spacecraft/TankSelectionDialog.hpp
@@ -0,0 +1,68 @@
+//$Header: 
+//------------------------------------------------------------------------------
+//                              TankSelectionDialog
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Waka Waktola
+// Created: 2005/01/26
+//
+/**
+ * Declares TankSelectionDialog class. This class shows dialog window where
+ * available tanks can be selected.
+ * 
+ */
+//------------------------------------------------------------------------------
+#ifndef TankSelectionDialog_hpp
+#define TankSelectionDialog_hpp
+
+#include "gmatwxdefs.hpp"
+#include "GmatDialog.hpp"
+#include "Spacecraft.hpp"
+
+class TankSelectionDialog : public GmatDialog
+{
+public:
+   TankSelectionDialog(wxWindow *parent, Spacecraft *spacecraft, 
+                        StringArray selectedTanks);
+   
+private:    
+   Integer selectedCount;
+   Integer availableCount;
+   
+   StringArray selectedTankNames;
+   StringArray availableTankNames;
+   
+   Spacecraft *theSpacecraft;
+   
+   wxListBox *availableListBox;
+   wxListBox *selectedListBox;
+   
+   wxButton *selectButton;
+   wxButton *removeButton;
+   
+   wxButton *selectAllButton;
+   wxButton *removeAllButton;
+   
+   // methods inherited from GmatDialog
+   virtual void Create();
+   virtual void LoadData();
+   virtual void SaveData();
+   virtual void ResetData();
+   
+   // event handling   
+   DECLARE_EVENT_TABLE();
+   void OnSelect();
+   void OnButtonClick(wxCommandEvent &event);
+   
+   // IDs for the controls and the menu commands
+   enum
+   {     
+      ID_LISTBOX = 30400,
+      ID_BUTTON,
+   };
+};
+
+#endif
+
+@
