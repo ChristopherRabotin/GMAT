a5003220602ff0a9bf47e06a3ed57a80749e375b
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2005-02-01 18:58:16 +0000

    Initial version.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@1913 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/util/LeapSecsFileReader.cpp
@@ -0,0 +1,166 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              LeapSecsFileReader
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Author: Allison R. Greene
+// Created: 2005/01/26
+//
+/**
+ * Reads time coefficent file, creates a table of coefficients and converts
+ * to the utc.
+ */
+//------------------------------------------------------------------------------
+
+#include "LeapSecsFileReader.hpp"
+#include "MessageInterface.hpp"
+#include "StringTokenizer.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+//------------------------------------------------------------------------------
+//  LeapSecsFileReader()
+//------------------------------------------------------------------------------
+/**
+ *  Constructor.
+ */
+//------------------------------------------------------------------------------
+LeapSecsFileReader::LeapSecsFileReader(std::string withFileName)
+{
+   initialized = false;
+   LoadTimeCoeffFile(withFileName);
+}
+
+//------------------------------------------------------------------------------
+//  ~LeapSecsFileReader()
+//------------------------------------------------------------------------------
+/**
+ *  Destructor.
+ */
+//------------------------------------------------------------------------------
+LeapSecsFileReader::~LeapSecsFileReader()
+{
+}
+
+//------------------------------------------------------------------------------
+//  LeapSecsFileReader(const LeapSecsFileReader& tcfr)
+//------------------------------------------------------------------------------
+/**
+ *  Copy constructor.
+ */
+//------------------------------------------------------------------------------
+LeapSecsFileReader::LeapSecsFileReader(const LeapSecsFileReader& lsfr)
+{
+}
+
+//------------------------------------------------------------------------------
+//  bool LoadTimeCoeffFile(std::string withFileName)
+//------------------------------------------------------------------------------
+/**
+ */ 
+bool LeapSecsFileReader::LoadTimeCoeffFile(std::string withFileName)
+{
+//   MessageInterface::ShowMessage("LeapSecsFileReader::LoadTimeCoeffFile()\n");
+
+   std::ifstream instream;
+   
+   instream.open(withFileName.c_str());
+
+   if (instream == NULL)
+      return false;
+
+   while (!instream.eof())
+   {
+      std::string line;
+      getline(instream,line);
+//      MessageInterface::ShowMessage("LeapSecsFileReader::LoadTimeCoeffFile %s\n",
+//                         line.c_str());
+      Parse(line);
+   }
+
+   initialized = true;
+   instream.close();
+
+//   print out of the table
+//   for (std::vector<LeapSecondInformation>::iterator i = lookUpTable.end()-1;
+//              i >= lookUpTable.begin(); i--)
+//   {
+//      MessageInterface::ShowMessage("%f\t%f\t%f\t%f\t\n", (*i).julianDate,
+//            (*i).offset1, (*i).offset2, (*i).offset3);
+//   }
+
+
+   return true;
+}
+
+//------------------------------------------------------------------------------
+// bool Parse()
+//------------------------------------------------------------------------------
+/**
+ * Parses each line to add leap second information to the table
+ * 
+ * Format of the line is: 
+ *       YYYY MMM  D =JD jDate  TAI-UTC= off1 S + (MJD - off2) X off3 S
+ * @return true if the file parses successfully
+ */
+//------------------------------------------------------------------------------
+bool LeapSecsFileReader::Parse(std::string line)
+{
+//   MessageInterface::ShowMessage("LeapSecsFileReader::Parse()\n");
+   Real jDate, off1, off2, off3;
+    
+   StringTokenizer stringToken(line," ");
+   Integer count = stringToken.CountTokens();
+   
+   if (count == 15)
+   {
+      jDate = atof(stringToken.GetToken(4).c_str());
+      off1 = atof(stringToken.GetToken(6).c_str());
+      off2 = atof(stringToken.GetToken(11).c_str());
+      off3 = atof(stringToken.GetToken(13).c_str());
+   
+      LeapSecondInformation leapSecInfo = {jDate, off1, off2, off3};
+      lookUpTable.push_back(leapSecInfo);
+      return true;
+   }
+   else
+      return false;
+
+}
+
+//------------------------------------------------------------------------------
+// Real NumberOfLeapSecondsFrom(UtcMjd *utcMjd)
+//------------------------------------------------------------------------------
+/**
+ * Converts utcmjd to utcjd and then looks it up from the table.  If file is not
+ * read, 0 is returned.
+ * 
+ * @return number of leap seconds
+ * @note Assumes that JD from table is utcjd.
+ */
+//------------------------------------------------------------------------------
+Real LeapSecsFileReader::NumberOfLeapSecondsFrom(UtcMjd utcMjd)
+{
+   if (initialized)
+   {
+      Real jd = utcMjd + GmatTimeUtil::JD_MJD_OFFSET;
+
+      for (std::vector<LeapSecondInformation>::iterator i = lookUpTable.end()-1;
+                 i >= lookUpTable.begin(); i--)
+      {
+          if (jd > (*i).julianDate)
+          {
+             return ((*i).offset1 + ((utcMjd - (*i).offset2) * (*i).offset3));
+          }
+      }
+
+      return 0;
+   }
+   else
+      return 0;
+}
---
+++ src/base/util/LeapSecsFileReader.hpp
@@ -0,0 +1,56 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              LeapSecsFileReader
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Author: Allison R. Greene
+// Created: 2005/01/26
+//
+/**
+ * Reads time coefficent file, creates a table of coefficients and converts
+ * to the utc.
+ *
+ * File found at : ftp://maia.usno.navy.mil/ser7/tai-utc.dat
+ */
+//------------------------------------------------------------------------------
+
+#ifndef LeapSecsFileReader_hpp
+#define LeapSecsFileReader_hpp
+
+#include "gmatdefs.hpp"
+#include "A1Mjd.hpp"  
+#include "TimeTypes.hpp"
+
+class LeapSecsFileReader
+{
+public:
+   LeapSecsFileReader(std::string withFileName = "tai-utc.dat");
+   virtual ~LeapSecsFileReader();
+   LeapSecsFileReader(const LeapSecsFileReader& lsfr);
+   LeapSecsFileReader& operator=(const LeapSecsFileReader& lsfr);
+        
+   bool    LoadTimeCoeffFile(std::string withFileName);
+   Real    NumberOfLeapSecondsFrom(UtcMjd utcMjd);
+
+protected:
+
+
+private:
+   bool Parse(std::string line);
+   bool initialized;
+
+   struct LeapSecondInformation
+   {
+      Real julianDate;         // arg: 2/1/05 assumed to be utc
+      Real offset1;
+      Real offset2;
+      Real offset3;
+   };    
+   
+   std::vector<LeapSecondInformation> lookUpTable;
+};
+
+#endif // LeapSecsFileReader_hpp
