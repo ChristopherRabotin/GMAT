3d138944244e3264c4d85308e6bd2eeace422806
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2005-02-11 18:51:36 +0000

    Updated for namespace.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@2001 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/util/TimeSystemConverter.hpp
+++ src/base/util/TimeSystemConverter.hpp
@@ -27,66 +27,62 @@
 #include "EopFile.hpp"
 #include "LeapSecsFileReader.hpp"
 
-class GMAT_API TimeSystemConverter 
+struct TimeSystemConverterExceptions
 {
-public:
-    class TimeSystemConverterException : public BaseException
-    {
-       public: 
-          TimeSystemConverterException(const std::string &message =
-           "TimeSystemConverterException: Can't convert due to invalid date")
-           : BaseException(message) {};
-    };
-    class ImplementationException : public BaseException
-    {
-       public:
-          ImplementationException(const std::string &message =
-           "TimeSystemConverter: Converstion not implemented: ")
-           : BaseException(message) {};
-    };
-
-    // Default constructor
-    TimeSystemConverter();
-    TimeSystemConverter(const std::string &name);
-    TimeSystemConverter(const std::string &typeStr, const std::string &name);
-    // Copy constructor
-    TimeSystemConverter(const TimeSystemConverter &TimeSystemConverter);
-    // Assignment operator
-    TimeSystemConverter& operator=(const TimeSystemConverter &TimeSystemConverter);
-
-    // Destructor
-    virtual ~TimeSystemConverter();
-
-    // public method 
-    Real Convert(const Real time,
-                        const std::string &fromTimeFormat,
-                        const std::string &toTimeFormat);
-
-    Real ConvertToTaiMjd(std::string fromTimeFormat, Real time);
-    Real ConvertFromTaiMjd(std::string toTimeFormat, Real time);
-    bool SetEopFile(EopFile *eopFile);
-    bool SetLeapSecsFileReader(LeapSecsFileReader *leapSecsFileReader);
-
-protected:
-   // Declare protetced method data
-
-private:
-   static const std::string TIME_SYSTEM_TEXT[];
-   Real jdOffset;
-   EopFile *theEopFile;
-   LeapSecsFileReader *theLeapSecsFileReader;
-
-   static const Real TDB_COEFF1;
-   static const Real TDB_COEFF2;
-   static const Real M_E_OFFSET;
-   static const Real M_E_COEFF1;
-   static const Real T_TT_OFFSET;
-   static const Real T_TT_COEFF1;
-   static const Real L_B; 
-   static const Real TCB_JD_MJD_OFFSET;
-   static const Real NUM_SECS;
-
+   class ImplementationException : public BaseException
+   {
+      public:
+         ImplementationException(const std::string &message =
+         "TimeSystemConverter: Converstion not implemented: ")
+         : BaseException(message) {};
+   };
+   class FileException : public BaseException
+   {
+      public:
+         FileException(const std::string &message =
+         "TimeSystemConverter: File is unknown: ")
+         : BaseException(message) {};
+   };
 
 };
+
+namespace TimeConverterUtil
+{
+   // Specified in Math Spec section 2.3
+   static const Real TDB_COEFF1                    = 0.001658;
+   static const Real TDB_COEFF2                    = 0.00001385;
+   static const Real M_E_OFFSET                    = 357.5277233;
+   static const Real M_E_COEFF1                    = 35999.05034;
+   static const Real T_TT_OFFSET                   = 2451545.0;
+   static const Real T_TT_COEFF1                   = 36525;
+   static const Real L_B                           = 1.550505e-8;
+   static const Real TCB_JD_MJD_OFFSET             = 2443144.5;
+   static const Real NUM_SECS                      = 86400;
+
+   static const std::string TIME_SYSTEM_TEXT[6] =
+         {
+            "A1Mjd",
+            "UtcMjd",
+            "Ut1Mjd",
+            "TdbMjd",
+            "TcbMjd",
+            "TtMjd",
+         };
+
+   static EopFile *theEopFile;
+   static LeapSecsFileReader *theLeapSecsFileReader;
+
+   Real Convert(const Real origValue,
+                      const std::string &fromType,
+                      const std::string &toType);
+
+   Real ConvertToTaiMjd(std::string fromType, Real origValue,
+      Real refJd= GmatTimeUtil::JD_NOV_17_1858);
+   Real ConvertFromTaiMjd(std::string toType, Real origValue,
+      Real refJd= GmatTimeUtil::JD_NOV_17_1858);
+
+   bool SetEopFile(EopFile *eopFile);
+   bool SetLeapSecsFileReader(LeapSecsFileReader *leapSecsFileReader);
+}
 
 #endif // TimeSystemConverter_hpp
