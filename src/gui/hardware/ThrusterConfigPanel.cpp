8d55aa246886b869a01ca011d373c3866dec094f
wwaktola <wwaktola@222415a0-0dc2-4184-858d-a27cde375526>
2005-02-17 21:45:28 +0000

    New panel.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@2055 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/hardware/ThrusterConfigPanel.cpp
@@ -0,0 +1,181 @@
+//$Header:
+//------------------------------------------------------------------------------
+//                            ThrusterConfigPanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P.
+//
+//
+// Author: Waka Waktola
+// Created: 2005/01/06
+/**
+ * This class contains information needed to setup users spacecraft thruster 
+ * parameters.
+ */
+//------------------------------------------------------------------------------
+#include "ThrusterConfigPanel.hpp"
+#include "ThrusterCoefficientDialog.hpp"
+#include "TankSelectionDialog.hpp"
+#include "MessageInterface.hpp"
+
+//------------------------------
+// event tables for wxWindows
+//------------------------------
+BEGIN_EVENT_TABLE(ThrusterConfigPanel, wxPanel)
+   EVT_TEXT(ID_TEXTCTRL, ThrusterConfigPanel::OnTextChange)
+   EVT_BUTTON(ID_BUTTON, ThrusterConfigPanel::OnButtonClick)
+END_EVENT_TABLE()
+
+//------------------------------
+// public methods
+//------------------------------
+
+//------------------------------------------------------------------------------
+// ThrusterConfigPanel(wxWindow *parent, const wxString &thrusterName)
+//------------------------------------------------------------------------------
+/**
+ * Constructs ThrusterConfigPanel object.
+ */
+//------------------------------------------------------------------------------
+ThrusterConfigPanel::ThrusterConfigPanel(wxWindow *parent, 
+                                  const wxString &thrusterName):GmatPanel(parent)
+{
+    coordsysCount = 0;
+    
+    Create();
+    Show();
+}
+
+//------------------------------------------------------------------------------
+// ~ThrusterConfigPanel()
+//------------------------------------------------------------------------------
+ThrusterConfigPanel::~ThrusterConfigPanel()
+{
+}
+
+//-------------------------------
+// private methods
+//-------------------------------
+
+//----------------------------------
+// methods inherited from GmatPanel
+//----------------------------------
+
+//------------------------------------------------------------------------------
+// void Create()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::Create()
+{
+    // Integer
+    Integer bsize = 3; // border size
+    
+    // wxString
+    wxString emptyList[] = {};
+    
+    // wxButton
+    cCoefButton = new wxButton( this, ID_BUTTON, wxT("Edit Thruster Coef."),
+                              wxDefaultPosition, wxDefaultSize, 0 );
+    kCoefButton = new wxButton( this, ID_BUTTON, wxT("Edit Impulse Coef."),
+                              wxDefaultPosition, wxDefaultSize, 0 );  
+                                
+    // wxComboBox 
+    coordsysComboBox = new wxComboBox( this, ID_COMBO, wxT(""),
+                      wxDefaultPosition, wxSize(125,-1), coordsysCount,
+                      emptyList, wxCB_DROPDOWN|wxCB_READONLY );;
+   
+    // wxTextCtrl
+    XTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                            wxDefaultPosition, wxSize(80,-1), 0 );
+    YTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                            wxDefaultPosition, wxSize(80,-1), 0 );
+    ZTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                            wxDefaultPosition, wxSize(80,-1), 0 );
+                            
+    //wxStaticText
+    coordsysStaticText = new wxStaticText( this, ID_TEXT, 
+                            wxT("Coordinate System"), wxDefaultPosition,
+                            wxDefaultSize, 0);
+    XStaticText = new wxStaticText( this, ID_TEXT, wxT("X Direction"),
+                            wxDefaultPosition,wxDefaultSize, 0);
+    YStaticText = new wxStaticText( this, ID_TEXT, wxT("Y Direction"),
+                            wxDefaultPosition,wxDefaultSize, 0);  
+    ZStaticText = new wxStaticText( this, ID_TEXT, wxT("Z Direction"),
+                            wxDefaultPosition,wxDefaultSize, 0); 
+    
+    // wx*Sizers                                           
+    wxFlexGridSizer *flexGridSizer1 = new wxFlexGridSizer( 2, 0, 0 );
+    
+    // Add to wx*Sizers 
+    flexGridSizer1->Add(coordsysStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
+    flexGridSizer1->Add(coordsysComboBox, 0, wxALIGN_LEFT|wxALL, bsize );
+    flexGridSizer1->Add(XStaticText, 0, wxALIGN_CENTRE|wxALL, bsize );
+    flexGridSizer1->Add(XTextCtrl, 0, wxALIGN_LEFT|wxALL, bsize );
+    flexGridSizer1->Add(YStaticText, 0, wxALIGN_CENTRE|wxALL, bsize );
+    flexGridSizer1->Add(YTextCtrl, 0, wxALIGN_LEFT|wxALL, bsize );
+    flexGridSizer1->Add(ZStaticText, 0, wxALIGN_CENTRE|wxALL, bsize );
+    flexGridSizer1->Add(ZTextCtrl, 0, wxALIGN_LEFT|wxALL, bsize );
+    flexGridSizer1->Add( 20, 20, 0, wxALIGN_CENTRE|wxALL, bsize);
+    flexGridSizer1->Add( 20, 20, 0, wxALIGN_CENTRE|wxALL, bsize);
+    flexGridSizer1->Add( 20, 20, 0, wxALIGN_CENTRE|wxALL, bsize);
+    flexGridSizer1->Add( 20, 20, 0, wxALIGN_CENTRE|wxALL, bsize);
+    flexGridSizer1->Add(cCoefButton, 0, wxALIGN_CENTRE|wxALL, bsize );
+    flexGridSizer1->Add(kCoefButton, 0, wxALIGN_LEFT|wxALL, bsize );
+    
+    theMiddleSizer->Add(flexGridSizer1, 0, wxGROW, bsize);
+}
+
+//------------------------------------------------------------------------------
+// void LoadData()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::LoadData()
+{  
+    DisplayData();
+}
+
+//------------------------------------------------------------------------------
+// void SaveData()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::SaveData()
+{
+}
+
+//------------------------------------------------------------------------------
+// void DisplayData()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::DisplayData()
+{ 
+}
+
+//------------------------------------------------------------------------------
+// void OnTextChange()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::OnTextChange()
+{
+    theApplyButton->Enable();
+} 
+
+//------------------------------------------------------------------------------
+// void OnButtonClick()
+//------------------------------------------------------------------------------
+void ThrusterConfigPanel::OnButtonClick(wxCommandEvent &event)
+{  
+    if (event.GetEventObject() == cCoefButton)
+    {
+       std::string type = "C";
+       
+       //ThrusterCoefficientDialog tcDlg(this, thrusters[currentThruster]->thruster, type);
+       //tcDlg.ShowModal();       
+    } 
+    else if (event.GetEventObject() == kCoefButton)
+    {
+       std::string type = "K";
+       
+       //ThrusterCoefficientDialog tcDlg(this, thrusters[currentThruster]->thruster, type);
+       //tcDlg.ShowModal();
+    }            
+}    
---
+++ src/gui/hardware/ThrusterConfigPanel.hpp
@@ -0,0 +1,79 @@
+//$Header:
+//------------------------------------------------------------------------------
+//                            ThrusterConfigPanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number NNG04CC06P.
+//
+//
+// Author: Waka Waktola
+// Created: 2005/01/06
+/**
+ * This class contains information needed to setup users spacecraft thruster 
+ * parameters.
+ */
+//------------------------------------------------------------------------------
+#ifndef ThrusterConfigPanel_hpp
+#define ThrusterConfigPanel_hpp
+
+#include "gmatwxdefs.hpp"
+#include "GmatPanel.hpp"
+#include "GuiInterpreter.hpp"
+
+#include "gmatdefs.hpp"
+#include "Spacecraft.hpp"
+#include "Thruster.hpp"
+
+class ThrusterConfigPanel: public GmatPanel
+{
+public:
+    ThrusterConfigPanel(wxWindow *parent, const wxString &thrusterName);
+    ~ThrusterConfigPanel();
+    
+private:
+    // methods inherited from GmatPanel
+    virtual void Create();
+    virtual void LoadData();
+    virtual void SaveData();
+    
+    // Event Handling
+    DECLARE_EVENT_TABLE();
+    void DisplayData();
+    void OnTextChange();
+    void OnButtonClick(wxCommandEvent &event);
+    
+    std::string thrusterName;
+    
+    Integer coordsysCount;
+    
+    wxButton *cCoefButton;
+    wxButton *kCoefButton;
+    
+    wxComboBox *coordsysComboBox;
+    
+    wxTextCtrl *XTextCtrl;
+    wxTextCtrl *YTextCtrl;
+    wxTextCtrl *ZTextCtrl;
+    
+    wxStaticText *coordsysStaticText;
+    wxStaticText *XStaticText;
+    wxStaticText *YStaticText;
+    wxStaticText *ZStaticText;
+        
+    // IDs for the controls and the menu commands
+    enum
+    {     
+        ID_TEXT = 30250,
+        ID_TEXTCTRL,
+        ID_LISTBOX,
+        ID_BUTTON,
+        ID_COMBO,
+    };
+};
+#endif
+
