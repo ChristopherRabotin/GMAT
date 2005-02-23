0a9b90d28c97b5115904faa62d6989f08f3b3ec0
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2005-02-23 17:25:55 +0000

    Initial version.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@2077 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/gui/command/ScriptEventPanel.cpp
@@ -0,0 +1,111 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              ScriptEventPanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Allison Greene
+// Created: 2005/1/12
+//
+/**
+ * Implements ScriptEventPanel class.
+ */
+//------------------------------------------------------------------------------
+
+#include "ScriptEventPanel.hpp"
+#include "MessageInterface.hpp"
+
+
+//#define DEBUG_PARAM_PANEL 1
+
+//------------------------------------------------------------------------------
+// event tables and other macros for wxWindows
+//------------------------------------------------------------------------------
+
+BEGIN_EVENT_TABLE(ScriptEventPanel, GmatPanel)
+   EVT_BUTTON(ID_BUTTON_OK, GmatPanel::OnOK)
+   EVT_BUTTON(ID_BUTTON_APPLY, GmatPanel::OnApply)
+   EVT_BUTTON(ID_BUTTON_CANCEL, GmatPanel::OnCancel)
+   EVT_BUTTON(ID_BUTTON_SCRIPT, GmatPanel::OnScript)
+   
+   EVT_TEXT(ID_TEXTCTRL, ScriptEventPanel::OnTextUpdate)
+END_EVENT_TABLE()
+
+//------------------------------------------------------------------------------
+// ScriptEventPanel()
+//------------------------------------------------------------------------------
+/**
+ * A constructor.
+ */
+//------------------------------------------------------------------------------
+ScriptEventPanel::ScriptEventPanel(wxWindow *parent, GmatCommand *cmd)
+   : GmatPanel(parent)
+{
+   theCommand = cmd;
+
+   if (theCommand != NULL)
+   {
+     Create();
+     Show();
+   }
+}
+
+//------------------------------------------------------------------------------
+// void Create()
+//------------------------------------------------------------------------------
+void ScriptEventPanel::Create()
+{
+   int bsize = 3; // border size
+   
+    // create sizers
+   mBottomSizer = new wxGridSizer( 1, 0, 0 );
+   mPageSizer = new wxBoxSizer(wxVERTICAL);
+
+
+   mFileContentsTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""),
+                            wxDefaultPosition, wxDefaultSize,
+                            wxTE_MULTILINE | wxGROW);
+
+
+   //------------------------------------------------------
+   // add to sizer
+   //------------------------------------------------------
+
+   mBottomSizer->Add(mFileContentsTextCtrl, 0, wxGROW | wxALIGN_CENTER | wxALL, 
+                     bsize);
+
+   //------------------------------------------------------
+   // add to parent sizer
+   //------------------------------------------------------
+   mPageSizer->Add(mBottomSizer, 1, wxGROW | wxALIGN_CENTER | wxALL, bsize);
+   theMiddleSizer->Add(mPageSizer, 1, wxGROW | wxALIGN_CENTER | wxALL, bsize);
+}
+
+//------------------------------------------------------------------------------
+// void LoadData()
+//------------------------------------------------------------------------------
+void ScriptEventPanel::LoadData()
+{
+   wxString scriptText = theCommand->GetGeneratingString().c_str();
+   mFileContentsTextCtrl->AppendText(scriptText);
+
+   theApplyButton->Disable();
+}
+
+//------------------------------------------------------------------------------
+// void SaveData()
+//------------------------------------------------------------------------------
+void ScriptEventPanel::SaveData()
+{
+//   std::string scriptText = mFileContentsTextCtrl->GetValue().c_str();
+//   theCommand->SetStringParameter("ScriptText", scriptText);
+}
+
+//------------------------------------------------------------------------------
+// void OnTextUpdate(wxCommandEvent& event)
+//------------------------------------------------------------------------------
+void ScriptEventPanel::OnTextUpdate(wxCommandEvent& event)
+{
+   theApplyButton->Enable();
+}
+
---
+++ src/gui/command/ScriptEventPanel.hpp
@@ -0,0 +1,58 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                              ScriptEventPanel
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// Author: Allison Greene
+// Created: 2005/1/12
+//
+/**
+ * Declares ScriptEventPanel class.
+ */
+//------------------------------------------------------------------------------
+
+#ifndef ScriptEventPanel_hpp
+#define ScriptEventPanel_hpp
+
+#include "GmatPanel.hpp"
+#include "Parameter.hpp"
+
+class ScriptEventPanel: public GmatPanel
+{
+public:
+   // constructors
+   ScriptEventPanel(wxWindow *parent, GmatCommand *cmd);
+    
+private:
+   // member data
+   GmatCommand *theCommand;
+      
+   wxGridSizer *mBottomSizer;
+   wxBoxSizer *mPageSizer;
+
+   wxTextCtrl *mFileContentsTextCtrl;
+    
+   // methods inherited from GmatPanel
+   virtual void Create();
+   virtual void LoadData();
+   virtual void SaveData();
+    
+   // event handling
+   void OnTextUpdate(wxCommandEvent& event);
+
+   DECLARE_EVENT_TABLE();
+
+   // IDs for the controls and the menu commands
+   enum
+   {     
+      ID_TEXT = 9000,
+      ID_LISTBOX,
+      ID_BUTTON,
+      ID_COLOR_BUTTON,
+      ID_COMBO,
+      ID_TEXTCTRL
+   };
+};
+
+#endif
