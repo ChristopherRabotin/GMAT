fa1299dde49579e72ef99ea2caab0ba42ac33b0c
wshoan <wshoan@222415a0-0dc2-4184-858d-a27cde375526>
2005-02-25 15:38:19 +0000

    header for EquatorAxes class
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@2112 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/coordsystem/EquatorAxes.hpp
@@ -0,0 +1,79 @@
+//$Header$
+//------------------------------------------------------------------------------
+//                                  EquatorAxes
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool.
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under 
+// MOMS Task order 124.
+//
+// Author: Wendy C. Shoan/GSFC/MAB
+// Created: 2005/02/24
+//
+/**
+ * Definition of the EquatorAxes class.
+ *
+ */
+//------------------------------------------------------------------------------
+
+#ifndef EquatorAxes_hpp
+#define EquatorAxes_hpp
+
+#include "gmatdefs.hpp"
+#include "GmatBase.hpp"
+#include "AxisSystem.hpp"
+#include "DynamicAxes.hpp"
+
+class GMAT_API EquatorAxes : public DynamicAxes
+{
+public:
+
+   // default constructor
+   EquatorAxes(const std::string &itsName = "");
+   // copy constructor
+   EquatorAxes(const EquatorAxes &eqAxes);
+   // operator = for assignment
+   const EquatorAxes& operator=(const EquatorAxes &eqAxes);
+   // destructor
+   virtual ~EquatorAxes();
+      
+   // method to initialize the data
+   virtual void Initialize(); 
+   
+   // methods to convert to/from the MJ2000 Equatorial axis system
+   virtual bool RotateToMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+                                 Rvector &outState); 
+   virtual bool RotateFromMJ2000Eq(const A1Mjd &epoch, const Rvector &inState,
+                                   Rvector &outState); 
+
+   
+   // all classes derived from GmatBase must supply this Clone method;
+   // this must be implemented in the 'leaf' classes
+   virtual GmatBase*       Clone(void) const;
+
+   // Parameter access methods - overridden from GmatBase
+   //virtual std::string     GetParameterText(const Integer id) const;     
+   //virtual Integer         GetParameterID(const std::string &str) const; 
+   //virtual Gmat::ParameterType
+   //                        GetParameterType(const Integer id) const;
+   //virtual std::string     GetParameterTypeString(const Integer id) const;
+   
+protected:
+
+   enum
+   {
+      EquatorAxesParamCount = DynamicAxesParamCount,
+   };
+   
+   //static const std::string PARAMETER_TEXT[EquatorAxesParamCount - 
+   //                                        DynamicAxesParamCount];
+   
+   //static const Gmat::ParameterType PARAMETER_TYPE[EquatorAxesParamCount - 
+   //                                                DynamicAxesParamCount];
+   
+   virtual void CalculateRotationMatrix(const A1Mjd &atEpoch);
+
+};
+#endif // EquatorAxes_hpp
