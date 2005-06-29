988ca523ce17c78f5229b92d4bc9e1795125e24d
jgurgan <jgurgan@222415a0-0dc2-4184-858d-a27cde375526>
2005-06-29 17:55:34 +0000

    Changed back to the original (1.27)
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@2600 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/spacecraft/Spacecraft.hpp
+++ src/base/spacecraft/Spacecraft.hpp
@@ -24,12 +24,12 @@
 #include "SpaceObject.hpp"
 #include "CoordinateSystem.hpp"
 #include "Rvector6.hpp"
-#include "Epoch.hpp"
-#include "StateVector.hpp"
+#include "StateConverter.hpp"
 #include "TimeConverter.hpp"
 #include "PropState.hpp"
 #include "FuelTank.hpp"
 #include "Thruster.hpp"
+#include "Anomaly.hpp"
 
 class GMAT_API Spacecraft : public SpaceObject
 {
@@ -96,11 +96,34 @@ public:
 
    //    virtual PropState& GetState(void); 
    // @todo will work on GetStateVector()
-   Rvector6 GetStateVector() const;
-   Rvector6 GetStateVector(const std::string &elementType) const;
-   bool     SetStateVector(const Rvector6 instate);
-   bool     SetStateVector(const std::string &elementType,
-                           const Rvector6 instate);
+   // Rvector6 GetStateVector(const std::string &elementType);
+
+   void SetState(const std::string &elementType, Real *instate);
+   void SetState(const Real s1, const Real s2, const Real s3, 
+                 const Real s4, const Real s5, const Real s6);
+
+   Rvector6 GetCartesianState();
+   Rvector6 GetKeplerianState();
+   Rvector6 GetModifiedKeplerianState();
+   // Will add more methods below later
+   // Rvector6 GetSphericalOneState() const;
+   // Rvector6 GetSphericalTwoState() const;
+
+   bool GetDisplay() const;
+   void SetDisplay(const bool displayFlag);
+
+   std::string GetDisplayDateFormat() const;
+   void SetDisplayDateFormat(const std::string &dateType);
+   std::string GetDisplayEpoch();
+   bool SetDisplayEpoch(const std::string &value);
+
+   std::string GetDisplayCoordType() const;
+   void SetDisplayCoordType(const std::string &coordType);
+
+   Real* GetDisplayState();
+   void SetDisplayState(const Real *s);
+   void SetDisplayState(const Rvector6 s);
+   void SaveDisplay();
 
 protected:
    enum SC_Param_ID 
@@ -109,7 +132,7 @@ protected:
       ELEMENT1_ID = SpaceObjectParamCount, ELEMENT2_ID, ELEMENT3_ID, 
       ELEMENT4_ID, ELEMENT5_ID, ELEMENT6_ID, 
       STATE_TYPE_ID, ANOMALY_ID, COORD_SYS_ID,
-      DRY_MASS_ID, CD_ID, CR_ID, DRAG_AREA_ID, SRP_AREA_ID,
+      DRY_MASS_ID,DATE_FORMAT_ID, CD_ID, CR_ID, DRAG_AREA_ID, SRP_AREA_ID,
       FUEL_TANK_ID, THRUSTER_ID, TOTAL_MASS_ID, 
       SC_ParamCount
    };
@@ -123,16 +146,32 @@ protected:
    // DJC:  7/21/04 Update for the state vector used in propagation
    // Real           state[6];
    // PropState      state;      // Moved to SpaceObject
-//   Epoch             epoch;
-   StateVector       stateVector;
    Real              dryMass;
    Real              coeffDrag;
    Real              dragArea;
    Real              srpArea;
    Real              reflectCoeff;
+   std::string       dateFormat;
+   std::string       stateType;
+   Anomaly           anomaly;
    CoordinateSystem  *coordinateSystem;
    std::string       coordSysName;
 
+   // for non-internal spacecraft information
+   StateConverter stateConverter;
+   TimeConverter  timeConverter;
+   Rvector6       cartesianState;
+   Rvector6       keplerianState;
+   Rvector6       modifiedKeplerianState;
+   Rvector6       sphericalOneState;
+   Rvector6       sphericalTwoState;
+   bool           isForDisplay;
+   std::string    displayEpoch;
+   Real           displayState[6];
+   std::string    displayDateFormat;
+   std::string    displayCoordType; 
+   std::string    displaySubType;
+   
    // Lists of hardware elements added 11/12/04, djc
    /// Fuel tank names
    StringArray    tankNames;
@@ -151,9 +190,17 @@ protected:
    Real           UpdateTotalMass() const;
    
 private:
-   // private methods
-   void        SetPropState();
+   bool        initialDisplay;
+   bool        hasElements[6];
 
+   // private methods
+   void        SetEpoch();
+   std::string GetElementName(const Integer id) const; 
+   std::string GetLocalCoordType() const;
+   void        SetInitialDisplay();
+   std::string ToString(const Real value);
+       
+   void        SetInitialState();
    void        DefineDefaultSpacecraft(); //loj: 4/28/05 Added
    void        InitializeDataMethod(const Spacecraft &s);
 };
