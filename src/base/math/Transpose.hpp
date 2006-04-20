f65ec8d4a648e0ac5f7a8fad2f3dbd16090c6e3d
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2006-04-20 14:32:18 +0000

    Initial version.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@3424 222415a0-0dc2-4184-858d-a27cde375526

---
+++ src/base/math/Transpose.hpp
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------------
+//                              Transpose
+//------------------------------------------------------------------------------
+// GMAT: Goddard Mission Analysis Tool
+//
+// **Legal**
+//
+// Developed jointly by NASA/GSFC and Thinking Systems, Inc. under contract
+// number S-67573-G
+//
+// Author: Allison Greene
+// Created: 2006/04/20
+//
+/**
+ * Declares Transpose class.
+ */
+//------------------------------------------------------------------------------
+#ifndef Transpose_hpp
+#define Transpose_hpp
+
+#include "MathFunction.hpp"
+
+class GMAT_API Transpose : public MathFunction
+{
+public:
+   Transpose(const std::string &nomme);
+   virtual ~Transpose();
+   Transpose(const Transpose &copy);
+                 
+   // inherited from GmatBase
+   virtual GmatBase* Clone() const;
+
+   virtual Rmatrix MatrixEvaluate();
+
+protected:
+
+  
+private:
+   
+};
+
+#endif // Transpose_hpp
