1e7f0e0199375933d197112833689cff9360bc9b
agreene <agreene@222415a0-0dc2-4184-858d-a27cde375526>
2006-05-23 19:49:24 +0000

    Updated to fix bugs 291 and 300.  ModifiedJulianDate was changed to compute the Julian days, subtract out the Julian offset, and then add in the fraction of days so that more significant figures could be used.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@3521 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/util/DateUtil.cpp
+++ src/base/util/DateUtil.cpp
@@ -107,9 +107,22 @@ Real JulianDate(YearNumber year, MonthOfYear month, DayOfMonth day,
 Real ModifiedJulianDate(YearNumber year, MonthOfYear month, DayOfMonth day, 
                         Integer hour, Integer minute, Real second)
 {
-   Real jd =  JulianDate(year, month, day, hour, minute, second);
+    // 5/23/06 - commented out
+    // Need to subtract out Julian date offset before adding the fraction
+    // of a day term to gain significant digits after the decimal place.
+    // The computation is copied from the Julian Date method.
+//   Real jd =  JulianDate(year, month, day, hour, minute, second);
+//   return (jd - GmatTimeUtil::JULIAN_DATE_OF_010541);
 
-   return (jd - GmatTimeUtil::JULIAN_DATE_OF_010541);
+   Integer computeYearMon = ( 7*(year + (Integer)(month + 9)/12 ) )/4;
+   Integer computeMonth = (275 * month)/9;
+   Real fractionalDay = ((second/60.0 + minute)/60.0 + hour)/24.0;
+
+   Real ModJulianDay = 367*year - computeYearMon + computeMonth + day +
+               1721013.5 -  GmatTimeUtil::JULIAN_DATE_OF_010541;
+   Real modJulianDate = ModJulianDay  + fractionalDay;
+
+   return modJulianDate;
 }
 
                 
