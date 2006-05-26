c660b45fe6b6a49d424a674172f49e60ecb42caf
lojun <lojun@222415a0-0dc2-4184-858d-a27cde375526>
2006-05-26 18:43:56 +0000

    added FindFirstAndLast(), FindParenMatch()
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@3536 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/util/StringUtil.cpp
+++ src/base/util/StringUtil.cpp
@@ -19,6 +19,7 @@
 
 #include "StringUtil.hpp"
 #include "MessageInterface.hpp"
+#include "GmatBaseException.hpp"
 
 //#define DEBUG_STRING_UTIL 1
 
@@ -26,19 +27,23 @@ using namespace std;
 
 
 //------------------------------------------------------------------------------
-// std::string RemoveAll(const std::string &str, char ch)
+// std::string RemoveAll(const std::string &str, char ch, Integer start = 0)
 //------------------------------------------------------------------------------
-std::string GmatStringUtil::RemoveAll(const std::string &str, char ch)
+std::string GmatStringUtil::RemoveAll(const std::string &str, char ch,
+                                      Integer start)
 {
    std::string str2 = str;
 
    std::string::iterator iter = str2.begin();
+   for (int i=0; i<start; i++)
+      iter++;
+   
    while (iter != str2.end())
    {
-      if (*iter == ' ')
+      if (*iter == ch)
          str2.erase(iter);
-      
-      ++iter;
+      else
+         ++iter;
    }
 
    return str2;
@@ -46,9 +51,11 @@ std::string GmatStringUtil::RemoveAll(const std::string &str, char ch)
 
 
 //------------------------------------------------------------------------------
-// std::string Trim(const std::string &str, StripType stype = TRAILING)
+// std::string Trim(const std::string &str, StripType stype = TRAILING,
+//                  bool removeSemicolon = false)
 //------------------------------------------------------------------------------
-std::string GmatStringUtil::Trim(const std::string &str, StripType stype)
+std::string GmatStringUtil::Trim(const std::string &str, StripType stype,
+                                 bool removeSemicolon)
 {
    std::string str2;
 
@@ -70,6 +77,10 @@ std::string GmatStringUtil::Trim(const std::string &str, StripType stype)
       ("==> index1=%d, index2=%d str=<%s>, str2=<%s>\n", index1, index2,
        str.c_str(), str2.c_str());
    #endif
+
+   if (removeSemicolon)
+      if (str2[str2.size()-1] == ';')
+         str2.erase(str2.size()-1, 1);
    
    return str2;
 }
@@ -182,10 +193,10 @@ std::string GmatStringUtil::Capitalize(const std::string &str)
 
 
 //------------------------------------------------------------------------------
-// void ParseParameter(const std::string str, std::string &type,
+// void ParseParameter(const std::string &str, std::string &type,
 //                     std::string &owner, std::string &depobj)
 //------------------------------------------------------------------------------
-void GmatStringUtil::ParseParameter(const std::string str, std::string &type,
+void GmatStringUtil::ParseParameter(const std::string &str, std::string &type,
                                     std::string &owner, std::string &depObj)
 {
    //find owner.depObj.type
@@ -200,7 +211,7 @@ void GmatStringUtil::ParseParameter(const std::string str, std::string &type,
 
       depObj = "";
       if (pos2 > pos1)
-         depObj = str1.substr(pos1+1, pos2);
+         depObj = str1.substr(pos1+1, pos2-pos1-1);
    }
 
    #if DEBUG_STRING_UTIL
@@ -208,6 +219,99 @@ void GmatStringUtil::ParseParameter(const std::string str, std::string &type,
       ("GmatStringUtil::ParseParameter() str=%s, type=%s, owner=%s, depObj=%s\n",
        str.c_str(), type.c_str(), owner.c_str(), depObj.c_str());
    #endif
+   
+}
+
+
+//------------------------------------------------------------------------------
+// void GetArrayIndex(const std::string &str, Integer &row, Integer &col,
+//                    std::string &name)
+//------------------------------------------------------------------------------
+void GmatStringUtil::GetArrayIndex(const std::string &str, Integer &row,
+                                   Integer &col, std::string &name)
+{
+   #if DEBUG_STRING_UTIL
+   MessageInterface::ShowMessage
+      ("Report::GetArrayIndex() str=%s\n", str.c_str());
+   #endif
+   
+   row = -1;
+   col = -1;
+   
+   // Handle Array indexing
+   UnsignedInt openParen = str.find('(');
+   if (openParen != str.npos)
+   {
+      UnsignedInt comma = str.find(',');
+      if (comma == str.npos)
+         throw GmatBaseException("Expecting \",\" for Array " + str);
+         
+      UnsignedInt closeParen = str.find(')');
+      if (closeParen == str.npos)
+         throw GmatBaseException("Expecting \")\" for Array " + str);
+         
+      std::string rowStr = str.substr(openParen+1, comma-openParen-1);
+      std::string colStr = str.substr(comma+1, closeParen-comma-1);
+
+      // array index starts at 0
+      row = atoi(rowStr.c_str()) - 1;
+      col = atoi(colStr.c_str()) - 1;
+   }
+
+   name = str.substr(0, openParen);
+   
+   #if DEBUG_STRING_UTIL
+   MessageInterface::ShowMessage
+      ("Report::GetArrayIndex() row=%d, col=%d, name=%s\n", row, col, name.c_str());
+   #endif
+}
+
+
+//------------------------------------------------------------------------------
+// void FindFirstAndLast(const std::string &str, char ch, Integer &first,
+//                       Integer &last)
+//------------------------------------------------------------------------------
+/*
+ * Finds first and last index of given chracter from the string.
+ * if given chracter is not found it sets to -1
+ */
+//------------------------------------------------------------------------------
+void GmatStringUtil::FindFirstAndLast(const std::string &str, char ch,
+                                      Integer &first, Integer &last)
+{
+   first = str.find_first_of(ch);
+   if (first == (Integer)str.npos)
+      first = -1;
+
+   last = str.find_last_of(ch);
+   if (last == (Integer)str.npos)
+      last = -1;
+}
+
+
+//------------------------------------------------------------------------------
+// void FindParenMatch(const std::string &str, Integer &openParen,
+//                     Integer &closePardn)
+//------------------------------------------------------------------------------
+/*
+ * Finds open and close parenthesis index from the string.
+ * if parhenthesis is not found it sets to -1
+ */
+//------------------------------------------------------------------------------
+void GmatStringUtil::FindParenMatch(const std::string &str, Integer &openParen,
+                                    Integer &closeParen, bool &isOuterParen)
+{
+   openParen = str.find_first_of('(');
+   if (openParen == (Integer)str.npos)
+      openParen = -1;
+
+   closeParen = str.find_last_of(')');
+   if (closeParen == (Integer)str.npos)
+      closeParen = -1;
+   
+   isOuterParen = false;
+   if (openParen != -1 && closeParen == (Integer)str.size()-1)
+      isOuterParen = true;
    
 }
 
--- src/base/util/StringUtil.hpp
+++ src/base/util/StringUtil.hpp
@@ -29,17 +29,24 @@ namespace GmatStringUtil
       TRAILING = 2,
       BOTH = 3,
    };
-   
-   std::string RemoveAll(const std::string &str, char ch);
-   std::string Trim(const std::string &str, StripType stype = TRAILING);
+      
+   std::string RemoveAll(const std::string &str, char ch, Integer start = 0);
+   std::string Trim(const std::string &str, StripType stype = TRAILING,
+                    bool removeSemicolon = false);
    std::string Strip(const std::string &str, StripType stype = TRAILING);
    bool ToDouble(const std::string &str, Real *value);
    std::string ToUpper(const std::string &str);
    std::string ToLower(const std::string &str);
    std::string Capitalize(const std::string &str);
 
-   void ParseParameter(const std::string str, std::string &type,
+   void ParseParameter(const std::string &str, std::string &type,
                        std::string &owner, std::string &depObj);
+   void GetArrayIndex(const std::string &str, Integer &row, Integer &col,
+                      std::string &name);
+   void FindFirstAndLast(const std::string &str, char ch, Integer &first,
+                         Integer &last);
+   void FindParenMatch(const std::string &str, Integer &open, Integer &close,
+                       bool &isOuterParen);
 }
 
 #endif // StringUtil_hpp
