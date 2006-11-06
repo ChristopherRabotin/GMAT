3c50212dba1f20e53910d8eb1878250d55e567a3
lruley <lruley@222415a0-0dc2-4184-858d-a27cde375526>
2006-11-06 02:25:35 +0000

    Fixed error messages.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@4062 222415a0-0dc2-4184-858d-a27cde375526

--- src/gui/hardware/TankConfigPanel.cpp
+++ src/gui/hardware/TankConfigPanel.cpp
@@ -77,78 +77,88 @@ TankConfigPanel::~TankConfigPanel()
 //------------------------------------------------------------------------------
 void TankConfigPanel::Create()
 {    
-   // wxTextCtrl
-   temperatureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-   refTemperatureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-   fuelMassTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-   fuelDensityTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-   pressureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-   volumeTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
-                            wxDefaultPosition, wxSize(80,-1), 0 );
-    
-   //wxStaticText
-   temperatureStaticText = new wxStaticText( this, ID_TEXT, wxT("Temperature"),
-                            wxDefaultPosition,wxDefaultSize, 0);
-   refTemperatureStaticText = new wxStaticText( this, ID_TEXT, wxT("Reference Temperature"),
-                            wxDefaultPosition,wxDefaultSize, 0);
+   // Integer
+   Integer bsize = 2; // border size
+
+   // Fuel Mass
    fuelMassStaticText = new wxStaticText( this, ID_TEXT, wxT("Fuel Mass"),
                             wxDefaultPosition,wxDefaultSize, 0);
-   fuelDensityStaticText = new wxStaticText( this, ID_TEXT, wxT("Fuel Density"),
-                            wxDefaultPosition,wxDefaultSize, 0);
+   fuelMassTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                          wxDefaultPosition, wxSize(100,-1), 0 );
+   unit3StaticText = new wxStaticText( this, ID_TEXT, wxT("kg"),
+                         wxDefaultPosition,wxDefaultSize, 0); 
+
+   // Pressure
    pressureStaticText = new wxStaticText( this, ID_TEXT, wxT("Pressure"),
                             wxDefaultPosition,wxDefaultSize, 0);
-   volumeStaticText = new wxStaticText( this, ID_TEXT, wxT("Volume"),
-                            wxDefaultPosition,wxDefaultSize, 0);  
-   unit1StaticText = new wxStaticText( this, ID_TEXT, wxT("C"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
-   unit2StaticText = new wxStaticText( this, ID_TEXT, wxT("C"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
-   unit3StaticText = new wxStaticText( this, ID_TEXT, wxT("kg"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
-   unit4StaticText = new wxStaticText( this, ID_TEXT, wxT("kg/m^3"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
+   pressureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                          wxDefaultPosition, wxSize(100,-1), 0 );
    unit5StaticText = new wxStaticText( this, ID_TEXT, wxT("kPa"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
+                         wxDefaultPosition,wxDefaultSize, 0); 
+
+   // Temperature
+   temperatureStaticText = new wxStaticText( this, ID_TEXT, wxT("Temperature"),
+                               wxDefaultPosition,wxDefaultSize, 0);
+   temperatureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                             wxDefaultPosition, wxSize(100,-1), 0 );
+   unit1StaticText = new wxStaticText( this, ID_TEXT, wxT("C"),
+                         wxDefaultPosition,wxDefaultSize, 0); 
+
+   // Reference Temperature
+   refTemperatureStaticText = 
+      new wxStaticText( this, ID_TEXT, wxT("Reference Temperature"),
+          wxDefaultPosition,wxDefaultSize, 0);
+   refTemperatureTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                                wxDefaultPosition, wxSize(100,-1), 0 );
+   unit2StaticText = new wxStaticText( this, ID_TEXT, wxT("C"),
+                         wxDefaultPosition,wxDefaultSize, 0); 
+
+   // Volume
+   volumeStaticText = new wxStaticText( this, ID_TEXT, wxT("Volume"),
+                          wxDefaultPosition,wxDefaultSize, 0);  
+   volumeTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                        wxDefaultPosition, wxSize(100,-1), 0 );
    unit6StaticText = new wxStaticText( this, ID_TEXT, wxT("m^3"),
-                            wxDefaultPosition,wxDefaultSize, 0); 
+                         wxDefaultPosition,wxDefaultSize, 0); 
                             
-   // wxCheckBox
-   pressureRegulatedCheckBox = new wxCheckBox( this, ID_CHECKBOX, 
-                               wxT("Pressure Regulated"),wxDefaultPosition, 
-                               wxDefaultSize, 0 );
-                            
-   Integer bsize = 5; // border size
+   // Fuel Density
+   fuelDensityStaticText = new wxStaticText( this, ID_TEXT, wxT("Fuel Density"),
+                               wxDefaultPosition,wxDefaultSize, 0);
+   fuelDensityTextCtrl = new wxTextCtrl( this, ID_TEXTCTRL, wxT(""), 
+                             wxDefaultPosition, wxSize(100,-1), 0 );
+   unit4StaticText = new wxStaticText( this, ID_TEXT, wxT("kg/m^3"),
+                         wxDefaultPosition,wxDefaultSize, 0); 
+
+   // Pressure Regulated
+   pressureRegulatedCheckBox = 
+      new wxCheckBox( this, ID_CHECKBOX, wxT("Pressure Regulated"),
+          wxDefaultPosition, wxDefaultSize, 0 );
    
    // wx*Sizers     
    wxFlexGridSizer *flexGridSizer1 = new wxFlexGridSizer( 3, 0, 0 );
 
    // Add to wx*Sizers 
-   flexGridSizer1->Add(temperatureStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(temperatureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit1StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(refTemperatureStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(refTemperatureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit2StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(fuelMassStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(fuelMassTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit3StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(fuelDensityStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(fuelDensityTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit4StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(pressureStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(pressureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit5StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(volumeStaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(volumeTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add(unit6StaticText, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add( 0, 0, wxALIGN_CENTRE|wxALL, bsize);
-   flexGridSizer1->Add(pressureRegulatedCheckBox, 0, wxALIGN_CENTER|wxALL, bsize );
-   flexGridSizer1->Add( 0, 0, wxALIGN_CENTRE|wxALL, bsize);
+   flexGridSizer1->Add(fuelMassStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(fuelMassTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit3StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(pressureStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(pressureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit5StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(temperatureStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(temperatureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit1StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(refTemperatureStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(refTemperatureTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit2StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(volumeStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(volumeTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit6StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(fuelDensityStaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(fuelDensityTextCtrl, 0, wxALIGN_CENTER|wxALL, bsize);
+   flexGridSizer1->Add(unit4StaticText, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(0, 0, wxALIGN_LEFT|wxALL, bsize);
+   flexGridSizer1->Add(pressureRegulatedCheckBox, 0, wxALIGN_RIGHT|wxALL, bsize);
+   flexGridSizer1->Add(0, 0, wxALIGN_LEFT|wxALL, bsize);
 
    theMiddleSizer->Add(flexGridSizer1, 0, wxALIGN_CENTRE|wxALL, bsize);
 }
@@ -166,23 +176,23 @@ void TankConfigPanel::LoadData()
    // Set object pointer for "Show Script"
    mObject = theFuelTank;
 
+   paramID = theFuelTank->GetParameterID("FuelMass");
+   fuelMassTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
+
+   paramID = theFuelTank->GetParameterID("Pressure");
+   pressureTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
+   
    paramID = theFuelTank->GetParameterID("Temperature");
    temperatureTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
          
    paramID = theFuelTank->GetParameterID("RefTemperature");
    refTemperatureTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
     
-   paramID = theFuelTank->GetParameterID("FuelMass");
-   fuelMassTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
-
-   paramID = theFuelTank->GetParameterID("FuelDensity");
-   fuelDensityTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
-   
-   paramID = theFuelTank->GetParameterID("Pressure");
-   pressureTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
-   
    paramID = theFuelTank->GetParameterID("Volume");
    volumeTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
+   
+   paramID = theFuelTank->GetParameterID("FuelDensity");
+   fuelDensityTextCtrl->SetValue(wxVariant(theFuelTank->GetRealParameter(paramID)));
    
    paramID = theFuelTank->GetParameterID("PressureRegulated");
    pressureRegulatedCheckBox->SetValue(theFuelTank->GetBooleanParameter(paramID));
@@ -215,82 +225,94 @@ void TankConfigPanel::SaveData()
       
       //loj: 9/26/06 theOkButton->Disable();            
       
+      // Fuel Mass 
+      inputString = fuelMassTextCtrl->GetValue();
+      if ((GmatStringUtil::ToDouble(inputString,&rvalue)) && (rvalue >= 0.0))
+      {
+         paramID = theFuelTank->GetParameterID("FuelMass");
+         theFuelTank->SetRealParameter(paramID, rvalue); 
+      }
+      else
+      {
+         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
+             inputString.c_str(), "Fuel Mass","Real Number >= 0.0");
+         canClose = false;
+      }
+
+      // Pressure 
+      inputString = pressureTextCtrl->GetValue();
+      if ((GmatStringUtil::ToDouble(inputString,&rvalue)) && (rvalue >= 0.0))
+      {
+         paramID = theFuelTank->GetParameterID("Pressure");
+         theFuelTank->SetRealParameter(paramID, rvalue); 
+      }
+      else
+      {
+         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
+             inputString.c_str(), "Pressure","Real Number >= 0.0");
+         canClose = false;
+      }
+
       // Temperature
-      paramID = theFuelTank->GetParameterID("Temperature");
       inputString = temperatureTextCtrl->GetValue(); 
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue)) 
+      if (GmatStringUtil::ToDouble(inputString,&rvalue)) 
+      {
+         paramID = theFuelTank->GetParameterID("Temperature");
+         theFuelTank->SetRealParameter(paramID, rvalue);
+      }
+      else
       {
          MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
              inputString.c_str(), "Temperature","Real Number");
          canClose = false;
-//         return;
       }
-      theFuelTank->SetRealParameter(paramID, rvalue);
 
       // Reference Temperature
-      paramID = theFuelTank->GetParameterID("RefTemperature");
       inputString = refTemperatureTextCtrl->GetValue();
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue)) 
+      if (GmatStringUtil::ToDouble(inputString,&rvalue)) 
+      {
+         paramID = theFuelTank->GetParameterID("RefTemperature");
+         theFuelTank->SetRealParameter(paramID, rvalue); 
+      }
+      else
       {
          MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
              inputString.c_str(), "Reference Temperature","Real Number");
          canClose = false;
-//         return;
       }
-      theFuelTank->SetRealParameter(paramID, rvalue); 
-
-      // Fuel Mass 
-      paramID = theFuelTank->GetParameterID("FuelMass");
-      inputString = fuelMassTextCtrl->GetValue();
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue) || rvalue < 0) 
-      {
-         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
-             inputString.c_str(), "Fuel Mass","Real Number >= 0");
-         canClose = false;
-//         return;
-      }
-      theFuelTank->SetRealParameter(paramID, rvalue); 
-
-      // Fuel Density 
-      paramID = theFuelTank->GetParameterID("FuelDensity");
-      inputString = fuelDensityTextCtrl->GetValue();
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue) || rvalue < 0) 
-      {
-         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
-             inputString.c_str(), "Fuel Density","Real Number >= 0");
-         canClose = false;
-//         return;
-      }
-      theFuelTank->SetRealParameter(paramID, rvalue); 
-
-      // Pressure 
-      paramID = theFuelTank->GetParameterID("Pressure");
-      inputString = pressureTextCtrl->GetValue();
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue) || rvalue < 0) 
-      {
-         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
-             inputString.c_str(), "Pressure","Real Number >= 0");
-         canClose = false;
-//         return;
-      }
-      theFuelTank->SetRealParameter(paramID, rvalue); 
 
       // Volume 
-      paramID = theFuelTank->GetParameterID("Volume");
       inputString = volumeTextCtrl->GetValue();
-      if (!GmatStringUtil::ToDouble(inputString,&rvalue)) 
+      if ((GmatStringUtil::ToDouble(inputString,&rvalue)) && (rvalue >= 0.0))
+      {
+         paramID = theFuelTank->GetParameterID("Volume");
+         theFuelTank->SetRealParameter(paramID, rvalue); 
+      }
+      else
       {
          MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
-             inputString.c_str(), "Volume","Real Number >= 0");
+             inputString.c_str(), "Volume","Real Number >= 0.0");
          canClose = false;
-//         return;
       }
-      theFuelTank->SetRealParameter(paramID, rvalue); 
       
+      // Fuel Density 
+      inputString = fuelDensityTextCtrl->GetValue();
+      if ((GmatStringUtil::ToDouble(inputString,&rvalue)) && (rvalue >= 0.0))
+      {
+         paramID = theFuelTank->GetParameterID("FuelDensity");
+         theFuelTank->SetRealParameter(paramID, rvalue); 
+      }
+      else
+      {
+         MessageInterface::PopupMessage(Gmat::ERROR_, msg.c_str(),
+             inputString.c_str(), "Fuel Density","Real Number >= 0.0");
+         canClose = false;
+      }
+
       // Pressure Regulated
       paramID = theFuelTank->GetParameterID("PressureRegulated");
       theFuelTank->SetBooleanParameter(paramID, 
-                                       pressureRegulatedCheckBox->GetValue());
+         pressureRegulatedCheckBox->GetValue());
 
       EnableUpdate(false);
 //      canClose = true;
@@ -298,6 +320,7 @@ void TankConfigPanel::SaveData()
    catch (BaseException &ex)
    {
       MessageInterface::PopupMessage(Gmat::ERROR_, ex.GetMessage());
+         canClose = false;
    }
    
    
