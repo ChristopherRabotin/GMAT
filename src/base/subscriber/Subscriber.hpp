6f6e870514c52e3629500f631123f5e640b17916
dconway <dconway@222415a0-0dc2-4184-858d-a27cde375526>
2007-07-31 18:25:29 +0000

    Fixed preface issue that prevented compiling on Linux
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@4836 222415a0-0dc2-4184-858d-a27cde375526

--- src/base/subscriber/Subscriber.hpp
+++ src/base/subscriber/Subscriber.hpp
@@ -83,8 +83,8 @@ public:
    virtual std::string GetStringParameter(const std::string &label) const;
    virtual bool SetStringParameter(const Integer id,
                                    const std::string &value);
-   virtual bool Subscriber::SetStringParameter(const std::string &label,
-                                               const std::string &value);
+   virtual bool SetStringParameter(const std::string &label,
+                                   const std::string &value);
    virtual bool SetStringParameter(const Integer id, const std::string &value,
                                    const Integer index);
    virtual bool SetStringParameter(const std::string &label,
