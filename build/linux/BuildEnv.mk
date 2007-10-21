2ee755de3bf36560c3c055b9d827e29e8e5d725d
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2007-10-21 00:57:19 +0000

    First round of makefile updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@5000 222415a0-0dc2-4184-858d-a27cde375526

---
+++ build/linux/BuildEnv.mk
@@ -0,0 +1,134 @@
+# $Header$
+# Build environment file for Linux
+
+# Flags used to control the build
+USE_MATLAB = 0
+USE_DEVIL = 1
+CONSOLE_APP = 0
+DEBUG_BUILD = 0
+PROFILE_BUILD = 0
+WX_28_SYNTAX = 1
+
+# MATLAB specific data
+MATLAB_INCLUDE = -I/opt/matlab73/extern/include
+MATLAB_LIB = -L/opt/matlab73/bin/glnx86 
+MATLAB_LIBRARIES = -leng -lmx -lut \
+                   -lmat -lpthread -lstdc++ -lm -licudata \
+                   -licuuc -licui18n -licuio -lz
+
+# DevIL data
+IL_HEADERS = -I/usr/local/include/IL
+IL_LIBRARIES = -lIL -lILU -lILUT
+
+# wxWidgets settings
+ifeq ($(WX_28_SYNTAX), 1)
+WX_28_DEFINES = -D__USE_WX280__ -D__USE_WX280_GL__ -DwxUSE_GLCANVAS
+else
+WX_28_DEFINES = 
+endif
+
+# The Console app does not support MATLAB linkage for now
+ifeq ($(CONSOLE_APP), 1)
+USE_MATLAB = 0
+endif
+
+# Compiler options
+CPP = g++
+C = gcc
+FORTRAN = gfortran
+
+ifeq ($(PROFILE_BUILD), 1)
+PROFILE_FLAGS = -pg
+else
+PROFILE_FLAGS = 
+endif
+
+DESIRED_OPTIMIZATIONS =  -DSTRICT -Wall -fno-pcc-struct-return -O3 \
+                 -finline-functions -funroll-loops -fno-rtti -DNO_GCC_PRAGMA \
+                 -march=pentium -malign-double -fexceptions \
+                 -fexpensive-optimizations
+
+ifeq ($(DEBUG_BUILD), 1)
+OPTIMIZATIONS = -fno-strict-aliasing -ffriend-injection $(WX_28_DEFINES)
+else
+OPTIMIZATIONS = -O3 -fno-strict-aliasing -ffriend-injection $(WX_28_DEFINES)
+endif
+
+# Do not edit below this line -- here we build up longer compile/link strings
+LINUX_MAC = 1
+
+# Build specific flags
+MATLAB_FLAGS = -D__USE_MATLAB__=1
+
+WXCPPFLAGS = `/usr/local/bin/wx-config --cppflags`
+WXLINKFLAGS = `/usr/local/bin/wx-config --libs --gl-libs --static=no`
+
+
+ifeq ($(CONSOLE_APP),1)
+CONSOLE_FLAGS = -D__CONSOLE_APP__
+else
+CONSOLE_FLAGS = 
+endif
+
+# Set options for debugging and profiling
+ifeq ($(DEBUG_BUILD), 1)
+DEBUG_FLAGS = -g
+else
+DEBUG_FLAGS = 
+endif
+
+# Build the complete list of flags for the compilers
+ifeq ($(USE_MATLAB),1)
+CPP_BASE = $(OPTIMIZATIONS) $(CONSOLE_FLAGS) -Wall $(MATLAB_FLAGS) \
+           $(WXCPPFLAGS) \
+           $(MATLAB_INCLUDE) $(PROFILE_FLAGS) $(DEBUG_FLAGS)
+else
+CPP_BASE = $(OPTIMIZATIONS) $(CONSOLE_FLAGS) -Wall \
+           $(WXCPPFLAGS) $(PROFILE_FLAGS) $(DEBUG_FLAGS)
+endif
+
+ifeq ($(USE_DEVIL), 0)
+CPPFLAGS = $(CPP_BASE) -DSKIP_DEVIL $(PROFILE_FLAGS) $(DEBUG_FLAGS)
+else
+CPPFLAGS = $(CPP_BASE) $(PROFILE_FLAGS) $(DEBUG_FLAGS) $(IL_HEADERS)
+endif
+
+
+F77_FLAGS = $(CPPFLAGS)
+
+F2C_FLAGS = -lgfortran -lm
+# F2C_FLAGS = -lf2c
+
+# Link specific flags
+ifeq ($(USE_DEVIL),1)
+
+ifeq ($(USE_MATLAB),1)
+LINK_FLAGS = $(WXLINKFLAGS)\
+             $(MATLAB_LIB) $(MATLAB_LIBRARIES) \
+             $(DEBUG_FLAGS) \
+             $(IL_LIBRARIES) $(PROFILE_FLAGS) $(F2C_FLAGS)
+else
+LINK_FLAGS = $(WXLINKFLAGS)\
+             $(DEBUG_FLAGS) \
+             $(IL_LIBRARIES) $(PROFILE_FLAGS) $(F2C_FLAGS)
+endif
+
+else
+
+ifeq ($(USE_MATLAB),1)
+LINK_FLAGS = $(WXLINKFLAGS)\
+             $(MATLAB_LIB) $(MATLAB_LIBRARIES) \
+             $(DEBUG_FLAGS) $(F2C_FLAGS)
+else
+LINK_FLAGS = $(WXLINKFLAGS) $(DEBUG_FLAGS) $(F2C_FLAGS)
+endif
+
+endif
+
+
+ifeq ($(USE_MATLAB),1)
+CONSOLE_LINK_FLAGS = $(MATLAB_LIB) $(MATLAB_LIBRARIES) -L../../base/lib \
+                    -lgfortran $(DEBUG_FLAGS) $(PROFILE_FLAGS)
+else
+CONSOLE_LINK_FLAGS = -L../../base/lib -lgfortran $(DEBUG_FLAGS) $(PROFILE_FLAGS) 
+endif
