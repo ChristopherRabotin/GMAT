6818c3d095f7886bb2ca8c8c654c2ef5c7b52935
lindajun <lindajun@222415a0-0dc2-4184-858d-a27cde375526>
2008-02-05 15:21:05 +0000

    To correctly convert arguments in 1xM cell array to string.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@5205 222415a0-0dc2-4184-858d-a27cde375526

--- bin/matlab/gmat_keyword/CallGMAT.m
+++ bin/matlab/gmat_keyword/CallGMAT.m
@@ -1,23 +1,61 @@
-%$Header: /GMAT/dev/cvs/supportfiles/matlab/gmat_keyword/CallGMAT.m,v 1.1 2007/08/22 19:15:16 shughes Exp $
+%$Id$
+%-------------------------------------------------------------------------------
+% function CallGMAT(cmd, varargin)
+%-------------------------------------------------------------------------------
+% GMAT: General Mission Analysis Tool
+%
+% Author: Linda Jun (NASA/GSFC)
+% Created: 2003/03/03
+%
+% To form command string from command name and arguments and send command string
+% to GMAT via ddepoke on Windows platform.
+%    command string = cmd varargin;
+%
+% Since varargin is a {1xM} cell, it needs to convert it to a string.
+% for example 'Create Spacecraft sat1' will have
+%    cmd = 'Create' and varargin = 'Spacecraft' 'sat1'
+% and command string will have
+%    'Create Spacecraft sat1;'
+%
+% @param  cmd  input command name (such as Create, Propagate, GMAT)
+% @param  varargin  command arguments (such as DefaultProp(DefaultSC))
+%-------------------------------------------------------------------------------
 function CallGMAT(cmd, varargin)
 
+%%disp('==============================');
+%%disp([cmd varargin{1}]);
+
+% convert {1xM} cell array of arguments to array of strings of arguments
 strarr = char(varargin{1});
-strarr = strcat(strarr, '*');
+%%disp(strarr);
+
+%-------------------------------------------------
+% concatenate command and command arguments
+%-------------------------------------------------
 m = size(strarr, 1);
-str = [cmd '*'];
+
+% add space after command if number of arguments is greater than zero
+str = cmd;
+if m > 0
+   str = [cmd ' '];
+end
+%%disp(str);
 
 for i = 1:m
-   str = strcat(str, strarr(i,:));
+   str1 = strarr(i,:);
+   str1 = deblank(str1);
+   str = [str str1];
+   if i < m
+      str = [str ' '];
+   end
 end
 
-str = strrep(str, '*', ' ');
-str = deblank(str);
 str = [str ';'];
-%disp(str);
+%%disp(str);
 
-%--------------------------------------
+%-------------------------------------------------
 % call dde function to send string
-%--------------------------------------
+%-------------------------------------------------
 global gmatChannel;
 
 Poke(gmatChannel, 'script', str);
