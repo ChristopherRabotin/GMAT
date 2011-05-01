9e7a1329ef6a0ea251a2de6fb4f6f7ac09f7ff52
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2011-05-01 01:31:52 +0000

    Merge of Development into Trunk, Step 2.
    This step completes the second piece of merge, for the build folders.  This should synchronize the build files with development, but the build files are not yet tested.  
    
    **** WARNING!!! **** Trunk might not build after this step.  DO NOT work with it at this point -- there are subsequent steps that will be performed to return Trunk to a stable (R2011a) code base!
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@9520 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: f478481877c92d46bb01958844c6b37d539373ec

--- build/linux64/RunGmatLauncher.sh
+++ build/linux64/RunGmatLauncher.sh
@@ -1,4 +1,4 @@
 # Linux launcher configured for use with a desktop icon
 # Update the path information here to match your configuration
-cd ~/GMAT-R2011a/bin;LD_LIBRARY_PATH=.:./lib:/opt/matlab/R2009b/bin/glnxa64 ./GMAT
+cd ~/GMAT-2011a/bin;LD_LIBRARY_PATH=.:./lib:/opt/matlab/R2009b/bin/glnxa64 ./GMAT
 
