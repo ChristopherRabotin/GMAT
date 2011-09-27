c30467f8463d8881f9d0bc4f976753d6e85695a3
jjkparker <jjkparker@222415a0-0dc2-4184-858d-a27cde375526>
2011-09-27 14:32:55 +0000

    build files for Win32 MATLAB libCInterface wrapper
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@9916 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 8c41fb15643b2e3938d52d5d59b88d50ec52422c

--- plugins/CInterfacePlugin/build/matlab/prepareInterface.m
+++ plugins/CInterfacePlugin/build/matlab/prepareInterface.m
@@ -1,7 +1,16 @@
 disp('This script builds the GMAT interface prototypes by calling ');
-disp('loadlibrary.  The loadlibrary call will fail; this is expected.');
-disp('The interfacewrapper should be built anyway.');
+disp('loadlibrary. The prototype file is placed in');
+disp('trunk/application/matlab/libCInterface.');
 
-[notfound, warnings] = loadlibrary('../lib/libCInterface',...
-    '../src/plugin/CInterfaceFunctions','mfilename','interfacewrapper',...
-    'includepath','../src/include');
+thisdir = fileparts(mfilename('fullpath'));
+appdir = fullfile(thisdir, '..', '..', '..', '..', 'application');
+libdir = fullfile(appdir, 'bin', 'libCInterface');
+hdir = fullfile(thisdir, '..', '..', 'src', 'plugin', ...
+    'CInterfaceFunctions');
+incdir = fullfile(thisdir, '..', '..', 'src', 'include');
+matlabdir = fullfile(appdir, 'matlab', 'libCInterface');
+
+[notfound, warnings] = loadlibrary(libdir, hdir, ...
+    'mfilename','interfacewrapper', 'includepath', incdir);
+
+movefile('interfacewrapper.m', matlabdir);
