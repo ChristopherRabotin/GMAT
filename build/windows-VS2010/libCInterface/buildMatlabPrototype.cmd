7f2a0b4b8c9fad9d9a4a05ffef6150693ae7d12d
jjkparker <jjkparker@222415a0-0dc2-4184-858d-a27cde375526>
2011-09-29 19:32:21 +0000

    updated buildMatlabPrototype.cmd to make Matlab close after generating the interface
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@9925 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 36522a5388f6065e8fc92bad737cf810fa443741

--- build/windows-VS2010/libCInterface/buildMatlabPrototype.cmd
+++ build/windows-VS2010/libCInterface/buildMatlabPrototype.cmd
@@ -1,4 +1,4 @@
 rem Builds the prototype file for MATLAB's loadlibrary function.
 rem Requires MATLAB in the path.
 
-matlab -nosplash -minimize -r "addpath('..\..\..\plugins\CInterfacePlugin\build\matlab'); prepareInterface"
+matlab -nosplash -minimize -r "addpath('..\..\..\plugins\CInterfacePlugin\build\matlab'); prepareInterface; quit"
