658d186a5d3cd143098b9f50ccb66b5cca0ba2ae
wendys-dev <wendys-dev@222415a0-0dc2-4184-858d-a27cde375526>
2011-10-18 20:04:00 +0000

    Updates for Macosx to build 32-bit or 64-bit on option (i.e. trying to encapsulate everything in one make file)
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@9953 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 56999e62433294827d0c8a6543e7a7b0bcf28ee3

---
+++ plugins/CInterfacePlugin/build/macosx/CInterfaceEnv.mk
@@ -0,0 +1,82 @@
+# $Id$
+# Architecture flags
+# Set to 0 for Windows, 1 for Linux or Mac
+LINUX_MAC = 1
+# Set to 0 for Linux, 1 for Mac 
+MAC_SPECIFIC = 1
+BUILD_64BIT_MAC = 0
+
+ifeq ($(BUILD_64BIT_MAC), 1)
+CSPICE_VER = cspice64
+GMAT_BIN_DIR = bin64
+GMAT_LIB_DIR = lib64
+GMAT_PLUGIN_DIR = plugins64
+MAC_ARCHITECTURE_FLAGS = 
+else
+CSPICE_VER = cspice
+GMAT_BIN_DIR = bin
+GMAT_LIB_DIR = lib
+GMAT_PLUGIN_DIR = plugins
+MAC_ARCHITECTURE_FLAGS = -arch i386
+endif
+
+# Environment settings for the GMAT C Interface plugin
+# location of GMAT base headers and libraries
+GMAT_CODE_LOCATION = ../../../src
+GMAT_BIN_LOCATION = ../../../application/$(GMAT_BIN_DIR)
+
+
+DEBUG_BUILD = 0
+
+USE_SPICE = 1
+
+ifeq ($(USE_SPICE), 1)
+# location of CSPICE headers and libraries
+# *** EDIT THIS *** -this is where you installed the version of CSPICE that you're using ...
+SPICE_DIR = /Applications/CSPICE_N0064
+SPICE_INCLUDE = -I$(SPICE_DIR)/$(CSPICE_VER)/include
+SPICE_LIB_DIR = $(SPICE_DIR)/$(CSPICE_VER)/lib
+SPICE_LIBRARIES = $(SPICE_LIB_DIR)/cspice.a
+SPICE_DIRECTIVE = -D__USE_SPICE__
+SPICE_STACKSIZE = ulimit -s 61440
+else
+SPICE_INCLUDE =
+SPICE_LIB_DIR =
+SPICE_LIBRARIES =
+SPICE_DIRECTIVE = 
+SPICE_STACKSIZE = echo 'SPICE not included in this build ...'
+endif
+
+# Select the base library
+BASE_LIBRARY = GmatBase
+
+# Compiler options
+CPP = g++
+C = gcc
+
+ifeq ($(PROFILE_BUILD), 1)
+PROFILE_FLAGS = -pg
+else
+PROFILE_FLAGS = 
+endif
+
+SHARED_EXTENSION = .dylib
+SHARED_LIB_FLAGS = -dylib -dynamiclib -two_levelnamespace -undefined dynamic_lookup -fPIC $(MAC_ARCHITECTURE_FLAGS)
+
+# --out-implib
+
+DESIRED_OPTIMIZATIONS =  -DSTRICT -Wall -fno-pcc-struct-return -O3 \
+                 -finline-functions -funroll-loops -fno-rtti -DNO_GCC_PRAGMA \
+                 -march=pentium -malign-double -fexceptions \
+                 -fexpensive-optimizations
+
+ifeq ($(DEBUG_BUILD), 1)
+OPTIMIZATIONS = -fno-strict-aliasing -fno-rtti -fPIC
+else
+OPTIMIZATIONS = -O3 -fno-strict-aliasing -fno-rtti -fPIC
+endif
+
+# Do not edit below this line -- here we build up longer compile/link strings
+CPP_BASE = $(OPTIMIZATIONS) -Wall $(PROFILE_FLAGS) $(DEBUG_FLAGS) $(MAC_ARCHITECTURE_FLAGS)
+
+CPPFLAGS = $(CPP_BASE)
