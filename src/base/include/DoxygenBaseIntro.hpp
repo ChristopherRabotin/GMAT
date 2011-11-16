e6c34a84fd891dbe3893ead37c6b44ff9644b375
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2011-11-16 18:48:15 +0000

    Doxygen configuration files for base and gui.  These are not quite ready for use yet -- file locations need to be set still.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@10014 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: bd078ca1fbdb0cc38c6b0f5150b7898865ea18b4

---
+++ src/base/include/DoxygenBaseIntro.hpp
@@ -0,0 +1,10 @@
+/**
+ * \mainpage GMAT Base Code Overview
+ *
+ * This documentation provides a description of the contents of the core engine
+ * code for the General Mission Analysis Tool, GMAT.  GMAT's engine is built
+ * into a shared library that provides all of the core functionality of the
+ * system, as described in the opening chapters to the GMAT Architectural
+ * Specification.  This library, libGmatBase, contains the classes structures,
+ * and other elements described in this documentation.
+ */
