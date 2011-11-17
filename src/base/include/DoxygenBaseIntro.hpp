aebc8cf880f9115d4083b3e83cc6b42f9185db85
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2011-11-17 18:14:07 +0000

    Doxygen configurations for base and gui.  These files provide a first cut at the files needed for separate base and gui output from Doxygen.  The output is written to <trunk>/doc/html, in separate base and gui folders.  If we decide we want a top level file, it should go in the doc/html folder with links to doc/html/base/index.html and doc/html/gui/index.html
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@10018 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 3a995e32410fe81037ebb2a17c8493206452502c

--- src/base/include/DoxygenBaseIntro.hpp
+++ src/base/include/DoxygenBaseIntro.hpp
@@ -2,9 +2,32 @@
  * \mainpage GMAT Base Code Overview
  *
  * This documentation provides a description of the contents of the core engine
- * code for the General Mission Analysis Tool, GMAT.  GMAT's engine is built
- * into a shared library that provides all of the core functionality of the
- * system, as described in the opening chapters to the GMAT Architectural
- * Specification.  This library, libGmatBase, contains the classes structures,
- * and other elements described in this documentation.
+ * code for the General Mission Analysis Tool, GMAT.
+ *
+ * GMAT's engine is built into a shared library that provides all of the core
+ * functionality of the system, as described in the opening chapters to the GMAT
+ * Architectural Specification.  This library, libGmatBase, contains the
+ * classes,structures, and other elements described in that documentation.
+ * Interested readers should review the Architectural Specification to gain an
+ * understanding of how the components described in this documentation behave.
+ *
+ * The documentation presented here describes the low level structures and data
+ * elements of GMAT's classes and functions contained in the engine and the core
+ * user configured elements comprising the resources and mission control
+ * sequence.  By studying this documentation, you can learn how GMAT's classes
+ * work and interact to accomplish mission modeling tasks.  Programmers that
+ * want to extend GMAT's functionality should read this documentation while
+ * writing their own classes derived from the GMAT code so that they can ensure
+ * that derived elements used in custom code conforms to GMAT's interfaces.
+ *
+ * This documentation comprises the application programming interface (API) for
+ * the components of the core GMAT system.  Every member of all classes in
+ * GMAT's base code has an entry in this documentation, along with the
+ * documentation extracted from the code.  The documentation in the code
+ * conforms to the needs of the Doxygen open source documentation system.  This
+ * conformity is enforced through the GMAT coding standards, as described in the
+ * GMAT Style Guide.
+ *
+ * The wxWidgets based GMAT graphical user interface (GUI) is documented in a
+ * separate set of pages.
  */
--- src/gui/include/DoxygenGuiIntro.hpp
+++ src/gui/include/DoxygenGuiIntro.hpp
@@ -1,10 +1,39 @@
 /**
- * \mainpage GMAT Base Code Overview
+ * \mainpage GMAT GUI Code Overview
  *
- * This documentation provides a description of the contents of the core engine
- * code for the General Mission Analysis Tool, GMAT.  GMAT's engine is built
- * into a shared library that provides all of the core functionality of the
- * system, as described in the opening chapters to the GMAT Architectural
- * Specification.  This library, libGmatBase, contains the classes structures,
- * and other elements described in this documentation.
+ * This documentation provides a description of the contents of the graphical
+ * user interface (GUI) code for the General Mission Analysis Tool, GMAT.
+ *
+ * GMAT's GUI is built into an application that provides system users with
+ * access to all of the resources and commands needed to model a Spacecraft
+ * mission in GMAT.  This application can be built for Windows, Mac, and Linux
+ * systems using the code documented here.  The resulting application interfaces
+ * the core GMAT engine, contained in a shared library named libGmatBase, with
+ * a friendly GUI documented here.
+ *
+ * The documentation presented here describes the GUI structures and data
+ * elements that access GMAT's classes and functions contained in the engine
+ * and the core user configured elements comprising the resources and mission
+ * control sequence.  The interface presented to users through this code
+ * provides the means needed to interact with these elements to create mission
+ * elements, design a mission timeline that uses those elements, and then run
+ * the timeline to model a mission, producing both graphical output and reports
+ * of the mission execution based on the user's specifications.
+ *
+ * By studying this documentation, you can learn how GMAT's GUI works and
+ * interacts with the core code to accomplish mission modeling tasks.
+ * Programmers that want to extend GMAT's functionality should read this
+ * documentation while writing their own extensions to the GMAT code so that
+ * they can ensure that derived elements used in custom code conforms to GMAT's
+ * interfaces.
+ *
+ * This documentation comprises the application programming interface (API) for
+ * the components of the GMAT wxWidgets based GUI.  Every member of all classes
+ * in GMAT's GUI code has an entry in this documentation, along with the
+ * documentation extracted from the code.  The documentation in the code
+ * conforms to the needs of the Doxygen open source documentation system.  This
+ * conformity is enforced through the GMAT coding standards, as described in the
+ * GMAT Style Guide.
+ *
+ * The GMAT engine is documented in a separate set of pages.
  */
