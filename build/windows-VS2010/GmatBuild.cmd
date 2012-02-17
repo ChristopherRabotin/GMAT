750cbcd804858b3643461ab820697e4b40002db6
jjkparker <jjkparker@222415a0-0dc2-4184-858d-a27cde375526>
2012-02-17 19:42:09 +0000

    updated build files with new assemble.sh
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@10239 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 8c096549f45d4b089c416208684c93020076d1ea

--- build/windows-VS2010/GmatBuild.cmd
+++ build/windows-VS2010/GmatBuild.cmd
@@ -1,46 +1,44 @@
-echo off
+@echo off
 REM Clears the screen
 CLS
 
+set MSYS=C:\msys
+set SH=%MSYS%\bin\sh.exe
+set Path=%MSYS%\bin;%Path%
+
 echo Starting build process at %date% - %time%
 echo    * Cleaning up old build (%time%)...
-c:\msys\bin\sh.exe -c "make clean"
-rem c:\msys\bin\sh.exe -c "make clean > GmatClean.log 2> GmatClean.err"
+%SH% -c "make clean > GmatClean.log 2> GmatClean.err"
 IF ERRORLEVEL 2 echo      There were errors cleaning the old build
 IF ERRORLEVEL 0 echo      Cleanup was successful
 
 rem echo    * Checking out the code (%time%)...
-rem c:\msys\bin\sh.exe -c "make checkout > GmatCheckout.log 2> GmatCheckout.err"
+%SH% -c "make checkout > GmatCheckout.log 2> GmatCheckout.err"
 rem IF ERRORLEVEL 2 GOTO CheckoutError
 rem IF ERRORLEVEL 0 echo      Checkout from svn was successful
 
 echo    * Compiling GMAT (%time%)...
-c:\msys\bin\sh.exe -c "make all"
-rem c:\msys\bin\sh.exe -c "make all > GmatBuild.log 2> GmatBuild.err"
+%SH% -c "make all > GmatBuild.log 2> GmatBuild.err"
 IF ERRORLEVEL 2 GOTO BuildError
 IF ERRORLEVEL 0 echo      Compilation was successful
 
 echo    * Creating user docs (%time%)...
-c:\msys\bin\sh.exe -c "make userdocs"
-rem c:\msys\bin\sh.exe -c "make userdocs > GmatUserDocs.log 2> GmatUserDocs.err"
+%SH% -c "make userdocs > GmatUserDocs.log 2> GmatUserDocs.err"
 IF ERRORLEVEL 2 echo      There were errors making the user documentation
 IF ERRORLEVEL 0 echo      User documentation build was successful
 
 echo    * Creating dev docs (%time%)...
-rem c:\msys\bin\sh.exe -c "make devdocs > GmatDevDocs.log 2> GmatDevDocs.err"
-c:\msys\bin\sh.exe -c "make devdocs "
+%SH% -c "make devdocs > GmatDevDocs.log 2> GmatDevDocs.err"
 IF ERRORLEVEL 2 echo      There were errors making the developer documentation
 IF ERRORLEVEL 0 echo      Developer documentation build was successful
 
 echo    * Assembling Daily Distribution (%time%)...
-c:\msys\bin\sh.exe -c "make dist"
-rem c:\msys\bin\sh.exe -c "make dist > GmatDist.log 2> GmatDist.err"
+%SH% -c "make dist > GmatDist.log 2> GmatDist.err"
 IF ERRORLEVEL 2 echo      There were errors assembling today's GMAT executable distribution
 IF ERRORLEVEL 0 echo      GMAT executable distribution was successfully assembled
 
 echo    * Assembling Latest Developer Docs Distribution (%time%)...
-c:\msys\bin\sh.exe -c "make dist-devdocs"
-rem c:\msys\bin\sh.exe -c "make dist-devdocs > GmatDistDevDocs.log 2> GmatDistDevDocs.err"
+%SH% -c "make dist-devdocs > GmatDistDevDocs.log 2> GmatDistDevDocs.err"
 IF ERRORLEVEL 2 echo      There were errors assembling the latest developer documentation distributable
 IF ERRORLEVEL 0 echo      Latest developer documentation distributable was successfully assembled
 
---
+++ build/windows-VS2010/assemble.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# This script assembles a complete Windows executable copy of GMAT after a
+# build. It assumes that it is being run from the trunk/build/windows-VS2010
+# directory.
+#
+# Usage: assemble.sh [option] ...
+# Options:
+#   -d dest       Place files in specified directory dest (default: ./gmat)
+#   -j jazzpath   Path to a working copy of <Jazz>/trunk/code
+#   -?            Print this usage message
+
+# Initializations
+dest=./gmat
+jazz=../../../GmatPlugins
+
+usage() {
+cat <<END
+Usage: $0 [option] ...
+Options:
+  -d dest       Place files in specified directory dest (default: ./gmat)
+  -j jazzpath   Path to a working copy of <Jazz>/trunk/code
+  -?            Print this usage message
+END
+}
+
+# Argument handling
+while getopts d:j: o
+do
+    case "$o" in
+        d) dest="$OPTARG";;
+        j) jazz="$OPTARG";;
+        ?) usage; exit 1;;
+    esac
+done
+
+# Create destination directory
+if [ ! -e "$dest" ]
+then
+    mkdir -p "$dest"
+fi
+
+# Copy application files
+cp -av ../../application/* "$dest"
+
+# Remove build files
+find "$dest" -iname '*.exp' -delete
+find "$dest" -iname '*.lib' -delete
+
+# Copy support libraries
+winbuildspath='//mesa-file/595/GMAT/Builds/windows/VS2010_build'
+cp -av \
+    "$winbuildspath"/../vc_lib/wx2.8.12/*.dll \
+    "$dest"/bin
+cp -av \
+    "$winbuildspath"/../vc_lib/*.dll \
+    "$dest"/bin
+
+# Remove any Windows hidden files
+find "$dest" -iname thumbs.db -delete
+
+# Mars-GRAM 2005 data
+mgpath="$jazz/MarsGRAMPlugin/data"
+cp -av "$mgpath/*" "$dest/data"
+
+# libCInterface MATLAB files
+cifacepath=../../plugins/CInterfacePlugin
+cp -av "$cifacepath/matlab" "$dest/matlab/libCInterface"
