db79109f44a274fcf56ca16dd2e4a0fd62de9959
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2012-07-13 17:03:08 +0000

    Another Linux 32 config file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@10643 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 5169614f3b2955e32d481d82d28929bc2b8f4e8b

---
+++ build/linux/RunGmatLauncher.sh
@@ -0,0 +1,4 @@
+# Linux launcher configured for use with a desktop icon
+# Update the path information here to match your configuration
+cd ~/GsfcCode/GmatDevelopment/application/bin;LD_LIBRARY_PATH=.:./lib:/opt/matlab/R2012a/bin/glnx86 ./GMAT
+
