728ef35bf81ecc7ed5255e279c797138c0379c99
djcinsb <djcinsb@222415a0-0dc2-4184-858d-a27cde375526>
2012-09-07 00:17:07 +0000

    [GMT-2347] Fixed the coordinate system inputs for finite burns when the spacecraft origin and force model origins differ
    git-svn-id: svn+ssh://svn.code.sf.net/p/gmat/code/trunk@10845 222415a0-0dc2-4184-858d-a27cde375526
    
    Former-commit-id: 26360cf4d8654cfaee1134aaa8c44d6f2b1216bf

--- src/base/forcemodel/ODEModel.cpp
+++ src/base/forcemodel/ODEModel.cpp
@@ -945,7 +945,7 @@ void ODEModel::BufferState()
     
 
 //------------------------------------------------------------------------------
-// void ODEModel::UpdateSpaceObject(Real newEpoch)
+// void UpdateSpaceObject(Real newEpoch)
 //------------------------------------------------------------------------------
 /**
  * Updates state data for the spacecraft or formation that use this force model.
@@ -1051,7 +1051,7 @@ void ODEModel::RevertSpaceObject()
           prevElapsedTime, elapsedTime);
    #endif
 
-   elapsedTime = prevElapsedTime;//previousState.GetEpoch();// * GmatTimeConstants::SECS_PER_DAY;
+   elapsedTime = prevElapsedTime;
 
    memcpy(rawState, previousState.GetState(), dimension*sizeof(Real));
    MoveToOrigin();
@@ -2311,9 +2311,20 @@ bool ODEModel::GetDerivatives(Real * state, Real dt, Integer order,
                objBody = ((SpaceObject*)(dynamicObjects[i]))->GetOrigin();
             if (objBody != NULL)
             {
-               Rvector6 offset = objBody->GetMJ2000State(((SpaceObject*)
+//               Rvector6 offset = objBody->GetMJ2000State(((SpaceObject*)
+               Rvector6 offset = forceOrigin->GetMJ2000State(((SpaceObject*)
                      (dynamicObjects[i]))->GetEpoch() + dt /
                      GmatTimeConstants::SECS_PER_DAY);
+
+               #ifdef DEBUG_DYNAMIC_FORCES
+                  MessageInterface::ShowMessage("Dynamics: resetting %s.%s "
+                        "from value %lf to %lf + %lf = %lf\n",
+                        dynamicObjects[i]->GetName().c_str(),
+                        dynamicObjects[i]->GetParameterText(dynamicIDs[i]).c_str(),
+                        dynamicObjects[i]->GetRealParameter(dynamicIDs[i]),
+                        state[dynamicsIndex[i]], offset[dynamicIDs[i]-stateStart],
+                        state[dynamicsIndex[i]] + offset[dynamicIDs[i]-stateStart]);
+               #endif
 
                dynamicObjects[i]->SetRealParameter(dynamicIDs[i],
                      state[dynamicsIndex[i]] +
@@ -2731,6 +2742,7 @@ Real ODEModel::EstimateError(Real *diffs, Real *answer) const
                err = sqrt(err / mag);
             else
                err = sqrt(err);
+            break;
       }
 
       #ifdef DEBUG_ERROR_ESTIMATE
