2fd3430fd98558a2c5c4889e0e7efa6c5eaacc9b
Joel Parker <joel.j.k.parker@nasa.gov>
2013-09-16 10:16:36 -0400

    changed "from" address to Linda's, and modularized a bit
    Former-commit-id: b055b6192b089a1eedba58890241b6b1e9e57f90

--- build/windows-VS2010/GmatBuildandSend.cmd
+++ build/windows-VS2010/GmatBuildandSend.cmd
@@ -1,8 +1,10 @@
 @echo off
 
-rem recipient address
+rem configuration
+set server=mailhost.gsfc.nasa.gov
+set fromaddress=linda.o.jun@nasa.gov
 set toaddress=gmat-buildtest@lists.sourceforge.net
 
-blat start.txt -to %toaddress% -f j.parker@nasa.gov -s "GMAT Build Started "%time% -server mailhost.gsfc.nasa.gov -timestamp -debug -log blat.log
+blat start.txt -to %toaddress% -f %fromaddress% -s "GMAT Build Started "%time% -server %server% -timestamp -debug -log blat.log
 call GmatBuild.cmd >results.txt
-blat results.txt -to %toaddress% -f j.parker@nasa.gov -s "GMAT Build "%time% -server mailhost.gsfc.nasa.gov -timestamp -debug -log blat.log
+blat results.txt -to %toaddress% -f %fromaddress% -s "GMAT Build "%time% -server %server% -timestamp -debug -log blat.log
