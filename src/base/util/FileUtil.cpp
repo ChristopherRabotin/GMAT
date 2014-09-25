6ecb0ddcc6f00c04c5cebeecc673503643471ead
Ravi Mathur <ravi.mathur@emergentspace.com>
2014-09-24 23:00:01 -0400

    Updated CMake build system to include all non-proprietary plugins. MatlabInterface and FminconOptimizer build files are still works in progress. Fixed references to __MAC__ that should be __APPLE__ for all modern compilers. Updated build system to include new base/ source files.
    Former-commit-id: 695183b1cdb1741bfd8956440b9c842f68689302

--- src/base/util/FileUtil.cpp
+++ src/base/util/FileUtil.cpp
@@ -42,7 +42,7 @@
 #include <direct.h>
 #endif
 
-#ifdef __MAC__
+#ifdef __APPLE__
 #include <mach-o/dyld.h>
 #include <unistd.h>                // for getcwd
 #endif
@@ -252,7 +252,7 @@ std::string GmatFileUtil::GetApplicationPath()
    }
    return appPath;
 
-#elif __MAC__
+#elif __APPLE__
    std::string appPath = "./";
    char path[GmatFile::MAX_PATH_LEN];
    char actualPath[GmatFile::MAX_PATH_LEN];
@@ -342,7 +342,7 @@ std::string GmatFileUtil::ParsePathName(const std::string &fullPath,
    
    std::string filePath("");
    std::string thePathToUse = fullPath;
-   #ifdef __MAC__
+   #ifdef __APPLE__
       std::string appString = ".app";
       std::string::size_type appLoc = fullPath.rfind(appString);
       if (appLoc != fullPath.npos)
