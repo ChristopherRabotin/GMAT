ef1c78758197754aaa75f93e382d87d7f4891e9e
Linda Jun <linda.o.jun@nasa.gov>
2014-10-06 10:22:20 -0400

    WX3 GMT-4732 There are no all Fields Visible in Attitude>ProcessingSpinner Panel
    Changed precSpinAnglesRatesSizer to wxFlexGridSizer(3)
    Changed to use defHeight instead of using fixed width of 20 to fix text cutoff with wx3
    
    
    Former-commit-id: b337773c3c516d0fb4f7504fad8a88dc15ad34b1

--- src/gui/spacecraft/AttitudePanel.cpp
+++ src/gui/spacecraft/AttitudePanel.cpp
@@ -225,13 +225,14 @@ void AttitudePanel::Create()
    // being clipped
    // NOTE - this may need to be adjusted for different platforms
    Integer staticTextWidth = 165;
-   
    #if __WXMAC__
    int buttonWidth = 40;
    int otherTextWidth = 100;
+   Integer defHeight = 20;
    #else
    int buttonWidth = 25;
    int otherTextWidth = 100;
+   Integer defHeight = -1; // to use wx default height
    #endif
    
    wxBitmap openBitmap = wxBitmap(OpenFolder_xpm);
@@ -271,7 +272,7 @@ void AttitudePanel::Create()
    
    config1StaticText =
       new wxStaticText( this, ID_TEXT, "Attitude "GUI_ACCEL_KEY"Model",
-                        wxDefaultPosition, wxSize(staticTextWidth,20), 0); // wxDefaultSize, 0);
+                        wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); // wxDefaultSize, 0);
    config1ComboBox = 
       new wxComboBox( this, ID_CB_MODEL, attitudeModelArray[0], 
          wxDefaultPosition, wxDefaultSize, modelSz, attitudeModelArray, 
@@ -281,7 +282,7 @@ void AttitudePanel::Create()
    // Coordinate System
    config2StaticText =
       new wxStaticText( this, ID_TEXT, GUI_ACCEL_KEY"Coordinate System",
-         wxDefaultPosition, wxSize(staticTextWidth,20), 0); // wxDefaultSize, 0);
+         wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); // wxDefaultSize, 0);
    coordSysComboBox =  theGuiManager->GetCoordSysComboBox(this, ID_CB_COORDSYS,
       wxDefaultSize);
    coordSysComboBox->SetToolTip(pConfig->Read(_T("CoordinateSystemHint")));
@@ -298,7 +299,7 @@ void AttitudePanel::Create()
 
    config4StaticText =
       new wxStaticText( this, ID_TEXT, GUI_ACCEL_KEY"Euler Angle Sequence",
-                        wxDefaultPosition, wxSize(staticTextWidth,20), 0); // wxDefaultSize, 0);
+                        wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); // wxDefaultSize, 0);
    config4ComboBox = new wxComboBox( this, ID_CB_SEQ, eulerSequenceArray[0],
                       wxDefaultPosition, wxDefaultSize, 12,
                       eulerSequenceArray, wxCB_DROPDOWN|wxCB_READONLY );
@@ -318,7 +319,8 @@ void AttitudePanel::Create()
    
    stateTypeComboBox = 
       new wxComboBox( this, ID_CB_STATE, stateArray[STARTUP_STATE_TYPE_SELECTION],
-         wxDefaultPosition, wxSize(180,20), AttStateTypeCount, stateArray,
+                      //wxDefaultPosition, wxSize(180,defHeight), AttStateTypeCount, stateArray,
+         wxDefaultPosition, wxDefaultSize, AttStateTypeCount, stateArray,
          wxCB_DROPDOWN|wxCB_READONLY );
    stateTypeComboBox->SetToolTip(pConfig->Read(_T("StateTypeHint")));
    
@@ -388,7 +390,8 @@ void AttitudePanel::Create()
    stateRateTypeComboBox =
       new wxComboBox( this, ID_CB_STATE_RATE, 
          stateRateArray[STARTUP_RATE_STATE_TYPE_SELECTION], wxDefaultPosition, 
-         wxSize(180,20), AttStateRateTypeCount, stateRateArray,
+                      //wxSize(180,defHeight), AttStateRateTypeCount, stateRateArray,
+         wxDefaultSize, AttStateRateTypeCount, stateRateArray,
          wxCB_DROPDOWN|wxCB_READONLY );                  
    stateRateTypeComboBox->SetToolTip(pConfig->Read(_T("RateStateTypeHint")));
    
@@ -429,7 +432,7 @@ void AttitudePanel::Create()
    // create the message to be displayed when the user selects "SpiceAttitude"
    spiceMessage =
       new wxStaticText( this, ID_TEXT, wxT("Set data on the SPICE tab."),
-                        wxDefaultPosition, wxSize(staticTextWidth,20), 0); //wxDefaultSize, 0);
+                        wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); //wxDefaultSize, 0);
 
    // Create body spin axis text label
    spinAxisLabel =
@@ -438,15 +441,15 @@ void AttitudePanel::Create()
    spinAxis1TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_SPIN_AXIS, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    spinAxis2TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_SPIN_AXIS, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    spinAxis3TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_SPIN_AXIS, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    
    // Create nutation reference vector text label
    nutRefVecLabel =
@@ -455,15 +458,15 @@ void AttitudePanel::Create()
    nutRefVec1TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_NUT_REF_VEC, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    nutRefVec2TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_NUT_REF_VEC, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    nutRefVec3TextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_NUT_REF_VEC, wxT(""), wxDefaultPosition, 
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    
    // Create labels and text ctrls for other angles and rates
    initPrecAngleLabel =
@@ -474,7 +477,7 @@ void AttitudePanel::Create()
    initPrecAngleTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_INIT_PREC_ANGLE, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    precessionRateLabel =
       new wxStaticText(this, ID_TEXT, wxT("Precession Rate"), wxDefaultPosition, wxDefaultSize, 0);
@@ -483,7 +486,7 @@ void AttitudePanel::Create()
    precessionRateTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_PRECESSION_RATE, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    nutationAngleLabel =
       new wxStaticText(this, ID_TEXT, wxT("Nutation Angle"), wxDefaultPosition, wxDefaultSize, 0);
@@ -492,7 +495,7 @@ void AttitudePanel::Create()
    nutationAngleTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_NUTATION_ANGLE, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    initSpinAngleLabel =
       new wxStaticText(this, ID_TEXT, wxT("Initial Spin Angle"), wxDefaultPosition, wxDefaultSize, 0);
@@ -501,7 +504,7 @@ void AttitudePanel::Create()
    initSpinAngleTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_INIT_SPIN_ANGLE, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    spinRateLabel =
       new wxStaticText(this, ID_TEXT, wxT("Spin Rate"), wxDefaultPosition, wxDefaultSize, 0);
@@ -510,7 +513,7 @@ void AttitudePanel::Create()
    spinRateTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_SPIN_RATE, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    // Now add the NadirPointing widgets
    bodyAlignVectorLabel =
@@ -518,34 +521,34 @@ void AttitudePanel::Create()
    bodyAlignVectorXTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_ALIGNMENT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    bodyAlignVectorYTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_ALIGNMENT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    bodyAlignVectorZTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_ALIGNMENT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
   bodyConstraintVectorLabel =
       new wxStaticText(this, ID_TEXT, wxT("Body Constraint Vector"), wxDefaultPosition, wxDefaultSize, 0);
    bodyConstraintVectorXTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_CONSTRAINT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    bodyConstraintVectorYTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_CONSTRAINT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
    bodyConstraintVectorZTextCtrl =
       new wxTextCtrl(this, ID_TEXTCTRL_BODY_CONSTRAINT_VECTOR, wxT(""), wxDefaultPosition,
                      //wxDefaultSize, 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
-                     wxSize(otherTextWidth,20), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
+                     wxSize(otherTextWidth,defHeight), 0, wxTextValidator(wxGMAT_FILTER_NUMERIC));
 
    // Reference Body must be a Celestial Body
    attRefBodyLabel =
       new wxStaticText( this, ID_TEXT, GUI_ACCEL_KEY"Attitude Reference Body",
-         wxDefaultPosition, wxSize(staticTextWidth,20), 0); // wxDefaultSize, 0);
+         wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); // wxDefaultSize, 0);
    referenceBodyComboBox =  theGuiManager->GetCelestialBodyComboBox(this, ID_CB_REFERENCE_BODY,
       wxDefaultSize);
    referenceBodyComboBox->SetToolTip(pConfig->Read(_T("ReferenceBodyHint")));
@@ -558,7 +561,7 @@ void AttitudePanel::Create()
 
    constraintTypeLabel =
       new wxStaticText( this, ID_TEXT, GUI_ACCEL_KEY"Attitude Constraint Type",
-         wxDefaultPosition, wxSize(staticTextWidth,20), 0); // wxDefaultSize, 0);
+         wxDefaultPosition, wxSize(staticTextWidth,defHeight), 0); // wxDefaultSize, 0);
    constraintTypeComboBox =
          new wxComboBox( this, ID_CB_CONSTRAINT_TYPE, constraintArray[0],
             wxDefaultPosition, wxDefaultSize, constraintTypesSz, constraintArray,
@@ -575,7 +578,7 @@ void AttitudePanel::Create()
    aemFileTextCtrl->SetToolTip(pConfig->Read(_T("AttitudeFileNameHint")));
    aemBrowseButton =
       new wxBitmapButton(this, ID_BUTTON_BROWSE, openBitmap, wxDefaultPosition,
-                         wxSize(buttonWidth, 20));
+                         wxSize(buttonWidth, defHeight));
    aemBrowseButton->SetToolTip(pConfig->Read(_T("BrowseAttitudeFileNameHint")));
 
 
@@ -619,7 +622,11 @@ void AttitudePanel::Create()
    
    // Create 3x5 flex grid sizer for ProcessingSpinner angles and rates
    // 1st column: Labels, 2nd column: Values, 3rd column: units
-   wxFlexGridSizer *precSpinAnglesRatesSizer = new wxFlexGridSizer(3,5,0,0);
+   // Used wxFlexGridSize(3) for 3 columns (Fix for GMT-4732, LOJ: 2014.10.02)
+   // wxFlexGridSizer(3,5,0,0) means 3 rows and 5 columns in wx3.0
+   // wxFlexGridSizer(3,5) means 3 columns and vgap of 5 in wx2.8
+   //wxFlexGridSizer *precSpinAnglesRatesSizer = new wxFlexGridSizer(3,5,0,0);
+   wxFlexGridSizer *precSpinAnglesRatesSizer = new wxFlexGridSizer(3);
    precSpinAnglesRatesSizer->Add(initPrecAngleLabel, 0, wxALIGN_LEFT|wxALL, bsize );
    precSpinAnglesRatesSizer->Add(initPrecAngleTextCtrl, 0, wxALIGN_LEFT|wxALL, bsize );
    precSpinAnglesRatesSizer->Add(initPrecAngleUnits, 0, wxALIGN_LEFT|wxALL, bsize );
@@ -833,7 +840,6 @@ void AttitudePanel::LoadData()
       MessageInterface::ShowMessage("AttitudePanel::LoadData() entered\n");
    #endif
    
-   unsigned int x, y;
    bool newAttitude = false;
    // check to see if the spacecraft has an attitude object
    theAttitude = (Attitude*) theSpacecraft->GetRefObject(Gmat::ATTITUDE, "");
