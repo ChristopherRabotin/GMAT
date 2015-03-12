9016afe5bc2ab4bcc76fdabf4454ad3dea19d1ae
Darrel Conway <djc@thinksysinc.com>
2015-03-12 16:41:00 -0700

    VS2013 build settings
    Former-commit-id: 11c2fb0bba590f56929757e33fe4ab29b3c4b8f0

--- plugins/PythonInterfacePlugin/src/base/plugin/GmatPluginFunctions.hpp
+++ plugins/PythonInterfacePlugin/src/base/plugin/GmatPluginFunctions.hpp
@@ -1,20 +1,4 @@
-//$Id$
-//------------------------------------------------------------------------------
-//                            GmatPluginFunctions
-//------------------------------------------------------------------------------
-// GMAT: General Mission Analysis Tool
-//
-// Copyright (c) 2012 Thinking Systems, Inc.
-// Free usage granted to all users; this is shell code.  Adapt and enjoy.  
-// Attribution is appreciated.
-//
-// Author: Darrel J. Conway, Thinking Systems, Inc.
-// Created: July 15, 2012
-//
-/**
- * Definition for library code interfaces.
- */
-//------------------------------------------------------------------------------
+
 #ifndef GmatPluginFunctions_hpp
 #define GmatPluginFunctions_hpp
 
