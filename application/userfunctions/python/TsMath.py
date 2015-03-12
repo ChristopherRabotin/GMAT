44af2e808fb510e121b4910e8e1eed1bf2cc1287
Darrel Conway <djc@thinksysinc.com>
2015-03-12 15:26:47 -0700

    Sample Python funcction call
    Former-commit-id: af764fb0d652fc9f04c4e60063b2d2f2ae54aef5

---
+++ application/userfunctions/python/TsMath.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 13:14:38 2015
+
+@author: djc@thinksysinc.com
+"""
+import math
+
+def sqrt(a):
+   retval = -1.0;
+   
+   if a >= 0:
+      retval = math.sqrt(a);
+   
+   return retval;
