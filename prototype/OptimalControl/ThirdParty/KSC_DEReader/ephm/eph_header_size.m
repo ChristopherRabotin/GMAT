ff88658f43e1610c590525ee20d10a20d3c40a34
sphughe1 <steven.p.hughes@nasa.gov>
2015-03-18 09:49:33 -0400

    More files that should not be in public repo.  Not release but making sure they aren't in the future.
    Former-commit-id: 1acdb0f26188ef8efdfe6f9f62288515da276ef4

--- prototype/OptimalControl/ThirdParty/KSC_DEReader/ephm/eph_header_size.m
+++
@@ -1,9 +0,0 @@
-function [ttlsize,cnamsize,sssize,iptsize] = eph_header_size()
-
-% EPH_HEADER_SIZE Set sizes for JPL Ephemeris header file parameters
-
-% DE200 values
-ttlsize  = [3 80];
-cnamsize = 8;
-sssize   = 3;
-iptsize  = [3 13];
