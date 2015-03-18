794ec7ed268adb88991503378ca1868a3ae8db6e
sphughe1 <steven.p.hughes@nasa.gov>
2015-03-18 09:48:48 -0400

    Deleting files that should not have been in a public repo.  These have not been released but Darrel noticed they might be later so removing to avoid that possibility.
    Former-commit-id: df66018dd7babadbfb4ca7c8f3a94fb7c2de1d8c

--- prototype/OptimalControl/ThirdParty/snopt/Contents.m
+++
@@ -1,6 +0,0 @@
-%-----------------------------------------------------------------------------%
-%                   SNOPT MATLAB Files for Use with GPOPS-II                  %
-%-----------------------------------------------------------------------------%
-% This directory contains a restricted version of SNOPT for use with GPOPS-II.%
-% The required paths are set automatically by running the path setup script.  %
-%-----------------------------------------------------------------------------%
