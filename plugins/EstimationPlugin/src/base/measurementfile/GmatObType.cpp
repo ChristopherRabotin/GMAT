a60c870fbd9b7c20f1e826e81f145cca08e6c920
Tuan Dang Nguyen <tuan.d.nguyen@nasa.gov>
2015-05-07 09:57:36 -0400

    Add code to verify measurement type from observation data
    Former-commit-id: c4b6a51578329da10437e02eaaa4e931825d6084

--- plugins/EstimationPlugin/src/base/measurementfile/GmatObType.cpp
+++ plugins/EstimationPlugin/src/base/measurementfile/GmatObType.cpp
@@ -358,6 +358,20 @@ bool GmatObType::AddMeasurement(MeasurementData *md)
 }
 
 
+StringArray GmatObType::GetAvailableMeasurementTypes()
+{
+   StringArray typeList;
+   typeList.push_back("Range_KM");
+   typeList.push_back("DSNRange");
+   typeList.push_back("Doppler");
+   typeList.push_back("DSNTwoWayRange");
+   typeList.push_back("DSNTwoWayDoppler");
+   typeList.push_back("USNTwoWayRange");
+
+   return typeList;
+}
+
+
 //-----------------------------------------------------------------------------
 // ObservationData* ReadObservation()
 //-----------------------------------------------------------------------------
@@ -426,6 +440,12 @@ ObservationData* GmatObType::ReadObservation()
    theLine >> type;
    currentObs.type = (Gmat::MeasurementType)type;
 
+   // Verify measurement type
+   StringArray typeList = GetAvailableMeasurementTypes();
+   if (find(typeList.begin(), typeList.end(), currentObs.typeName) == typeList.end())
+      throw MeasurementException("Error: GMAT can't handle measurement type '" + currentObs.typeName + "'.\n");
+
+
    // Signal based measurements have types that start at 9000; smaller IDs are
    // the old code.
    /// @todo Once ported to signal measurements, remove the code from here:
@@ -481,7 +501,8 @@ ObservationData* GmatObType::ReadObservation()
       }
 #endif
 
-      if ((currentObs.typeName == "Range")||(currentObs.typeName == "DSNRange")
+      //if ((currentObs.typeName == "Range")||(currentObs.typeName == "DSNRange")       // made changes by TUAN NGUYEN
+      if ((currentObs.typeName == "Range_KM")||(currentObs.typeName == "DSNRange")      // made changes by TUAN NGUYEN
          ||(currentObs.typeName == "Doppler"))
       {
          dataSize = 1;
--- plugins/EstimationPlugin/src/base/measurementfile/GmatObType.hpp
+++ plugins/EstimationPlugin/src/base/measurementfile/GmatObType.hpp
@@ -69,6 +69,8 @@ private:
 
    bool ProcessSignals(const std::string str, Integer& participantSize,
          Integer& dataSize);
+
+   StringArray       GetAvailableMeasurementTypes();          // made changes by TUAN NGUYEN
 };
 
 #endif /* GMATOBTYPE_HPP_ */
