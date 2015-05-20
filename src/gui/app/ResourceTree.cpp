eddd1ea850d22adde474e8eeb27896b355076e9a
Linda Jun <linda.o.jun@nasa.gov>
2015-05-20 15:40:43 -0400

    Fix for GMT-3664 Resource tree not repopulated
    Updated to repopulate Spacecraft tree item when default subscribers are added to Resource Output.
    
    
    Former-commit-id: a41fbd6b3b811062520faaccf557f1d9cd0ad2f2

--- src/gui/app/ResourceTree.cpp
+++ src/gui/app/ResourceTree.cpp
@@ -666,6 +666,31 @@ void ResourceTree::UpdateRecentFiles(wxString filename)
 
 }
 
+//------------------------------------------------------------------------------
+// void UpdateSpacecraft()
+//------------------------------------------------------------------------------
+/**
+ * Updates Spacecraft node.
+ */
+//------------------------------------------------------------------------------
+void ResourceTree::UpdateSpacecraft()
+{
+   #ifdef DEBUG_UPDATE
+   MessageInterface::ShowMessage
+      ("ResourceTree::UpdateSpacecraft() entered, number of spacecraft: %d\n",
+       GetChildrenCount(mSpacecraftItem));
+   #endif
+
+   // Update GUI spacecraft list
+   theGuiManager->UpdateSpacecraft();
+   
+   // If Spacecraft node is empty, populate with default spacecraft
+   if (GetChildrenCount(mSpacecraftItem) == 0)
+   {
+      DeleteChildren(mSpacecraftItem);
+      AddDefaultSpacecraft(mSpacecraftItem);
+   }
+}
 
 //------------------------------------------------------------------------------
 // void UpdateVariable()
@@ -3485,6 +3510,8 @@ void ResourceTree::OnAddReportFile(wxCommandEvent &event)
       Expand(item);
       SelectItem(GetLastChild(item));
       theGuiManager->UpdateSubscriber();
+      // Repopulate Spacecraft node as default ReportFile creates default Spacecraft
+      UpdateSpacecraft();
    }
 }
 
@@ -3511,6 +3538,8 @@ void ResourceTree::OnAddXyPlot(wxCommandEvent &event)
       Expand(item);
       SelectItem(GetLastChild(item));
       theGuiManager->UpdateSubscriber();
+      // Repopulate Spacecraft node as default XYPlot creates default Spacecraft
+      UpdateSpacecraft();
    }
 }
 
@@ -3537,6 +3566,8 @@ void ResourceTree::OnAddOrbitView(wxCommandEvent &event)
       Expand(item);
       SelectItem(GetLastChild(item));
       theGuiManager->UpdateSubscriber();
+      // Repopulate Spacecraft node as default XYPlot creates default Spacecraft
+      UpdateSpacecraft();
    }
 }
 
@@ -3563,6 +3594,8 @@ void ResourceTree::OnAddEphemerisFile(wxCommandEvent &event)
       Expand(item);
       SelectItem(GetLastChild(item));
       theGuiManager->UpdateSubscriber();
+      // Repopulate Spacecraft node as default XYPlot creates default Spacecraft
+      UpdateSpacecraft();
    }
 }
 
@@ -3591,6 +3624,8 @@ void ResourceTree::OnAddSubscriber(wxCommandEvent &event)
       Expand(item);
       SelectItem(GetLastChild(item));
       theGuiManager->UpdateSubscriber();
+      // Repopulate Spacecraft node as default ReportFile creates default Spacecraft
+      UpdateSpacecraft();
    }
 }
 
