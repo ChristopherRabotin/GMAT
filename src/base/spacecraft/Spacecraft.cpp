16f9be8405845586322fd40d04b8082b774b3b36
Darrel Conway <djc@thinksysinc.com>
2016-03-03 17:12:33 -0700

    Added code to manage mass flow modeled from a thrust history file
    Former-commit-id: dcfa9f718547bcbeac2d76f925d9eba12285fae2

--- src/base/spacecraft/Spacecraft.cpp
+++ src/base/spacecraft/Spacecraft.cpp
@@ -5965,15 +5965,17 @@ bool Spacecraft::ApplyTotalMass(Real newMass)
             newMass, massChange);
    #endif
 
-   // Find the active thruster(s)
-   ObjectArray active;
+   // Find the active thruster(s) and THF tanks
+   ObjectArray activeThrusters;
+   ObjectArray thrustHistoryTanks;
+
    RealArray   flowrate;
    Real        totalFlow = 0.0, rate;
    for (ObjectArray::iterator i = thrusters.begin(); i != thrusters.end(); ++i)
    {
       if ((*i)->GetBooleanParameter("IsFiring"))
       {
-         active.push_back(*i);
+         activeThrusters.push_back(*i);
          rate = ((Thruster*)(*i))->CalculateMassFlow();
          #ifdef DEBUG_MASS_FLOW
             MessageInterface::ShowMessage("Thruster %s returned %12.10f\n",
@@ -5983,55 +5985,96 @@ bool Spacecraft::ApplyTotalMass(Real newMass)
          totalFlow += rate;
       }
    }
-//   if (GmatMathUtil::IsEqual(totalFlow, 0.0))
-//   {
-////      MessageInterface::ShowMessage("Total Flow is zero!!!\n");  // temporary
-//      return true;
-//   }
 
-   // Divide the mass flow evenly between the tanks on each active thruster
-   Real numberFiring = active.size();
-   if ((numberFiring <= 0) && (massChange != 0.0))
+   for (ObjectArray::iterator i = tanks.begin(); i != tanks.end(); ++i)
+   {
+      if (((FuelTank*)(*i))->NoThrusterNeeded())
+      {
+         thrustHistoryTanks.push_back(*i);
+      }
+   }
+
+   // Divide the mass flow between the tanks on each activeThrusters thruster
+   UnsignedInt numberFiring = activeThrusters.size();
+   UnsignedInt numberDraining = thrustHistoryTanks.size();
+
+   if ((numberFiring <= 0) && (numberDraining <= 0) && (massChange != 0.0))
    {
       std::stringstream errmsg;
       errmsg.precision(15);
       errmsg << "Mass update " << massChange
              << " requested for " << instanceName
-             << " but there are no active thrusters";
+             << " but there are no active thrusters or thrust file tanks";
       throw SpaceObjectException(errmsg.str());
    }
 
-   Real dm;  // = massChange / numberFiring;
-   for (UnsignedInt i = 0; i < active.size(); ++i)
+   // For now, don't allow both modes
+   if ((numberFiring > 0) && (numberDraining > 0))
+   {
+      std::stringstream errmsg;
+      errmsg.precision(15);
+      errmsg << "Mass update " << massChange
+             << " requested for " << instanceName
+             << " from thrusters and from a thrust file.  That mode is not "
+             << "currently supported.";
+      throw SpaceObjectException(errmsg.str());
+   }
+
+   Real dm;
+
+   // todo: Check: This loop needs to be updated to split apart the flow based on plumbing
+   for (UnsignedInt i = 0; i < activeThrusters.size(); ++i)
    {
       // Change the mass in each attached tank
-      ObjectArray usedTanks = active[i]->GetRefObjectArray(Gmat::HARDWARE);
+      ObjectArray usedTanks = activeThrusters[i]->GetRefObjectArray(Gmat::HARDWARE);
       // ******
       if (!GmatMathUtil::IsEqual(totalFlow,0.0))
       {
-      dm = massChange * flowrate[i] / totalFlow;
+         dm = massChange * flowrate[i] / totalFlow;
 
-      #ifdef DEBUG_MASS_FLOW
-         MessageInterface::ShowMessage("flowrate = %12.10f, totalFlow = %12.10f\n",
-               flowrate[i], totalFlow);
-         MessageInterface::ShowMessage("%.12le from %s = [ ", dm, active[i]->GetName().c_str());
-      #endif
-
-      Real dmt = dm / usedTanks.size();
-      for (ObjectArray::iterator j = usedTanks.begin();
-            j != usedTanks.end(); ++j)
-      {
          #ifdef DEBUG_MASS_FLOW
-            MessageInterface::ShowMessage(" %.12le ", dmt);
+            MessageInterface::ShowMessage("flowrate = %12.10f, totalFlow = %12.10f\n",
+                  flowrate[i], totalFlow);
+            MessageInterface::ShowMessage("%.12le from %s = [ ", dm, activeThrusters[i]->GetName().c_str());
          #endif
-         (*j)->SetRealParameter("FuelMass",
-               (*j)->GetRealParameter("FuelMass") + dmt);
-      }
+
+         Real dmt = dm / usedTanks.size();
+         for (ObjectArray::iterator j = usedTanks.begin();
+               j != usedTanks.end(); ++j)
+         {
+            #ifdef DEBUG_MASS_FLOW
+               MessageInterface::ShowMessage(" %.12le ", dmt);
+            #endif
+            (*j)->SetRealParameter("FuelMass",
+                  (*j)->GetRealParameter("FuelMass") + dmt);
+         }
       } // *****************
       #ifdef DEBUG_MASS_FLOW
-               MessageInterface::ShowMessage(" ] ");
+         MessageInterface::ShowMessage(" ] ");
       #endif
    }
+
+   if (thrustHistoryTanks.size() > 0)
+   {
+      #ifdef DEBUG_MASS_FLOW
+         MessageInterface::ShowMessage("Depleting mass thruster free: [");
+      #endif
+
+      dm = massChange / thrustHistoryTanks.size();
+      for (UnsignedInt i = 0; i < thrustHistoryTanks.size(); ++i)
+      {
+         #ifdef DEBUG_MASS_FLOW
+            MessageInterface::ShowMessage(" %.12le ", dm);
+         #endif
+
+         thrustHistoryTanks[i]->SetRealParameter("FuelMass",
+               thrustHistoryTanks[i]->GetRealParameter("FuelMass") + dm);
+      }
+      #ifdef DEBUG_MASS_FLOW
+         MessageInterface::ShowMessage(" ] ");
+      #endif
+   }
+
    #ifdef DEBUG_MASS_FLOW
       MessageInterface::ShowMessage("\n");
    #endif
