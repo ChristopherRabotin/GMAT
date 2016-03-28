f2a982d95b33e96836c38a674ac9bee239a170d1
sphughe1 <steven.p.hughes@nasa.gov>
2016-03-28 10:21:46 -0400

    Renamed test driver files for consistency.
    Former-commit-id: 70c2a05f11c13f1fc2b62b7ef3199b4eeff2ad86

--- prototype/TAT-C/test/UnitTest_EarthModel.m
+++ prototype/TAT-C/test/UnitTest_EarthModel.m
@@ -2,9 +2,10 @@
 
 cb = Earth();
 
-GMT = cb.ComputeGMT(2457260.12345679)
+GMT = cb.ComputeGMT(2457260.12345679);
 
 %  Need to verify the sign.
 if abs(GMT*180/pi + 198.002628503035)
    error('error calcualing GMT')
 end
+
--- prototype/TAT-C/test/UnitTest_OrbitState.m
+++ prototype/TAT-C/test/UnitTest_OrbitState.m
@@ -3,7 +3,7 @@
 % Create the spacecraft state object and set Keplerian elements
 state = OrbitState();
 
-%  Test the Cartesian state converstion
+%  Set Keplerian and test conversion to Cartesian
 state.SetKeplerianState(6900,0.002,pi/3,pi/4,pi/4,pi/4);
 truthCart = [-2436.063522947054 2436.063522947055 5967.112612227063 ...
     -5.385803634090905 -5.378203080755706 0.009308738717021944]';
@@ -13,7 +13,7 @@ if diff > 1e-11
     error('error in conversion from Keplerian to Cartesian')
 end
 
-%  Test the Keplerian state conversion
+% Set Cartesian state and test conversion to Keplerian
 truthKep = [6900,0.002,pi/3,pi/4,pi/4,pi/4];
 state.SetCartesianState(truthCart);
 kepOut = state.GetKeplerianState();
