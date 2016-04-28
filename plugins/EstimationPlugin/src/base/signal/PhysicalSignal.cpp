c5c74c075257310d2c852d9b40dabe36c778f2d7
Tuan Dang Nguyen <tuan.d.nguyen@nasa.gov>
2016-04-28 11:24:13 -0400

    GMT-5576 QA check for Atmospheric Corrections
    Former-commit-id: 6c7128e87e598f56d253c9369ec57fa128fed760

--- plugins/EstimationPlugin/src/base/signal/PhysicalSignal.cpp
+++ plugins/EstimationPlugin/src/base/signal/PhysicalSignal.cpp
@@ -45,6 +45,11 @@
 //#define DEBUG_RELATIVITY_CORRECTION
 //#define DEBUG_RANGE_CALCULATION
 
+
+Integer PhysicalSignal::tropoWarningCount = 0;
+Integer PhysicalSignal::ionoWarningCount = 0;
+
+
 //------------------------------------------------------------------------------
 // PhysicalSignal(const std::string &typeStr, const std::string &name)
 //------------------------------------------------------------------------------
@@ -2257,7 +2262,27 @@ RealArray PhysicalSignal::MediaCorrection(Real freq, Rvector3 r1B, Rvector3 r2B,
          MessageInterface::ShowMessage(" frequency = %le MHz,  epoch1 = %.12lf   epoch2 = %.12lf,   r2B-r1B = ('%.8lf   %.8lf   %.8lf')km\n", freq, epoch1, epoch2, rangeVector[0], rangeVector[1], rangeVector[2]);
          MessageInterface::ShowMessage(" TroposhereCorrection = (%lf m,  %lf arcsec,   %le s)\n", tropoCorrection[0], tropoCorrection[1], tropoCorrection[2]);
       #endif
-      
+
+      // GMT-5576 QA Check for Atmosphere Correction
+      if (tropoCorrection[0] < 0.0)
+      {
+         if (tropoWarningCount == 0)
+         {
+            MessageInterface::ShowMessage("Warning: Troposphere correction has a negative value (%lf km).\n", tropoCorrection[0]);
+            MessageInterface::ShowMessage("The correction is calculated based on signal sent or received on station %s at epoch %.12lf A1Mjd to spacecraft %s.\n", theData.tNode->GetName().c_str(), epoch1, theData.rNode->GetName().c_str());
+            ++tropoWarningCount;
+         }
+      }
+      else if (tropoCorrection[0] > 1000.0)
+      {
+         if (tropoWarningCount == 0)
+         {
+            MessageInterface::ShowMessage("Warning: Troposphere correction has an expected lager value (%lf km).\n", tropoCorrection[0]);
+            MessageInterface::ShowMessage("The correction is calculated based on signal sent or received on station %s at epoch %.12lf A1Mjd to spacecraft %s.\n", theData.tNode->GetName().c_str(), epoch1, theData.rNode->GetName().c_str());
+            ++tropoWarningCount;
+         }
+      }
+
       mediaCorrection[0] = tropoCorrection[0];
       mediaCorrection[1] = tropoCorrection[1];
       mediaCorrection[2] = tropoCorrection[2];
@@ -2282,7 +2307,27 @@ RealArray PhysicalSignal::MediaCorrection(Real freq, Rvector3 r1B, Rvector3 r2B,
             MessageInterface::ShowMessage("                      epoch2 = %lf,  r2B = ('%.8lf   %.8lf   %.8lf')km\n", epoch2, r2B[0], r2B[1], r2B[2]);
             MessageInterface::ShowMessage(" IonoshereCorrection = (%lf m,  %lf arcsec,   %le s)\n", ionoCorrection[0], ionoCorrection[1], ionoCorrection[2]);
          #endif
-      
+
+         // GMT-5576 QA Check for Atmosphere Correction
+         if (ionoCorrection[0] < 0.0)
+         {
+            if (ionoWarningCount == 0)
+            {
+               MessageInterface::ShowMessage("Warning: Ionosphere correction has a negative value (%lf km).\n", ionoCorrection[0]);
+               MessageInterface::ShowMessage("The correction is calculated based on signal sent or received on station %s at epoch %.12lf A1Mjd to spacecraft %s.\n", theData.tNode->GetName().c_str(), epoch1, theData.rNode->GetName().c_str());
+               ++ionoWarningCount;
+            }
+         }
+         else if (ionoCorrection[0] > 1000.0)
+         {
+            if (ionoWarningCount == 0)
+            {
+               MessageInterface::ShowMessage("Warning: Ionosphere correction has an expected large value (%lf km).\n", ionoCorrection[0]);
+               MessageInterface::ShowMessage("The correction is calculated based on signal sent or received on station %s at epoch %.12lf A1Mjd to spacecraft %s.\n", theData.tNode->GetName().c_str(), epoch1, theData.rNode->GetName().c_str());
+               ++ionoWarningCount;
+            }
+         }
+
          // 3. Combine effects:
          mediaCorrection[0] += ionoCorrection[0];
          mediaCorrection[1] += ionoCorrection[1];
