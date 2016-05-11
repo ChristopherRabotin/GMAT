4613e4e63425eab53c0202458ecad5cf66355f28
Linda Jun <linda.o.jun@nasa.gov>
2016-05-11 18:27:44 -0400

    GMT-4996 Need reportable Finite thrust parameters Created TotalMassFlowRate so that it can be shown in the GUI ParameterSelectDialog for default mission.
    Former-commit-id: 5fea3eb0686aa1e57ccd63d8b63350434dc7c0a8

--- src/base/executive/Moderator.cpp
+++ src/base/executive/Moderator.cpp
@@ -8201,6 +8201,7 @@ void Moderator::CreateDefaultParameters()
    // The ref object of TotalThrust1 is actually Burn object, so we will create
    // Parameters with ImpulsiveBurn to show FiniteBurn property list in the default
    // mission GUI.
+   CreateParameter("TotalMassFlowRate", "DefaultIB.TotalMassFlowRate");
    CreateParameter("TotalThrust1", "DefaultIB.TotalThrust1");
    CreateParameter("TotalThrust2", "DefaultIB.TotalThrust2");
    CreateParameter("TotalThrust3", "DefaultIB.TotalThrust3");
