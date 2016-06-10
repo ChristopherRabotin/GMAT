dcfe5d3cee9b7af6bfc50257023c55223bfe6815
Linda Jun <linda.o.jun@nasa.gov>
2016-06-10 09:40:01 -0400

    Reset dataChanged flags after loading data so that it will not save the same data. Fixed WX3 related debug output.
    Former-commit-id: f0b0c5de54051d8d1a6fda30f0f49e8a37c04655

--- src/gui/spacecraft/VisualModelCanvas.cpp
+++ src/gui/spacecraft/VisualModelCanvas.cpp
@@ -452,7 +452,7 @@ bool VisualModelCanvas::LoadModel(const wxString &filePath)
 {
    #ifdef DEBUG_LOAD_MODEL
    MessageInterface::ShowMessage
-      ("VisualModelCanvas::LoadModel(filePath) entered, filePath='%s'\n", filePath.c_str());
+      ("VisualModelCanvas::LoadModel(filePath) entered, filePath='%s'\n", filePath.WX_TO_C_STRING);
    #endif
    
    if (GmatFileUtil::DoesFileExist(filePath.c_str()))
@@ -482,7 +482,7 @@ void VisualModelCanvas::LoadModel()
 {
    #ifdef DEBUG_LOAD_MODEL
    MessageInterface::ShowMessage
-      ("VisualModelCanvas::LoadModel() entered, modelPath='%s'\n", modelPath.c_str());
+      ("VisualModelCanvas::LoadModel() entered, modelPath='%s'\n", modelPath.WX_TO_C_STRING);
    #endif
    ModelManager *mm = ModelManager::Instance();
    std::string mP = modelPath.WX_TO_STD_STRING;
