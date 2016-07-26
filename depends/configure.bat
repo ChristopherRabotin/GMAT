b575cbf7fe63eb0666520ade908a6a3287991e32
Ravi Mathur <ravi.mathur@emergentspace.com>
2016-07-26 12:13:22 -0400

    Expand Xerces support for 32/64-bit and multiple VisualStudio versions
    Former-commit-id: ef2d26ced636905fb90259cf3b94c03500f57805

--- depends/configure.bat
+++ depends/configure.bat
@@ -226,11 +226,11 @@ echo ********** Configuring Xerces-C++ %xercesver% **********
 
 set xerces_path=xerces
 IF %use_64bit% EQU 1 (
-	set xerces_type=64
-	set xplatform=x64
+	set xerces_outdir=%xerces_path%\Build\Win64
+	set xerces_arch=x64
 ) ELSE (
-	set xerces_type=32
-	set xplatform=Win32
+	set xerces_outdir=%xerces_path%\Build\Win32
+	set xerces_arch=Win32
 )
 
 :: Download Xerces if it doesn't exist
@@ -249,14 +249,14 @@ IF NOT EXIST %xerces_path% (
 )
 
 :: Compile Xerces
-IF NOT EXIST %xerces_path%\Build\Win%xerces_type% (
+IF NOT EXIST %xerces_outdir% (
 	:: Compile debug Xerces
 	:: XercesLibOverride is needed because the Xerces Runtime Library cannot be directly changed with an msbuild flag
 	echo.
 	echo -- Compiling debug Xerces. This could take a while...
 	cd %xerces_path%\projects\Win32\VC%vs_version%\xerces-all\XercesLib
 	msbuild /m ^
-		/property:Configuration="Static Debug";Platform=%xplatform% ^
+		/property:Configuration="Static Debug";Platform=%xerces_arch% ^
 		/property:TargetName=xerces-c_3D ^
 		/property:ForceImportBeforeCppTargets="%depends_dir%\bin\xerces\XercesLibOverride.prop" ^
 		XercesLib.vcxproj > %logs_dir%\xerces_build_debug.log 2>&1
@@ -265,7 +265,7 @@ IF NOT EXIST %xerces_path%\Build\Win%xerces_type% (
 	echo.
 	echo -- Compiling release Xerces. This could take a while...
 	msbuild /m ^
-		/property:Configuration="Static Release";Platform=%xplatform% ^
+		/property:Configuration="Static Release";Platform=%xerces_arch% ^
 		/property:TargetName=xerces-c_3 ^
 		/property:ForceImportBeforeCppTargets="%depends_dir%\bin\xerces\XercesLibOverride.prop" ^
 		XercesLib.vcxproj > %logs_dir%\xerces_build_release.log 2>&1
