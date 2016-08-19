9e14b9b33bde8aa68271378c0596939cf9683f7e
Darrel J. Conway <djc@thinksysinc.com>
2016-08-19 13:56:40 -0700

    GMT-5440  Issued warning on run when unset mix ratio coefficients are detected.
    Former-commit-id: 4c36de1bf01996dac47116103d77f324b68bfad0

--- src/base/hardware/Thruster.cpp
+++ src/base/hardware/Thruster.cpp
@@ -1636,6 +1636,17 @@ bool Thruster::Initialize()
    if (!retval)
       return false;
    
+   if (mixRatio[mixRatio.GetSize()-1] == RATIO_DEFAULT)
+   {
+      MessageInterface::ShowMessage("Warning: The number of coefficients in "
+            "the mix ratio does not match the number of tanks used by Thruster "
+            "%s.  Unset ratio values are set to 1.0\n", instanceName.c_str());
+
+      for (Integer i = 0; i < mixRatio.GetSize(); ++i)
+         if (mixRatio[i] == RATIO_DEFAULT)
+            mixRatio[i] = 1.0;
+   }
+
    if (mixRatio.GetSize() == 0)
    {
       mixRatio.SetSize(tankNames.size());
